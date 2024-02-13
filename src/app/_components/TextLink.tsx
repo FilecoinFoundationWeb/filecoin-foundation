c9c342e974cf6a0db4fa5061daeb11e18971d2c9
Mirha Masala <mirha@hey.com>
2024-02-13 15:25:08 +0100

    Refactor TextLink with clsx

---
+++ src/app/_components/TextLink.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link'
+
+import clsx from 'clsx'
+
+type TextLinkProps = {
+  href: string
+  children: React.ReactNode
+  className?: string
+}
+
+export function TextLink({
+  href,
+  children,
+  className,
+  ...props
+}: TextLinkProps) {
+  const isInternalLink = href.startsWith('/') || href.startsWith('#')
+  const baseStyles = `underline`
+
+  className = clsx(baseStyles, className)
+
+  if (isInternalLink) {
+    return (
+      <Link href={href} className={className} {...props}>
+        {children}
+      </Link>
+    )
+  }
+
+  return (
+    <a href={href} className={className} {...props}>
+      {children}
+    </a>
+  )
+}
