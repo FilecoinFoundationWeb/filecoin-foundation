c80a25f61e6d7eb50dbd691b2602b9d98831592e
Mirha Masala <26620750+mirhamasala@users.noreply.github.com>
2024-02-13 09:37:42 +0100

    Remove navigation options from CMS (#3)
    * Remove navigation items from CMS options
    
    * Refactor navigation component

--- src/app/_components/Navigation.tsx
+++ src/app/_components/Navigation.tsx
@@ -1,27 +1,38 @@
 import CustomLink from '@/components/CustomLink'
 import Logo from '@/components/Logo'
 
-import navigationData from '@/content/shared/navigation.yml'
+import { PATHS } from '@/constants/paths'
 
-type NavigationItemProps = {
-  order: number
-  url: string
+type NavigationItem = {
   title: string
+  path: string
+  order: number
 }
 
-function NavigationItem({ item }: { item: NavigationItemProps }) {
+const navigationItems = [
+  { title: 'About', path: PATHS.ABOUT, order: 1 },
+  { title: 'Blog', path: PATHS.BLOG, order: 2 },
+  { title: 'Governance', path: PATHS.GOVERNANCE, order: 3 },
+  { title: 'Ecosystem', path: PATHS.ECOSYSTEM, order: 4 },
+  { title: 'Case Studies', path: PATHS.CASE_STUDIES, order: 5 },
+  { title: 'Grants', path: PATHS.GRANTS, order: 6 },
+  { title: 'Get Involved', path: PATHS.GET_INVOLVED, order: 7 },
+  { title: 'Public Data', path: PATHS.PUBLIC_DATA, order: 8 },
+  { title: 'Awards', path: PATHS.PUBLIC_DATA_AWARDS, order: 9 },
+  { title: 'Events', path: PATHS.EVENTS, order: 10 },
+]
+
+const sortedNavigationItems = navigationItems.sort((a, b) => a.order - b.order)
+
+function NavigationItem({ item }: { item: NavigationItem }) {
   return (
     <li className="mr-5 last:mr-0 whitespace-nowrap">
-      <CustomLink href={item.url}>{item.title}</CustomLink>
+      <CustomLink href={item.path}>{item.title}</CustomLink>
     </li>
   )
 }
 
 export default function Navigation() {
-  const sortedNavigationItems = navigationData.navigation_items.sort(
-    (a: NavigationItemProps, b: NavigationItemProps) => a.order - b.order
-  )
-
   return (
     <nav className="flex justify-between items-center p-4">
       <CustomLink className="block grow" href="/" aria-label="Go to homepage">
@@ -29,8 +40,8 @@ export default function Navigation() {
         <span className="sr-only">Home</span>
       </CustomLink>
       <ul className="list-none flex items-center m-0 p-0">
-        {sortedNavigationItems.map((item: NavigationItemProps) => (
-          <NavigationItem key={item.url} item={item} />
+        {sortedNavigationItems.map((item: NavigationItem) => (
+          <NavigationItem key={item.path} item={item} />
         ))}
       </ul>
     </nav>
