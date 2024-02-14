1d608b9e9637de3e057d31b4412cf1883be0e30d
Mirha Masala <mirha@hey.com>
2024-02-14 10:43:45 +0100

    Refactor Section

--- src/app/_components/Section.tsx
+++ src/app/_components/Section.tsx
@@ -1,20 +1,39 @@
 import { TextLink } from '@/components/TextLink'
 
 type Props = {
+  kicker?: string
   title: string
   content: string | string[]
   link?: { url: string; text: string }
   children?: React.ReactNode
 }
-export default function Section({ title, content, link, children }: Props) {
+
+function Content({ content }: { content: string | string[] }) {
+  if (Array.isArray(content)) {
+    return (
+      <>
+        {content.map((paragraph, index) => (
+          <p key={`paragraph-${index}`}>{paragraph}</p>
+        ))}
+      </>
+    )
+  }
+
+  return <p>{content}</p>
+}
+
+export default function Section({
+  kicker,
+  title,
+  content,
+  link,
+  children,
+}: Props) {
   return (
     <section>
+      {kicker && <span className="uppercase text-sm">{kicker}</span>}
       <h2>{title}</h2>
-      {Array.isArray(content) ? (
-        content.map((paragraph, index) => <p key={index}>{paragraph}</p>)
-      ) : (
-        <p>{content}</p>
-      )}
+      <Content content={content} />
       {link && <TextLink href={link.url}>{link.text}</TextLink>}
       {children}
     </section>
