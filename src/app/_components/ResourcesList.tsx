e998048415d83e0ccefaa7759830c01b119ccc76
Mirha Masala <mirha@hey.com>
2024-02-14 10:43:34 +0100

    Create ResourcesList

---
+++ src/app/_components/ResourcesList.tsx
@@ -0,0 +1,76 @@
+export function ResourcesList() {
+  return (
+    <ul>
+      <li>
+        <a href="https://docs.filecoin.io/">
+          <h3>Filecoin Documentation</h3>
+          <p>
+            Filecoin project documentation homepage offers the tools to
+            contribute to the network.
+          </p>
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/filecoin-project">
+          <h3>Filecoin GitHub</h3>
+          <p>
+            Contribute to Filecoin and help improve it by engaging in the
+            Filecoin GitHub repository.
+          </p>
+        </a>
+      </li>
+      <li>
+        <a href="https://liftoff.filecoin.io/">
+          <h3>Filecoin Mainnet Ignition & Liftoff</h3>
+          <p>
+            This covers the timeline of the Filecoin Mainnet Ignition & Liftoff.
+          </p>
+        </a>
+      </li>
+      <li>
+        <a href="https://dashboard.starboard.ventures/dashboard">
+          <h3>Filecoin Network Status</h3>
+          <p>
+            This dashboard provides the latest update of the Filecoin Network.
+          </p>
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/filecoin-project/community/discussions">
+          <h3>Filecoin Forums</h3>
+          <p>
+            Details on the latest events hosted by Filecoin Foundation and a
+            full list of community events
+          </p>
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/filecoin-project/research">
+          <h3>Filecoin Research</h3>
+          <p>
+            Breakthroughs underpinning the Filecoin protocol, as well as future
+            areas of exploration.
+          </p>
+        </a>
+      </li>
+      <li>
+        <a href="https://filecoin.io/2020-engineering-filecoins-economy-en.pdf">
+          <h3>Cryptoeconomics Overview</h3>
+          <p>
+            This report describes the components of the Filecoin economy and how
+            they interact.
+          </p>
+        </a>
+      </li>
+      <li>
+        <a href="https://proto.school/">
+          <h3>Filecoin Tutorials (ProtoSchool)</h3>
+          <p>
+            This is a self-guided interactive Filecoin tutorial designed to
+            introduce decentralized web concepts, protocols, and tools.
+          </p>
+        </a>
+      </li>
+    </ul>
+  )
+}
