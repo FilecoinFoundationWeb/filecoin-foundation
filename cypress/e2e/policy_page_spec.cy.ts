fa15c8ae9cf6d514355c30797ff318646470c2b0
Mirha Masala <mirha@hey.com>
2024-02-21 15:12:51 +0000

    Skip tests

--- cypress/e2e/policy_page_spec.cy.ts
+++ cypress/e2e/policy_page_spec.cy.ts
@@ -1,6 +1,6 @@
 import { PATHS } from '../../src/app/_constants/paths'
 import { testPageMetadata } from '../support/test-utils'
 
-describe('Policy Page', function () {
+describe.skip('Policy Page', function () {
   testPageMetadata(PATHS.POLICY)
 })
