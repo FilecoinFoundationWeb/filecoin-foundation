93cb3c870712b833ac99c18dd11867bd4b927d35
Mirha Masala <mirha@hey.com>
2024-02-21 12:14:06 +0000

    Update GetInvolved page content

--- src/app/get-involved/page.tsx
+++ src/app/get-involved/page.tsx
@@ -1,4 +1,6 @@
+import { GetInvolvedList } from '@/components/GetInvolvedList'
 import { PageHeader } from '@/components/PageHeader'
+import { Section } from '@/components/Section'
 import { SectionDeepDive } from '@/components/SectionDeepDive'
 import { StructuredDataScript } from '@/components/StructuredDataScript'
 import { TextLink } from '@/components/TextLink'
@@ -10,6 +12,10 @@ import { generateWebPageStructuredData } from '@/utils/structuredData'
 import { attributes } from '@/content/pages/get-involved.md'
 
 import { PATHS } from '@/constants/paths'
+import {
+  FILECOIN_SOCIAL_MEDIA_URLS,
+  SOCIAL_MEDIA_URLS,
+} from '@/constants/siteMetadata'
 
 const { title, description, seo } = attributes
 
@@ -34,73 +40,20 @@ export default function GetInvolved() {
           we build, we&apos;re hiring for roles to promote the Filecoin
           ecosystem and the broader Web3 movement. Check out our open roles.
         </p>
-        <TextLink href="https://boards.greenhouse.io/filecoinfoundation">
-          View All
-        </TextLink>
+        <TextLink href={SOCIAL_MEDIA_URLS.greenhouse}>View All</TextLink>
       </section>
 
-      <section>
-        <header>
-          <h2>Get Involved</h2>
-          <p>How to get involved and key links.</p>
-          <p>
-            Join Filecoin Slack and participate in our bi-weekly community
-            governance calls every other Tuesday.
-          </p>
-          <TextLink href="https://filecoin.io/slack">
-            Join the Community
-          </TextLink>
-        </header>
-        <ul>
-          <li>
-            <TextLink href="http://hub.fil.org/events">
-              <h3>Attend an event</h3>
-              <p>Join or watch a Filecoin ecosystem event.</p>
-            </TextLink>
-          </li>
-          <li>
-            <TextLink href="https://www.meetup.com/Filecoin-San-Francisco">
-              <h3>Join a meetup group</h3>
-              <p>Find a group of people in your area who connect regularly.</p>
-            </TextLink>
-          </li>
-          <li>
-            <TextLink href="https://docs.filecoin.io/about-filecoin/ways-to-contribute/">
-              <h3>Contribute to the Filecoin project</h3>
-              <p>
-                Shape the future by contributing to Filecoin’s code, research,
-                or docs.
-              </p>
-            </TextLink>
-          </li>
-          <li>
-            <TextLink href="https://filecoin.io/slack">
-              <h3>Participate online</h3>
-              <p>
-                Participate in one of the many online forums about Filecoin,
-                events, or the Filecoin Slack channel.
-              </p>
-            </TextLink>
-          </li>
-          <li>
-            <TextLink href="https://sp.filecoin.io/">
-              <h3>Become a storage provider or builder</h3>
-              <p>
-                Dive right in and become an essential part of the ecosystem.
-              </p>
-            </TextLink>
-          </li>
-          <li>
-            <TextLink href="https://proto.school/">
-              <h3>Learn more on ProtoSchool</h3>
-              <p>
-                Understand how it all works with interactive tutorials from
-                ProtoSchool.
-              </p>
-            </TextLink>
-          </li>
-        </ul>
-      </section>
+      <Section
+        kicker="Get Involved"
+        title="How To Get Involved and Key Links"
+        content="Join Filecoin Slack and participate in our bi-weekly community governance calls every other Tuesday."
+        link={{
+          href: FILECOIN_SOCIAL_MEDIA_URLS.slack,
+          text: 'Join the Community',
+        }}
+      >
+        <GetInvolvedList />
+      </Section>
 
       <SectionDeepDive>
         <VideoArticle
