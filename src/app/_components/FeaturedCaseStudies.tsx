102ca967054c4eb6efe1d5f16dc86fecf89e40fa
Mirha Masala <mirha@hey.com>
2024-03-11 15:37:36 +0000

    Refactor FeaturedCaseStudies component

--- src/app/_components/FeaturedCaseStudies.tsx
+++ src/app/_components/FeaturedCaseStudies.tsx
@@ -1,17 +1,44 @@
-import { CaseStudiesList } from '@/components/CaseStudiesList'
+import { type Route } from 'next'
+
+import { Card } from '@/components/Card'
+import { CardLayout } from '@/components/CardLayout'
 
 import { getCaseStudiesData } from '@/utils/getCaseStudyData'
 
+import { PATHS } from '@/constants/paths'
+
+type FeaturedCaseStudiesProps = {
+  maxPosts?: number
+}
+
 const caseStudies = getCaseStudiesData()
 
-const featuredCaseStudies = caseStudies.filter(
-  (caseStudy) => caseStudy.featured
-)
+export function FeaturedCaseStudies({
+  maxPosts = 4,
+}: FeaturedCaseStudiesProps) {
+  const featuredCaseStudies = caseStudies
+    .filter((caseStudy) => caseStudy.featured)
+    .slice(0, maxPosts)
 
-export function FeaturedCaseStudies() {
   if (featuredCaseStudies.length === 0) {
     return <p>No featured projects available.</p>
   }
 
-  return <CaseStudiesList caseStudies={featuredCaseStudies} />
+  return (
+    <CardLayout>
+      {featuredCaseStudies.map(({ title, description, slug, image }) => (
+        <Card
+          key="slug"
+          title={title}
+          description={description || ''}
+          cta={{
+            type: 'read',
+            href: `${PATHS.CASE_STUDIES.path}/${slug}` as Route,
+          }}
+          image={image}
+          style="muted"
+        />
+      ))}
+    </CardLayout>
+  )
 }
