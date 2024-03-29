9363b00cf1ee4f56f5765e93fd25f40b8dcf63c9
Mirha Masala <mirha@hey.com>
2024-03-29 12:33:45 +0000

    Add button styles

--- src/app/_components/Button.tsx
+++ src/app/_components/Button.tsx
@@ -20,7 +20,8 @@ type ButtonInnerProps = {
 const variantStyles = {
   primary:
     'border-brand-300 bg-brand-300 text-brand-800 hover:border-brand-400 hover:bg-brand-400',
-  ghost: 'border-white bg-brand-800 hover:border-brand-400',
+  ghost:
+    'border-white bg-brand-800 text-brand-100 hover:border-brand-400 hover:text-brand-400 focus:text-brand-400',
 }
 
 function ButtonInner({ icon, children }: ButtonInnerProps) {
@@ -40,7 +41,7 @@ export function Button({
   ...rest
 }: ButtonProps) {
   const baseStyles =
-    'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-lg border px-9 py-3 font-semibold no-underline transition focus:outline-2 focus:outline-white'
+    'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-lg border px-9 py-3 font-semibold transition hover:no-underline focus:outline-2 focus:outline-white'
 
   className = clsx(baseStyles, variantStyles[variant], className)
 
