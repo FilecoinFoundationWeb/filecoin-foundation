c9a43cff14f2432983eb03b4071885c9fd9d5712
Mirha Masala <mirha@hey.com>
2024-04-03 08:09:19 +0100

    Include different layouts

--- src/app/_components/CardLayout.tsx
+++ src/app/_components/CardLayout.tsx
@@ -1,8 +1,25 @@
+import clsx from 'clsx'
+
 type CardLayoutProps = {
   children: React.ReactNode
   as?: React.ElementType
+  type?: 'blogPost' | 'caseStudy'
 }
 
-export function CardLayout({ as: Tag = 'ul', children }: CardLayoutProps) {
-  return <Tag className="grid gap-4 lg:grid-cols-2">{children}</Tag>
+export function CardLayout({
+  as: Tag = 'ul',
+  type = 'blogPost',
+  children,
+}: CardLayoutProps) {
+  const baseLayoutStyles = 'grid gap-4'
+  const extendedLayoutStyles = {
+    blogPost: 'lg:grid-cols-2',
+    caseStudy: 'md:grid-cols-2 lg:grid-cols-3',
+  }
+
+  return (
+    <Tag className={clsx(baseLayoutStyles, extendedLayoutStyles[type])}>
+      {children}
+    </Tag>
+  )
 }
