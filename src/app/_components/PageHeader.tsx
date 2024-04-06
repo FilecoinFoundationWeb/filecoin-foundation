345bdd46a9bed2bb7d69c8ad73fda3893b75c2e3
Mirha Masala <mirha@hey.com>
2024-04-06 08:26:43 +0100

    Refactor PageHeader

--- src/app/_components/PageHeader.tsx
+++ src/app/_components/PageHeader.tsx
@@ -1,20 +1,71 @@
+import Image from 'next/image'
+
+import { Button } from '@/components/Button'
 import { Heading } from '@/components/Heading'
-import { TextLink } from '@/components/TextLink'
+
+type ctaProps = {
+  href: string
+  text: string
+  icon?: React.ReactNode
+}
+
+type imageProps = {
+  url: string
+  alt: string
+}
 
 type PageHeaderProps = {
   title: string
   description: string
-  link?: { href: string; text: string }
+  cta: ctaProps
+  secondaryCta?: ctaProps
+  image?: imageProps
+  metaData?: React.ReactNode
 }
 
-export function PageHeader({ title, description, link }: PageHeaderProps) {
+export function PageHeader({
+  title,
+  description,
+  cta,
+  secondaryCta,
+  image,
+  metaData,
+}: PageHeaderProps) {
   return (
-    <header>
-      <Heading className="mb-4" tag="h1" variant="4xl">
-        {title}
-      </Heading>
-      <p>{description}</p>
-      {link && <TextLink href={link.href}>{link.text}</TextLink>}
-    </header>
+    <>
+      <header className="flex flex-col gap-6 md:flex-row">
+        <div className="flex flex-col md:w-1/2">
+          <Heading tag="h1" variant="4xl" className="mb-4">
+            {title}
+          </Heading>
+          {metaData && <div className="mb-6">{metaData}</div>}
+          <p className="mb-6">{description}</p>
+          <div className="flex flex-col gap-4 sm:flex-row sm:gap-6 md:flex-col md:gap-4">
+            <Button href={cta.href} variant="primary" className="flex-1">
+              {cta.text}
+            </Button>
+            {secondaryCta && (
+              <Button
+                href={secondaryCta.href}
+                variant="ghost"
+                className="flex-1"
+              >
+                {secondaryCta.text}
+              </Button>
+            )}
+          </div>
+        </div>
+        {image && (
+          <div className="relative h-32 w-full rounded-lg border border-brand-100 sm:h-60 md:h-auto md:w-1/2">
+            <Image
+              fill
+              src={image.url}
+              alt={image.alt}
+              className="block rounded-lg object-cover"
+            />
+          </div>
+        )}
+      </header>
+    </>
   )
 }
