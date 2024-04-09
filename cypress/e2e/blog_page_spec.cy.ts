fece9662c2f80433aa0b1b05da818a43e5ba60cb
Mirha Masala <mirha@hey.com>
2024-04-09 06:54:11 +0100

    Refactor and extend tests to include pages with a featured post

--- cypress/e2e/blog_page_spec.cy.ts
+++ cypress/e2e/blog_page_spec.cy.ts
@@ -2,5 +2,5 @@ import { PATHS } from '../../src/app/_constants/paths'
 import { testPageMetadata } from '../support/test-utils'
 
 describe('Blog Page', function () {
-  testPageMetadata(PATHS.BLOG)
+  testPageMetadata(PATHS.BLOG, true)
 })
--- cypress/support/test-utils.ts
+++ cypress/support/test-utils.ts
@@ -3,38 +3,80 @@ import matter from 'gray-matter'
 import { PATHS, PathConfig } from '../../src/app/_constants/paths'
 import { BASE_URL } from '../../src/app/_constants/siteMetadata'
 
-export function testPageMetadata(path: PathConfig) {
+export function testPageMetadata(
+  path: PathConfig,
+  includesFeaturedPost: boolean = false,
+) {
   it(`should use the correct metadata from the markdown file`, function () {
     const filePath = `${path.mainContentPath}.md`
 
+    // Retrieve and process the main content's metadata
     cy.readFile(filePath).then((markdownContent: string) => {
-      const frontMatter = matter(markdownContent).data as {
-        header: { title: string; description: string }
+      const {
+        header,
+        seo,
+        featured_post: featuredPostSlug,
+      } = matter(markdownContent).data as {
+        header?: { title: string; description: string }
         seo: { title: string; description: string }
+        featured_post?: string
       }
 
-      const { header, seo } = frontMatter
-
+      // Visit the current page
       cy.visit(path.path)
 
-      if (path.path === PATHS.HOME.path) {
-        cy.title().should('eq', 'Filecoin Foundation')
-      } else {
-        cy.title().should('eq', `${seo.title} | Filecoin Foundation`)
+      // Check the page title
+      cy.title().should(
+        'eq',
+        path.path === PATHS.HOME.path
+          ? 'Filecoin Foundation'
+          : `${seo.title} | Filecoin Foundation`,
+      )
+
+      // Conditional logic for pages with a featured post
+      if (includesFeaturedPost && featuredPostSlug) {
+        handleFeaturedPost(path, featuredPostSlug)
+      } else if (header) {
+        verifyHeaderContent(header)
       }
 
-      cy.get('header')
-        .first()
-        .should('exist')
-        .within(() => {
-          cy.get('h1').should('have.text', header.title)
-
-          cy.get('p').should('have.text', header.description)
-        })
-
-      cy.get('link[rel="canonical"]')
-        .should('exist')
-        .should('have.attr', 'href', BASE_URL + path.path)
+      // Canonical link check, moved outside the conditionals to avoid repetition
+      cy.get('link[rel="canonical"]').should(
+        'have.attr',
+        'href',
+        `${BASE_URL}${path.path}`,
+      )
     })
   })
+}
+
+// Function to handle verification of featured post content
+function handleFeaturedPost(path: PathConfig, slug: string) {
+  const featuredPostContentPath = `${path.entriesContentPath}/${slug}`
+  const featuredPostFilePath = `${featuredPostContentPath}.md`
+
+  // Fetch and wrap the featured post's data for later use
+  cy.readFile(featuredPostFilePath).then((content: string) => {
+    const { data } = matter(content)
+    cy.wrap(data).as('featuredPostData')
+  })
+
+  cy.get('@featuredPostData').then((featuredPostData) => {
+    const post = featuredPostData as unknown as {
+      title: string
+      description: string
+    }
+    verifyHeaderContent(post)
+  })
+}
+
+// Function to verify the header's title and description
+function verifyHeaderContent(content: { title: string; description: string }) {
+  cy.get('header')
+    .first()
+    .should('exist')
+    .within(() => {
+      cy.get('h1').should('have.text', content.title)
+      cy.get('p').should('have.text', content.description)
+    })
 }
