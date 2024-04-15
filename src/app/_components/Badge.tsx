95112a6a70f4fd410dda75d1b704fa8cb27b82d5
Barbara Peric <b.peric10@gmail.com>
2024-04-15 15:45:03 +0100

    FIX: make prettier work

--- src/app/_components/Badge.tsx
+++ src/app/_components/Badge.tsx
@@ -1,27 +1,20 @@
 import clsx from 'clsx'
 
 type BadgeProps = {
-  variant?: 'primary' | 'ghost',
+  variant?: 'primary' | 'ghost'
   children: React.ReactNode
 }
 
 const variantStyles = {
-  primary:
-    'bg-brand-300 text-brand-800',
-  ghost:
-    'bg-transparent text-brand-100 border-current border',
+  primary: 'bg-brand-300 text-brand-800',
+  ghost: 'bg-transparent text-brand-100 border-current border',
 }
 
-export function Badge({ variant = "primary", children }: BadgeProps) {
-  
+export function Badge({ variant = 'primary', children }: BadgeProps) {
   const baseStyles =
     'inline-block max-w-fit rounded-lg font-semibold px-2 py-1 text-center text-xs'
 
   const className = clsx(baseStyles, variantStyles[variant])
 
-  return (
-      <span className={className}>
-        {children}
-      </span>
-    )
+  return <span className={className}>{children}</span>
 }
