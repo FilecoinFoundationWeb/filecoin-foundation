00a904ce98068d31c660ec7d3a7ed4251220681a
Charly Martin <charly@hey.com>
2024-04-15 10:14:03 +0100

    Add ScreenSizeIndicator component in dev env only

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { BreadCrumbsWrapper as BreadCrumbs } from '@/components/BreadCrumbsWrapp
 import { Footer } from '@/components/Footer'
 import { Navigation } from '@/components/Navigation'
 import { NetlifyIdentityManager } from '@/components/NetlifyIdentityManager'
+import { ScreenSizeIndicator } from '@/components/ScreenSizeIndicator'
 import { StructuredDataScript } from '@/components/StructuredDataScript'
 
 import { baseOrganizationSchema } from '@/utils/structuredData'
@@ -43,6 +44,7 @@ export default function RootLayout({
         </main>
 
         <Footer />
+        <ScreenSizeIndicator />
       </body>
     </html>
   )
