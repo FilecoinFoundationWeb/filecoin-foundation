5527d3fa5264ff3e42bc1b53605381ca98d4283a
Mirha Masala <26620750+mirhamasala@users.noreply.github.com>
2024-04-16 13:19:45 +0100

    CR (#64)

--- src/app/_components/PageHeader.tsx
+++ src/app/_components/PageHeader.tsx
@@ -38,34 +38,30 @@ export function PageHeader({
 }: PageHeaderProps) {
   return (
     <header className="flex flex-col gap-6 md:flex-row">
-      <div className="flex flex-col space-y-4 md:w-1/2">
+      <div className="flex flex-col gap-4 md:w-1/2">
         {isFeatured && <Badge>Featured</Badge>}
         <Heading tag="h1" variant="4xl" className="text-balance">
           {title}
         </Heading>
-        <div className="space-y-6">
-          {metaData && metaData.length > 0 && (
-            <span>
-              <Meta metaData={metaData} />
-            </span>
-          )}
-          <p>{description}</p>
-          <div className="flex flex-col gap-4 sm:flex-row sm:gap-6 md:flex-col md:gap-4">
-            <Button href={cta.href} variant="primary" className="flex-1">
-              {cta.text}
+        {metaData && metaData.length > 0 && (
+          <span className="mb-2">
+            <Meta metaData={metaData} />
+          </span>
+        )}
+        <p>{description}</p>
+
+        <div className="flex flex-col gap-4 sm:flex-row sm:gap-6 md:flex-col md:gap-4">
+          <Button href={cta.href} variant="primary" className="flex-1">
+            {cta.text}
+          </Button>
+          {secondaryCta && (
+            <Button href={secondaryCta.href} variant="ghost" className="flex-1">
+              {secondaryCta.text}
             </Button>
-            {secondaryCta && (
-              <Button
-                href={secondaryCta.href}
-                variant="ghost"
-                className="flex-1"
-              >
-                {secondaryCta.text}
-              </Button>
-            )}
-          </div>
+          )}
         </div>
       </div>
+
       {image && (
         <div className="relative h-32 w-full rounded-lg border border-brand-100 sm:h-60 md:h-auto md:w-1/2">
           <Image
