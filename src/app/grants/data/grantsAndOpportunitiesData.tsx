014c1a6635334268701e18a8a6c193ec10027aea
Barbara Peric <b.peric10@gmail.com>
2024-04-18 13:46:38 +0100

    FIX: move data in its own file

---
+++ src/app/grants/data/grantsAndOpportunitiesData.tsx
@@ -0,0 +1,22 @@
+import { Megaphone, Coins, Coin } from '@phosphor-icons/react/dist/ssr'
+
+export const grantsAndOpportunitiesData = [
+  {
+    title: 'Open Grants',
+    description:
+      'Open Grants support projects that improve Filecoin functionality and enhance network utility and directly contribute to the Foundation’s mission.',
+    Icon: Coins,
+  },
+  {
+    title: 'Microgrant',
+    description:
+      'Documentation enhancement microgrants are available to support the improvement of existing documentation or the creation of new resources related to the Filecoin network.',
+    Icon: Coin,
+  },
+  {
+    title: 'Requests for Proposal',
+    description:
+      'Request for Proposals (RFP) grants have clearly scoped deliverables, milestones, and funding limits. Refer to GitHub for any ongoing RFPs.',
+    Icon: Megaphone,
+  },
+]
