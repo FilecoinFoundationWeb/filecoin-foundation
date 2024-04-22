1ad2aa9608b234ad418488a6b78ff6f12a42715f
Barbara Peric <50910606+barbaraperic@users.noreply.github.com>
2024-04-22 08:03:34 +0100

    STYLE: change copy of grants page (#70)
    * STYLE: change copy of grants page
    
    * FIX: cypress test and seo description
    
    * FIX: PR comment
    
    * FIX: typescript

--- src/app/grants/page.tsx
+++ src/app/grants/page.tsx
@@ -30,7 +30,7 @@ export default function Grants() {
       <PageHeader
         title={header.title}
         description={header.description}
-        cta={{ href: FILECOIN_URLS.grants.email, text: 'Email for more info' }}
+        cta={{ href: FILECOIN_URLS.grants.email, text: 'Apply Now' }}
       />
 
       <section>
