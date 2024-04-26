6c8d451d44d318ad7d5953d64aedb343d09199cf
Barbara Peric <b.peric10@gmail.com>
2024-04-26 07:29:58 +0100

    FIX: use button component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,21 +1,15 @@
-import { baseButtonStyles, variantButtonStyles } from '@/components/Button'
-import { CustomLink } from '@/components/CustomLink'
+import { Button } from '@/components/Button'
 
 export default function NotFound() {
   return (
-    <div className="xs:items-center xs:justify-center m-auto flex max-w-xs flex-col gap-6">
+    <div className="xs:items-center xs:text-center m-auto flex max-w-xs flex-col gap-6">
       <div className="h-[104px]"></div>
       <span className="text-8xl">404</span>
       <h2 className="text-3xl">Page Not Found</h2>
       <p>
         We&apos;re sorry, but the page you were looking for could not be found
       </p>
-      <CustomLink
-        href="/"
-        className={`${baseButtonStyles} ${variantButtonStyles.primary}`}
-      >
-        Go to Home Page
-      </CustomLink>
+      <Button href="/">Go to Home Page</Button>
       <div className="h-[104px]"></div>
     </div>
   )
