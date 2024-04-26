1b7524d3a96014ff6139fc09710d37585b5efb42
Barbara Peric <b.peric10@gmail.com>
2024-04-26 07:16:17 +0100

    FIX: remove type

--- src/app/_components/BreadCrumbsWrapper.tsx
+++ src/app/_components/BreadCrumbsWrapper.tsx
@@ -7,7 +7,7 @@ import { BreadCrumbs } from '@/components/BreadCrumbs'
 import { PATHS } from '@/constants/paths'
 
 export function BreadCrumbsWrapper() {
-  const pathname = usePathname() as string
+  const pathname = usePathname()
   const allPaths = Object.values(PATHS).map((route) => route.path) as string[]
 
   if (pathname === PATHS.HOME.path) {
