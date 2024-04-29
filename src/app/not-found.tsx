8030b0564fc3559c73e1351b90ecf87a027e1c84
Barbara Peric <b.peric10@gmail.com>
2024-04-29 07:53:25 +0100

    FIX: PR comments

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,16 @@
 import { Button } from '@/components/Button'
 
+import { PATHS } from './_constants/paths'
+
 export default function NotFound() {
   return (
-    <div className="xs:items-center xs:text-center m-auto flex max-w-xs flex-col gap-6">
-      <div className="h-[104px]"></div>
+    <div className="m-auto flex max-w-xs flex-col gap-6 py-[104px] sm:items-center sm:text-center">
       <span className="text-8xl">404</span>
       <h2 className="text-3xl">Page Not Found</h2>
       <p>
         We&apos;re sorry, but the page you were looking for could not be found
       </p>
-      <Button href="/">Go to Home Page</Button>
-      <div className="h-[104px]"></div>
+      <Button href={PATHS.HOME.path}>Go to Home Page</Button>
     </div>
   )
 }
