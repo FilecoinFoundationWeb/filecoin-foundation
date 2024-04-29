03b8063c8e5c515fe6f952da1c66c9b04cdc8cb9
Barbara Peric <b.peric10@gmail.com>
2024-04-29 08:05:49 +0100

    FIX: padding

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ import { PATHS } from './_constants/paths'
 
 export default function NotFound() {
   return (
-    <div className="m-auto flex max-w-xs flex-col gap-6 py-[104px] sm:items-center sm:text-center">
+    <div className="m-auto flex max-w-xs flex-col gap-6 py-32 sm:items-center sm:text-center">
       <span className="text-8xl">404</span>
       <h2 className="text-3xl">Page Not Found</h2>
       <p>
