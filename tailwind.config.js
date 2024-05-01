56c7984abfaa248463761dd9e341c1bf55f99f9a
Charly Martin <charly@hey.com>
2024-05-01 12:45:20 +0100

    Design feedback

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,15 +23,44 @@ module.exports = {
             '--tw-prose-bold': theme('colors.brand.100'),
             '--tw-prose-links': theme('colors.brand.300'),
             '--tw-prose-quotes': theme('colors.brand.100'),
+            '--tw-prose-quote-borders': theme('colors.brand.300'),
             '--tw-prose-counters': theme('colors.brand.100'),
             '--tw-prose-bullets': theme('colors.brand.100'),
             '--tw-prose-code': theme('colors.brand.100'),
+            '--tw-prose-pre-bg': theme('colors.brand.700'),
+            '--tw-prose-pre-code': theme('colors.brand.300'),
 
             a: {
               textDecoration: 'none',
               '&:hover': {
                 textDecoration: 'underline',
               },
+            },
+
+            code: {
+              '&::before, &::after': {
+                content: 'none !important',
+              },
+            },
+
+            pre: {
+              borderWidth: '1px',
+              borderStyle: 'solid',
+              borderColor: theme('colors.brand.600'),
+            },
+
+            iframe: {
+              width: '100%',
+
+              '&::focus-visible': {
+                outline: 'none',
+              },
+            },
+
+            'iframe[src*="youtube.com"]': {
+              width: '100%',
+              height: 'auto',
+              aspectRatio: '16/9',
             },
           },
         },
