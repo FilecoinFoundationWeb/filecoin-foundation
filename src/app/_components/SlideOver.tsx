7f714e89474a43e38d4fe2906ca2c09e881b0a87
Charly Martin <charly@hey.com>
2024-05-06 16:00:48 +0100

    Add generic slide-over component

---
+++ src/app/_components/SlideOver.tsx
@@ -0,0 +1,51 @@
+import { Fragment } from 'react'
+
+import { Dialog, Transition } from '@headlessui/react'
+
+interface SlideOverProps {
+  open: boolean
+  setOpen: (open: boolean) => void
+  children: React.ReactNode
+}
+
+export function SlideOver({ open, setOpen, children }: SlideOverProps) {
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog className="relative z-10" onClose={setOpen}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-in-out duration-500 sm:duration-700"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in-out duration-500 sm:duration-700"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 backdrop-blur-lg" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-hidden">
+          <div className="absolute inset-0 overflow-hidden">
+            <div className="pointer-events-none fixed inset-y-0 right-0 flex w-full max-w-[480px]">
+              <Transition.Child
+                as={Fragment}
+                enter="transform transition ease-in-out duration-500 sm:duration-700"
+                enterFrom="translate-x-full"
+                enterTo="translate-x-0"
+                leave="transform transition ease-in-out duration-500 sm:duration-700"
+                leaveFrom="translate-x-0"
+                leaveTo="translate-x-full"
+              >
+                <Dialog.Panel className="pointer-events-auto w-full">
+                  <div className="flex h-full flex-col overflow-y-scroll bg-brand-800">
+                    {children}
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  )
+}
