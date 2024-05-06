b3872edab582041d74cd51e721eee4c65f835647
Mirha Masala <mirha@hey.com>
2024-05-06 11:20:05 +0100

    Include PageHeader

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,14 +1,12 @@
-import Image from 'next/image'
-
 import { BlogPosting, WithContext } from 'schema-dts'
 
-import { Heading } from '@/components/Heading'
 import { MarkdownContent } from '@/components/MarkdownContent'
+import { PageHeader } from '@/components/PageHeader'
+import { PageLayout } from '@/components/PageLayout'
 import { StructuredDataScript } from '@/components/StructuredDataScript'
 
 import { BlogPostData } from '@/types/blogPostTypes'
 
-import { formatDate } from '@/utils/formatDate'
 import { generateDynamicContentMetadata } from '@/utils/generateDynamicContentMetadata'
 import { getBlogPostData } from '@/utils/getBlogPostData'
 import { baseOrganizationSchema } from '@/utils/structuredData'
@@ -33,7 +31,7 @@ export async function generateMetadata({ params }: BlogPostProps) {
 }
 
 function createBlogPostStructuredData(
-  data: BlogPostData
+  data: BlogPostData,
 ): WithContext<BlogPosting> {
   const { title, description, image, publishedOn, updatedOn, slug } = data
 
@@ -62,31 +60,15 @@ function createBlogPostStructuredData(
 export default function BlogPost({ params }: BlogPostProps) {
   const { slug } = params
   const data = getBlogPostData(slug)
-  const { title, publishedOn, image, content } = data
+  const { title, description, image, content } = data
 
   return (
-    <>
+    <PageLayout>
       <StructuredDataScript
         structuredData={createBlogPostStructuredData(data)}
       />
-      <header>
-        {publishedOn && (
-          <span className="block">{formatDate(publishedOn, 'blog')}</span>
-        )}
-        <Heading tag="h1" variant="2xl">
-          {title}
-        </Heading>
-        <Image
-          priority
-          src={image.url}
-          alt={image.alt}
-          width={770}
-          height={440}
-          className="block h-auto object-contain"
-        />
-      </header>
-
+      <PageHeader title={title} description={description} image={image} />
       {content && <MarkdownContent>{content}</MarkdownContent>}
-    </>
+    </PageLayout>
   )
 }
