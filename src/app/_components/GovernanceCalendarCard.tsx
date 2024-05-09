9c7e69b11ce27459cc3135bd03c644a4523749d8
Barbara Peric <b.peric10@gmail.com>
2024-05-09 13:12:54 +0100

    FIX: PR comments and STYLE: new Figma design

--- src/app/_components/GovernanceCalendarCard.tsx
+++ src/app/_components/GovernanceCalendarCard.tsx
@@ -1,9 +1,10 @@
-import { Clock, CalendarBlank } from '@phosphor-icons/react/dist/ssr'
+import { Clock, CalendarPlus } from '@phosphor-icons/react/dist/ssr'
 
 import { Heading } from '@/components/Heading'
 
 import { addMinutesToISO, extractTimeFromISO } from '@/utils/convertISOTime'
 
+import { Icon, IconProps } from './Icon'
 import { TextLink } from './TextLink'
 
 export type GovernanceCalendarCardProps = {
@@ -24,13 +25,36 @@ type CalendarProps = {
 function Calendar({ startDate }: CalendarProps) {
   const { day, month } = extractTimeFromISO(startDate)
   return (
-    <div className="min-w-xs grid grid-rows-[_40px,_96px] rounded-md border border-blue-400 bg-blue-500 sm:min-w-32">
-      <span className="flex items-center justify-center rounded-tl-[5px] rounded-tr-[5px] text-base font-normal uppercase">
+    <div className="grid min-h-40 min-w-36 grid-rows-[_40px,auto] rounded-md border border-blue-400 bg-blue-500">
+      <span className="flex items-center justify-center rounded-t text-base font-normal uppercase">
         {month}
       </span>
-      <span className="flex items-center justify-center rounded-bl-[5px]  rounded-br-[5px] bg-blue-100 text-5xl font-bold text-blue-400">
+      <span className="flex flex-1 items-center justify-center rounded-b bg-blue-100 text-5xl font-bold text-blue-400">
         {day}
       </span>
+    </div>
+  )
+}
+
+function OutlineLabel({
+  text,
+  borderColor = 'blue-100',
+  icon,
+}: {
+  text: string
+  borderColor?: 'blue-100' | 'blue-500'
+  icon?: IconProps['component']
+}) {
+  const borderStyles = {
+    'blue-100': 'border-blue-100',
+    'blue-500': 'border-blue-500',
+  }
+  return (
+    <div
+      className={`flex w-max items-center gap-1 rounded-lg border ${borderStyles[borderColor]} px-2 py-1 text-xs`}
+    >
+      {icon && <Icon component={icon} size={16} />}
+      <span className="-m-px">{text}</span>
     </div>
   )
 }
@@ -40,7 +64,6 @@ export function GovernanceCalendarCard({
   startDate,
   durationInMinutes,
   cta,
-  as: Tag = 'li',
 }: GovernanceCalendarCardProps) {
   const { time } = extractTimeFromISO(startDate)
   const endTime = addMinutesToISO(startDate, durationInMinutes)
@@ -50,27 +73,24 @@ export function GovernanceCalendarCard({
   }
 
   return (
-    <Tag className="flex flex-col gap-6 rounded-md border border-blue-500 p-4 sm:flex-row">
+    <li className="flex flex-col rounded-md border border-blue-500 p-1 sm:flex-row">
       <Calendar startDate={startDate} />
 
-      <div className="flex flex-1 flex-col items-start justify-between gap-6">
+      <div className="flex flex-1 flex-col items-start justify-between gap-6 p-4">
         <div className="flex flex-col gap-3">
+          <div className="flex gap-2">
+            <OutlineLabel text={` UTC ${time} - ${endTime}`} icon={Clock} />
+            <OutlineLabel text="Zoom" borderColor="blue-500" />
+          </div>
           <Heading tag="h3" variant="lg">
             {title}
           </Heading>
-
-          <div className="flex w-max items-center gap-1 rounded-lg border border-blue-100 px-1 py-[2px] text-xs uppercase">
-            <Clock size={13} />
-            <span className="mb-[-1px]">
-              UTC {time} - {endTime}
-            </span>
-          </div>
         </div>
-        <TextLink className="flex gap-1 text-base" href={cta.url}>
-          <CalendarBlank size={24} />
+        <TextLink className="flex gap-2 text-base" href={cta.url}>
+          <CalendarPlus size={24} />
           {cta.text}
         </TextLink>
       </div>
-    </Tag>
+    </li>
   )
 }
