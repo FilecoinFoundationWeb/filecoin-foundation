e3c2c0cd4d1faf4099ab9e065a7b34315bb04c78
Mirha Masala <mirha@hey.com>
2024-05-14 21:06:40 +0100

    Remove unnecessary style

--- src/app/_components/Badge.tsx
+++ src/app/_components/Badge.tsx
@@ -12,7 +12,7 @@ const borderStyles = {
 
 export function Badge({ borderColor = 'brand-500', children }: BadgeProps) {
   const baseStyles =
-    'inline-block max-w-fit rounded-lg border bg-brand-800 px-2 py-1 text-center text-xs font-semibold capitalize text-brand-100'
+    'inline-block max-w-fit rounded-lg border bg-brand-800 px-2 py-1 text-xs font-semibold capitalize text-brand-100'
 
   return (
     <span className={clsx(baseStyles, borderStyles[borderColor])}>
