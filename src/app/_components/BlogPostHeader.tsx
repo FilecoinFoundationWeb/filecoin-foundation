0a74336fcac2eb0da537b06dea6f0b84cc571c48
Mirha Masala <mirha@hey.com>
2024-05-14 14:18:33 +0100

    Default to variant

--- src/app/_components/BlogPostHeader.tsx
+++ src/app/_components/BlogPostHeader.tsx
@@ -21,7 +21,7 @@ export function BlogPostHeader({
   return (
     <header className="space-y-6">
       <div className="space-y-4">
-        {category && <Badge variant="secondary">{category}</Badge>}
+        {category && <Badge>{category}</Badge>}
         <Heading tag="h1" variant="4xl">
           {title}
         </Heading>
