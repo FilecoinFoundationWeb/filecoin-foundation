43af88a453da175f131a0c07884cc21351a4c544
Mirha Masala <mirha@hey.com>
2024-05-15 07:41:08 +0100

    CR - Separate data from presentation

--- src/app/_components/DesktopNavigation.tsx
+++ src/app/_components/DesktopNavigation.tsx
@@ -13,51 +13,7 @@ import { NavigationPopover } from '@/components/NavigationPopover'
 import { isInternalLink, isExternalLink } from '@/utils/linkUtils'
 
 import { PATHS } from '@/constants/paths'
-import { FILECOIN_DOCS_URLS } from '@/constants/siteMetadata'
-
-const getInvolvedItems = [
-  {
-    label: PATHS.EVENTS.label,
-    href: PATHS.EVENTS.path,
-    description: 'Overview of upcoming and past events',
-  },
-  {
-    label: PATHS.GRANTS.label,
-    href: PATHS.GRANTS.path,
-    description:
-      'Information on funding opportunities supporting projects that contribute to the growth of the Filecoin Network',
-  },
-  {
-    label: 'Become a Storage Provider',
-    href: 'https://destor.com/destor-network/overview',
-    description: 'Dive right in and become an essential part of the ecosystem',
-  },
-  {
-    label: 'Contribute to the Filecoin Project',
-    href: FILECOIN_DOCS_URLS.waysToContribute,
-    description:
-      'Shape the future of Filecoin by contributing to its code, research, or docs.',
-  },
-]
-
-const communityItems = [
-  {
-    label: PATHS.ECOSYSTEM.label,
-    href: PATHS.ECOSYSTEM.path,
-    description: 'Learn about various ecosystem projects or submit your own',
-  },
-  {
-    label: PATHS.GOVERNANCE.label,
-    href: PATHS.GOVERNANCE.path,
-    description:
-      'Learn about Filecoin Improvement Proposals and other governance mechanisms',
-  },
-]
-
-const learnMoreItem = {
-  label: 'Learn more about Filecoin',
-  href: FILECOIN_DOCS_URLS.site,
-}
+import { desktopNavigationItems } from '@/data/components/navigationData'
 
 type SubLinkProps = {
   label: string | Route
@@ -70,6 +26,9 @@ type InternalLinkProps = {
   href: Route
   isActive?: boolean
 }
+
+const { getInvolvedItems, communityItems, learnMoreItem } =
+  desktopNavigationItems
 
 function InternalSubLink({ label, description, href }: SubLinkProps) {
   return (
