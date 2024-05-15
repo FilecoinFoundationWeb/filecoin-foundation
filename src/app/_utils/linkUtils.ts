4e5ec7708eb8035c129ac4a09582e72adf48821a
Charly Martin <charly@hey.com>
2024-05-15 11:56:26 +0100

    fixup! CR: Refactor external internal link check

--- src/app/_utils/linkUtils.ts
+++ src/app/_utils/linkUtils.ts
@@ -2,8 +2,8 @@ import { Route } from 'next'
 
 type Link = string | Route
 
-export function isExternalLink(href: Link) {
-  return href.startsWith('http://') || href.startsWith('https://')
+export function isExternalLink(href: string) {
+  return !isInternalLink(href)
 }
 
 export function isInternalLink(href: Link) {
