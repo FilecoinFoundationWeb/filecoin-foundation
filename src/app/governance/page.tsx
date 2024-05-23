bd3034be01f83ee1e38e2bb8b7fbe55025d9e429
Mirha Masala <mirha@hey.com>
2024-05-23 16:38:15 +0100

    CR: Refactor to use constant

--- src/app/governance/page.tsx
+++ src/app/governance/page.tsx
@@ -35,7 +35,7 @@ export default function Governance() {
         title={header.title}
         description={header.description}
         cta={{
-          href: '#',
+          href: FILECOIN_FOUNDATION_URLS.governance.docs,
           text: 'Learn More',
         }}
       />
