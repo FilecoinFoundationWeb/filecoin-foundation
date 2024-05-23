91ee9f65cf7b24a1570cee39f3043d1b297bd573
Charly Martin <charly@hey.com>
2024-05-23 11:12:36 +0200

    CR: const to function

--- src/app/_components/ListboxButton.tsx
+++ src/app/_components/ListboxButton.tsx
@@ -8,17 +8,19 @@ type ListboxButtonProps = {
   children: React.ReactNode
 }
 
-export const ListboxButton: React.FC<ListboxButtonProps> = ({
+export function ListboxButton({
   ariaLabel,
   open,
   children,
-}) => (
-  <Listbox.Button
-    aria-haspopup="listbox"
-    aria-expanded={open}
-    aria-label={ariaLabel}
-    className="border-1 inline-flex w-full items-center justify-between gap-2 rounded-lg border border-brand-300 p-3 text-brand-300 hover:border-current hover:text-brand-400 focus:outline-2 focus:outline-brand-100 md:min-w-40"
-  >
-    {children}
-  </Listbox.Button>
-)
+}: ListboxButtonProps) {
+  return (
+    <Listbox.Button
+      aria-haspopup="listbox"
+      aria-expanded={open}
+      aria-label={ariaLabel}
+      className="border-1 inline-flex w-full items-center justify-between gap-2 rounded-lg border border-brand-300 p-3 text-brand-300 hover:border-current hover:text-brand-400 focus:outline-2 focus:outline-brand-100 md:min-w-40"
+    >
+      {children}
+    </Listbox.Button>
+  )
+}
