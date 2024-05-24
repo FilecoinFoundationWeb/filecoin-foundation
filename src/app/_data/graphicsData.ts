58c72b75a7cee76ea03c4de4f7fe196678eb61b0
Charly Martin <charly@hey.com>
2024-05-24 12:26:14 +0200

    Import graphics

---
+++ src/app/_data/graphicsData.ts
@@ -0,0 +1,68 @@
+import about from '@graphics/Filorg_About.png'
+import ecosystem from '@graphics/Filorg_Ecosystem_Explorer.png'
+import events1 from '@graphics/Filorg_Events1.png'
+import events2 from '@graphics/Filorg_Events2.png'
+import events3 from '@graphics/Filorg_Events3.png'
+import governance1 from '@graphics/Filorg_Governance1.png'
+import governance2 from '@graphics/Filorg_Governance2.png'
+import governance3 from '@graphics/Filorg_Governance3.png'
+import grants from '@graphics/Filorg_Grants.png'
+import home from '@graphics/Filorg_Home.png'
+import imageComingSoon from '@graphics/Filorg_ImageComingSoon.png'
+import logoComingSoon from '@graphics/Filorg_LogoComingSoon.png'
+import rAndD from '@graphics/Filorg_RandD.png'
+
+export const graphicsData = {
+  home: {
+    src: home,
+    alt: 'A grand library with modern server racks and a large network of spheres above, symbolizing data and technology.',
+  },
+  about: {
+    src: about,
+    alt: 'A digital rendering of the Earth with interconnected points, highlighting global connectivity and communication.',
+  },
+  ecosystem: {
+    src: ecosystem,
+    alt: 'Magnifying glass focusing on a network of interconnected blue and white spheres, symbolizing exploration and analysis of connections.',
+  },
+  events1: {
+    src: events1,
+    alt: 'Auditorium with rows of seats facing a stage displaying a digital Earth image, surrounded by modern lighting and panels.',
+  },
+  events2: {
+    src: events2,
+    alt: 'Large event space with people gathered around tables for networking, many wearing yellow backpacks, against a bright blue backdrop.',
+  },
+  events3: {
+    src: events3,
+    alt: 'Speaker presenting to a full audience in a dark auditorium, with attendees attentively listening and engaged in the presentation.',
+  },
+  governance1: {
+    src: governance1,
+    alt: 'Interconnected network of blue and white spheres with icons of people, representing a social network or digital communication.',
+  },
+  governance2: {
+    src: governance2,
+    alt: 'Digital illustration of a futuristic bridge construction with cranes and vehicles on a circuit board-like surface, symbolizing advanced infrastructure.',
+  },
+  governance3: {
+    src: governance3,
+    alt: 'Modern conference room with people gathered around a circular table, discussing important issues, illuminated by natural light.',
+  },
+  grants: {
+    src: grants,
+    alt: 'Futuristic garden with blue plants and a waterfall, set in a modern architectural space with columns and curved structures.',
+  },
+  rAndD: {
+    src: rAndD,
+    alt: 'A high-tech R&D lab with workstations and a large spherical network structure above a central platform.',
+  },
+  imageComingSoon: {
+    src: imageComingSoon,
+    alt: 'Image coming soon',
+  },
+  logoComingSoon: {
+    src: logoComingSoon,
+    alt: 'Logo coming soon',
+  },
+}
