6d6afb527237eb1fd7b601ed460248132a17fa19
Mirha Masala <mirha@hey.com>
2024-05-27 14:21:35 +0100

    Refactor styles

--- src/app/_components/MobileNavigation.tsx
+++ src/app/_components/MobileNavigation.tsx
@@ -19,11 +19,6 @@ import { PATHS } from '@/constants/paths'
 const getInvolvedItems = [PATHS.EVENTS, PATHS.GRANTS]
 const communityItems = [PATHS.ECOSYSTEM, PATHS.GOVERNANCE]
 
-const linkTouchTarget = {
-  class: 'inline-block px-5 py-2',
-  offsetClass: '-ml-5',
-}
-
 type MobileLinkProps = {
   label: string
   path: Route
@@ -33,10 +28,10 @@ type MobileLinkProps = {
 
 function MobileLink({ label, path, nested, setOpen }: MobileLinkProps) {
   return (
-    <li className={clsx(!nested && linkTouchTarget.offsetClass)}>
+    <li>
       <Link
         href={path}
-        className={clsx(linkBaseStyles, linkTouchTarget.class)}
+        className={clsx(linkBaseStyles, nested && 'ml-6')}
         onClick={() => setOpen(false)}
       >
         {label}
@@ -77,65 +72,68 @@ export function MobileNavigation() {
       />
 
       <SlideOver open={open} setOpen={setOpen}>
-        <div className="flex items-center justify-between px-6 pb-12 pt-8 sm:pb-16">
-          <Link
-            className="flex-shrink-0 outline-white focus:outline-2"
-            href={PATHS.HOME.path}
-            aria-label="Go to homepage"
-            onClick={() => setOpen(false)}
-          >
-            <Logo />
-          </Link>
+        <div className="flex flex-col gap-12 px-6 py-8">
+          <div className="flex items-center justify-between">
+            <Link
+              className="flex-shrink-0 outline-white focus:outline-2"
+              href={PATHS.HOME.path}
+              aria-label="Go to homepage"
+              onClick={() => setOpen(false)}
+            >
+              <Logo />
+            </Link>
 
-          <IconButton
-            icon={X}
-            label="Close mobile navigation"
-            onClick={() => setOpen(false)}
-          />
-        </div>
+            <IconButton
+              icon={X}
+              label="Close mobile navigation"
+              onClick={() => setOpen(false)}
+            />
+          </div>
 
-        <ul className="px-6 pb-12 sm:pb-16" aria-label="Navigation options">
-          <MobileLink
-            label={PATHS.ABOUT.label}
-            path={PATHS.ABOUT.path}
-            setOpen={setOpen}
-          />
-
-          <li className="bottom-sideline">
-            <p className="my-2 text-brand-200">Get Involved</p>
-          </li>
-
-          {getInvolvedItems.map((item) => (
+          <ul className="space-y-6" aria-label="Navigation options">
             <MobileLink
-              key={item.path}
-              nested
-              label={item.label}
-              path={item.path}
+              label={PATHS.ABOUT.label}
+              path={PATHS.ABOUT.path}
               setOpen={setOpen}
             />
-          ))}
 
-          <li className="bottom-sideline">
-            <p className="my-2 text-brand-200">Community</p>
-          </li>
-          {communityItems.map((item) => (
+            <li className="text-brand-200">
+              <span className="mb-4 block">Get Involved</span>
+              <ul className="space-y-6 border-l">
+                {getInvolvedItems.map((item) => (
+                  <MobileLink
+                    key={item.path}
+                    nested
+                    label={item.label}
+                    path={item.path}
+                    setOpen={setOpen}
+                  />
+                ))}
+              </ul>
+            </li>
+
+            <li className="text-brand-200">
+              <span className="mb-4 block">Community</span>
+              <ul className="space-y-4 border-l">
+                {communityItems.map((item) => (
+                  <MobileLink
+                    key={item.path}
+                    nested
+                    label={item.label}
+                    path={item.path}
+                    setOpen={setOpen}
+                  />
+                ))}
+              </ul>
+            </li>
+
             <MobileLink
-              key={item.path}
-              nested
-              label={item.label}
-              path={item.path}
+              label={PATHS.BLOG.label}
+              path={PATHS.BLOG.path}
               setOpen={setOpen}
             />
-          ))}
+          </ul>
 
-          <MobileLink
-            label={PATHS.BLOG.label}
-            path={PATHS.BLOG.path}
-            setOpen={setOpen}
-          />
-        </ul>
-
-        <div className="px-6">
           <Social />
         </div>
       </SlideOver>
