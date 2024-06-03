e5677edad14386af47020838f3aaf9f155367cec
Barbara Peric <b.peric10@gmail.com>
2024-06-03 10:37:33 +0100

    FIX: syntax

--- cypress/e2e/terms_page_spec.cy.ts
+++ cypress/e2e/terms_page_spec.cy.ts
@@ -1,6 +1,6 @@
 import { PATHS } from '../../src/app/_constants/paths'
 import { testPageMetadata } from '../support/test-utils'
 
-describe('Terms Page', () => {
+describe('Terms Page', function () {
   testPageMetadata(PATHS.TERMS, false, false)
 })
