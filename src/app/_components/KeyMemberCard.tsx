80e7b9ed1e015036518ec74d866e6f1018a8b44f
Charly Martin <charly@hey.com>
2024-06-03 16:36:20 +0100

    fixup! CR: Make KeyMemberCard clickable

--- src/app/_components/KeyMemberCard.tsx
+++ src/app/_components/KeyMemberCard.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
-import Link from 'next/link'
 
 import { LinkedinLogo } from '@phosphor-icons/react/dist/ssr'
 
 import { Heading } from '@/components/Heading'
 import { Icon } from '@/components/Icon'
+import { TextLink } from '@/components/TextLink'
 
 import type { MemberData } from '@/types/memberType'
 
@@ -17,34 +17,32 @@ export function KeyMemberCard({
   image,
 }: KeyMemberCardProps) {
   return (
-    <li aria-label={`${name}'s profile.`}>
-      <Link
-        href={linkedin}
-        aria-label={`Visit ${name}'s LinkedIn profile.`}
-        className="flex rounded-lg border border-brand-500 p-1 "
-      >
-        <Image
-          src={image}
-          alt={`Photo of ${name}`}
-          sizes="10rem"
-          className="aspect-[3/4] w-32 rounded object-cover"
-        />
+    <li className="relative flex rounded-lg border border-brand-500 p-1">
+      <Image
+        src={image}
+        alt={`Photo of ${name}`}
+        sizes="10rem"
+        className="aspect-[3/4] w-32 rounded object-cover"
+      />
 
-        <div className="m-3 flex grow flex-col justify-between">
-          <div>
-            <Heading tag="h3" variant="lg">
-              {name}
-            </Heading>
+      <div className="m-3 grow">
+        <Heading tag="h3" variant="lg">
+          {name}
+        </Heading>
 
-            <p className="mt-1 text-brand-300">{title}</p>
-          </div>
+        <p className="mt-1 text-brand-300">{title}</p>
 
-          <span className="flex items-center gap-2 text-brand-300">
+        <TextLink
+          href={linkedin}
+          aria-label={`Visit ${name}'s LinkedIn profile.`}
+          className="absolute inset-0 rounded-lg focus:outline focus:outline-2 focus:outline-brand-100"
+        >
+          <span className="absolute bottom-4 left-36 flex items-center gap-2 text-brand-300">
             <Icon component={LinkedinLogo} />
             <span>LinkedIn</span>
           </span>
-        </div>
-      </Link>
+        </TextLink>
+      </div>
     </li>
   )
 }
