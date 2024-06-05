7ddbef6c1749b9ec14cb63012c75d3b2fd770c6b
Mirha Masala <mirha@hey.com>
2024-06-05 20:33:34 +0100

    Add Employee Privacy Policy Page

---
+++ src/app/employee-privacy-policy/page.tsx
@@ -0,0 +1,36 @@
+import { PageHeader } from '@/components/PageHeader'
+import { StructuredDataScript } from '@/components/StructuredDataScript'
+
+import { createMetadata } from '@/utils/createMetadata'
+import { generateWebPageStructuredData } from '@/utils/structuredData'
+
+import {
+  attributes,
+  react as Content,
+} from '@/content/pages/employee-privacy-policy.md'
+
+import { PATHS } from '@/constants/paths'
+
+const { header, seo } = attributes
+
+export const metadata = createMetadata({ seo, path: PATHS.PRIVACY_POLICY.path })
+
+const policyPageBaseData = generateWebPageStructuredData({
+  title: seo.title,
+  description: seo.description,
+  path: PATHS.EMPLOYEE_PRIVACY_POLICY.path,
+})
+
+export default function EmployeePrivacyPolicy() {
+  const { title } = header
+
+  return (
+    <article>
+      <StructuredDataScript structuredData={policyPageBaseData} />
+      <PageHeader.Title>{title}</PageHeader.Title>
+      <section className="prose mt-6">
+        <Content />
+      </section>
+    </article>
+  )
+}
