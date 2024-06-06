90c2992d33a529a440898bb5c1c65b6ad8f6e8c1
Mirha Masala <mirha@hey.com>
2024-06-06 15:12:36 +0100

    CR

--- cypress/support/canonicalLinkUtil.ts
+++ cypress/support/canonicalLinkUtil.ts
@@ -1,6 +1,6 @@
 import { BASE_URL } from '../../src/app/_constants/siteMetadata'
 
-export function verifyCanonicalLink(path) {
+export function verifyCanonicalLink(path: string) {
   const expectedCanonicalLink = `${BASE_URL}${path}`
   cy.get('link[rel="canonical"]')
     .should('exist')
