d084e99b415d05a762a580a24fc29bc4c3585fa1
Barbara Peric <b.peric10@gmail.com>
2024-06-06 13:53:38 +0100

    set up sentry

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -3,7 +3,7 @@ import * as Sentry from '@sentry/nextjs'
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 0.05,
 })
