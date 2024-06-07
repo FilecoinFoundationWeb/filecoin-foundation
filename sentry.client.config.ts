3b81ccdf244fd8dfcb3f3c6cb7bb8850ead54e91
Barbara Peric <b.peric10@gmail.com>
2024-06-07 16:34:57 +0100

    FIX: set env in sentry config

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -18,4 +18,5 @@ Sentry.init({
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
   replaysSessionSampleRate: 0.1,
+  environment: process.env.NODE_ENV || 'production',
 })
