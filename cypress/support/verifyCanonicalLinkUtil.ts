8c2efba5f94e2ca6caa790ce2afbf38aa7007d45
Mirha Masala <mirha@hey.com>
2024-06-07 09:08:12 +0100

    CR: Fix verifyCanonicalLink test util

--- cypress/support/verifyCanonicalLinkUtil.ts
+++ cypress/support/verifyCanonicalLinkUtil.ts
@@ -1,7 +1,8 @@
 import { BASE_URL } from '../../src/app/_constants/siteMetadata'
 
 export function verifyCanonicalLink(path: string) {
-  const expectedCanonicalLink = `${BASE_URL}${path}`
+  const expectedCanonicalLink = path === '/' ? BASE_URL : `${BASE_URL}${path}`
+
   cy.get('link[rel="canonical"]')
     .should('exist')
     .should('have.attr', 'href', expectedCanonicalLink)
