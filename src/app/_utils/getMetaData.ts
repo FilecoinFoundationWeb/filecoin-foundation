864299d474fd55e8b6112ca8deb19845ad472dbe
Barbara Peric <50910606+barbaraperic@users.noreply.github.com>
2024-06-07 12:24:37 +0100

    Event Post: style header (#198)
    * FIX: style event post header
    
    * CR
    
    ---------
    
    Co-authored-by: Mirha Masala <26620750+mirhamasala@users.noreply.github.com>
    Co-authored-by: Charly Martin <charly@hey.com>

--- src/app/_utils/getMetaData.ts
+++ src/app/_utils/getMetaData.ts
@@ -1,7 +1,31 @@
 import { BlogPostData } from '@/types/blogPostTypes'
+import { EventData } from '@/types/eventTypes'
 
 import { formatDate } from './formatDate'
 
-export function getMetaData(publishedOn?: BlogPostData['publishedOn']) {
+export function getBlogPostMetaData(publishedOn?: BlogPostData['publishedOn']) {
   return publishedOn ? [formatDate(publishedOn)] : []
+}
+
+export function getEventMetaData(event: EventData) {
+  if (!event.startDate && !event.location) {
+    return []
+  }
+
+  const { startDate, endDate, location } = event
+
+  const formattedStartDate = formatDate(startDate)
+  const formattedEndDate = endDate ? formatDate(endDate) : null
+
+  const metaDataContent = [formattedStartDate]
+
+  if (formattedEndDate) {
+    metaDataContent[0] += ` - ${formattedEndDate}`
+  }
+
+  if (location) {
+    metaDataContent.push(location)
+  }
+
+  return metaDataContent
 }
