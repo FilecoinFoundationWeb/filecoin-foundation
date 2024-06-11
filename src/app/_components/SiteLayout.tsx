284d55636eb1b9589a9b67ccea0de74c69ee3214
Mirha Masala <26620750+mirhamasala@users.noreply.github.com>
2024-06-11 14:02:27 +0100

    Revert "Set Main Font Family to Archivo (#219)"
    This reverts commit 11ad1eaea69efdd094adf7185a2ec2d865c023cb.

--- src/app/_components/SiteLayout.tsx
+++ src/app/_components/SiteLayout.tsx
@@ -1,5 +1,3 @@
-import { Archivo } from 'next/font/google'
-
 import { BreakpointDebugger } from '@/components/_dev_BreakpointDebugger'
 import { Footer } from '@/components/Footer'
 import { Navigation } from '@/components/Navigation'
@@ -7,16 +5,10 @@ import { NetlifyIdentityManager } from '@/components/NetlifyIdentityManager'
 
 import { LayoutProps } from '@/layout'
 
-const archivo = Archivo({
-  weight: ['300', '400', '500', '600', '700'],
-  subsets: ['latin'],
-  display: 'swap',
-})
-
 export function SiteLayout({ children }: LayoutProps) {
   return (
-    <html lang="en" className={archivo.className}>
-      <body className="m-auto flex max-w-[1032px] flex-col justify-between bg-brand-800 px-6 pb-6 pt-8 tracking-wide text-brand-100">
+    <html lang="en">
+      <body className="m-auto flex max-w-[1032px] flex-col justify-between bg-brand-800 px-6 pb-6 pt-8 text-brand-100">
         <NetlifyIdentityManager />
 
         <Navigation />
