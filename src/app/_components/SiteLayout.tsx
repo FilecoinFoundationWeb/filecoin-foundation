4e679bbd3b6a87226cf265a00db11678fc451056
Mirha Masala <mirha@hey.com>
2024-06-11 08:08:56 +0100

    Increase letter spacing globally

--- src/app/_components/SiteLayout.tsx
+++ src/app/_components/SiteLayout.tsx
@@ -16,7 +16,7 @@ const archivo = Archivo({
 export function SiteLayout({ children }: LayoutProps) {
   return (
     <html lang="en" className={archivo.className}>
-      <body className="m-auto flex max-w-[1032px] flex-col justify-between bg-brand-800 px-6 pb-6 pt-8 text-brand-100">
+      <body className="m-auto flex max-w-[1032px] flex-col justify-between bg-brand-800 px-6 pb-6 pt-8 tracking-wide text-brand-100">
         <NetlifyIdentityManager />
 
         <Navigation />
