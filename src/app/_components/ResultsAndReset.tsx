2cd2e63722dbd22a2d6f95e3b54f118679b0c52c
Charly Martin <charly@hey.com>
2024-06-19 15:57:33 +0100

    Add space between reset button and categories

--- src/app/_components/ResultsAndReset.tsx
+++ src/app/_components/ResultsAndReset.tsx
@@ -10,17 +10,17 @@ export function ResultsAndReset({ results }: ResultsAndResetProps) {
   const { resetSearchParams } = useUpdateSearchParams()
 
   return (
-    <div className="flex items-baseline lg:gap-8">
-      <span className="inline-flex flex-1 items-center gap-2">
-        <span>Results</span>
-        {results && (
-          <span className="grid h-5 w-8 place-content-center rounded-[42px] bg-brand-300 p-1 text-xs font-bold text-brand-800">
+    <div className="flex items-baseline lg:mb-5 lg:gap-8">
+      {results && (
+        <span className="inline-flex flex-1 items-center gap-2">
+          <span>Results</span>
+          <span className="grid h-5 w-8 place-content-center rounded-full bg-brand-300 p-1 text-xs font-bold text-brand-800">
             {results}
           </span>
-        )}
-      </span>
+        </span>
+      )}
       <button
-        className="focus:brand-outline inline-flex whitespace-nowrap font-bold text-brand-300 underline"
+        className="inline-flex whitespace-nowrap font-bold text-brand-300 underline focus:brand-outline"
         onClick={resetSearchParams}
       >
         Reset Filters
