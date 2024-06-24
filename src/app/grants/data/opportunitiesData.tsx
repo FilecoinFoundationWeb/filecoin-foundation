78bb24f63fd9e2a54b63c86605cca3b3c9d6c310
Barbara Peric <50910606+barbaraperic@users.noreply.github.com>
2024-06-24 11:40:40 +0100

    Grants Page: add links to cards (#245)
    * FEAT: add links to cards in grant page
    
    * FIX: link import
    
    * FIX: github link
    
    * FIX: make all cards clickable and include icons
    
    * PR fixes

--- src/app/grants/data/opportunitiesData.tsx
+++ src/app/grants/data/opportunitiesData.tsx
@@ -1,22 +1,44 @@
-import { Megaphone, Coins, Coin } from '@phosphor-icons/react/dist/ssr'
+import {
+  Megaphone,
+  Coins,
+  Coin,
+  ArrowUpRight,
+} from '@phosphor-icons/react/dist/ssr'
+
+import { FILECOIN_FOUNDATION_URLS } from '@/constants/siteMetadata'
 
 export const opportunitiesData = [
   {
     title: 'Open Grants',
     description:
-      "Open Grants support projects that drive the Filecoin ecosystem forward, including projects that enhance the network's utility or improve Filecoin functionality.",
+      "Open Grants support projects that drive the Filecoin ecosystem forward, including projects that enhance the network's utility or improve Filecoin functionality. Refer to GitHub for more details.",
     icon: Coins,
+    cta: {
+      href: FILECOIN_FOUNDATION_URLS.grants.documents.openGrants,
+      text: 'Learn More',
+      icon: ArrowUpRight,
+    },
   },
   {
     title: 'Documentation Grants',
     description:
       'Grants are available to support the creation of new resources related to the Filecoin network or the improvement of existing documentation. Refer to GitHub for more details.',
     icon: Coin,
+    cta: {
+      href: FILECOIN_FOUNDATION_URLS.grants.documents.documentationGrants,
+      text: 'Learn More',
+      icon: ArrowUpRight,
+    },
   },
   {
-    title: 'Requests for Proposal',
+    title: 'Requests for Proposals',
     description:
       'Request for Proposals (RFP) grants have clearly scoped deliverables, milestones, and funding limits. Refer to GitHub for any ongoing RFPs.',
     icon: Megaphone,
+    cta: {
+      href: FILECOIN_FOUNDATION_URLS.grants.documents.requestsForProposals,
+      text: 'Learn More',
+      icon: ArrowUpRight,
+    },
   },
 ]
