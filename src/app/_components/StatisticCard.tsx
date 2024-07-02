65a8bea969e8fa85eacdf2a17a54aec8f0c8f6c1
Mirha Masala <mirha@hey.com>
2024-07-02 15:02:07 +0100

    CR

--- src/app/_components/StatisticCard.tsx
+++ src/app/_components/StatisticCard.tsx
@@ -1,12 +1,16 @@
 import { Icon, type IconProps } from '@/components/Icon'
 
 export type StatisticCardProps = {
-  stat: number
   icon: IconProps['component']
+  value: number
   description: string
 }
 
-export function StatisticCard({ icon, stat, description }: StatisticCardProps) {
+export function StatisticCard({
+  icon,
+  value,
+  description,
+}: StatisticCardProps) {
   return (
     <li className="flex min-h-32 rounded-lg border border-brand-300 p-1">
       <figure className="grid w-1/3 place-items-center rounded bg-brand-500">
@@ -16,7 +20,7 @@ export function StatisticCard({ icon, stat, description }: StatisticCardProps) {
       <div className="flex w-2/3 place-items-center p-4">
         <div className="space-y-2">
           <span className="text-4xl font-light text-brand-100">
-            {stat.toLocaleString()}+
+            {value.toLocaleString()}+
           </span>
           <p className="text-sm leading-4 text-brand-200">{description}</p>
         </div>
--- src/app/orbit/data/statisticsData.tsx
+++ src/app/orbit/data/statisticsData.tsx
@@ -7,26 +7,26 @@ import {
 
 import type { StatisticCardProps } from '@/components/StatisticCard'
 
-export const statisticsData: Array<StatisticCardProps> = [
+export const statisticsData: StatisticCardProps[] = [
   {
     icon: UsersThree,
-    stat: 145,
+    value: 145,
     description: 'Orbit Ambassadors active worldwide',
   },
   {
     icon: GraduationCap,
-    stat: 10_000,
+    value: 10_000,
     description:
       'Early-career developers, students, and enthusiasts introduced to the Filecoin ecosystem',
   },
   {
     icon: Trophy,
-    stat: 190,
+    value: 190,
     description: 'Workshops and community hackathons hosted',
   },
   {
     icon: GlobeHemisphereWest,
-    stat: 40,
+    value: 40,
     description: 'Countries that have hosted Orbit events',
   },
 ]
