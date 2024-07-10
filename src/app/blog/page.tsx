2f1016c0059a3237ae3b89f01d5c2c3ca9134d10
Mirha Masala <mirha@hey.com>
2024-07-10 08:51:06 +0100

    Get featured entries with util function

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -25,7 +25,7 @@ import { type NextServerSearchParams } from '@/types/searchParams'
 import { buildImageSizeProp } from '@/utils/buildImageSizeProp'
 import { getCategorySettings } from '@/utils/categoryUtils'
 import { createMetadata } from '@/utils/createMetadata'
-import { getBlogPostsData } from '@/utils/getBlogPostData'
+import { getBlogPostData, getBlogPostsData } from '@/utils/getBlogPostData'
 import { getBlogPostMetaData } from '@/utils/getMetaData'
 
 import { attributes } from '@/content/pages/blog.md'
@@ -49,7 +49,7 @@ type Props = {
 const posts = getBlogPostsData()
 const { categorySettings, validCategoryOptions } = getCategorySettings('blog')
 const { featured_entry: featuredPostSlug, seo } = attributes
-const featuredPost = posts.find((post) => post.slug === featuredPostSlug)
+const featuredPost = getBlogPostData(featuredPostSlug || '')
 
 export const metadata = createMetadata({
   seo,
