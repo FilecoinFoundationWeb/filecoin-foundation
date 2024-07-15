2e6a687d09c25e275fb24e67b698d8e79480baea
Barbara Peric <b.peric10@gmail.com>
2024-07-15 16:21:03 +0100

    refactor so test can run in chunks

--- cypress/e2e/weekly_schedule/all_ecosystem_explorer_projects_spec.cy.ts
+++ cypress/e2e/weekly_schedule/all_ecosystem_explorer_projects_spec.cy.ts
@@ -1,11 +1,76 @@
 import { PATHS } from '../../../src/app/_constants/paths'
-import { verifyAllEntryLinks } from '../../support/verifyAllEntryLinksUtil'
+import { verifyAllEcosystemProjectLinks } from '../../support/verifyAllEntryLinksUtil'
 
 describe('All Ecosystem Explorer Projects', () => {
-  it('should check links', () => {
-    verifyAllEntryLinks(
+  it('should check links with artificial-intelligence category', () => {
+    verifyAllEcosystemProjectLinks(
       PATHS.ECOSYSTEM_EXPLORER.entriesContentPath as string,
       PATHS.ECOSYSTEM_EXPLORER.path as string,
+      'artificial-intelligence',
+    )
+  })
+
+  it('should check links with education-academia-health category', () => {
+    verifyAllEcosystemProjectLinks(
+      PATHS.ECOSYSTEM_EXPLORER.entriesContentPath as string,
+      PATHS.ECOSYSTEM_EXPLORER.path as string,
+      'education-academia-health',
+    )
+  })
+
+  it('should check links with finance category', () => {
+    verifyAllEcosystemProjectLinks(
+      PATHS.ECOSYSTEM_EXPLORER.entriesContentPath as string,
+      PATHS.ECOSYSTEM_EXPLORER.path as string,
+      'finance',
+    )
+  })
+
+  it('should check links with media-entertainment category', () => {
+    verifyAllEcosystemProjectLinks(
+      PATHS.ECOSYSTEM_EXPLORER.entriesContentPath as string,
+      PATHS.ECOSYSTEM_EXPLORER.path as string,
+      'media-entertainment',
+    )
+  })
+
+  it('should check links with network category', () => {
+    verifyAllEcosystemProjectLinks(
+      PATHS.ECOSYSTEM_EXPLORER.entriesContentPath as string,
+      PATHS.ECOSYSTEM_EXPLORER.path as string,
+      'network',
+    )
+  })
+
+  it('should check links with public-goods-dweb category', () => {
+    verifyAllEcosystemProjectLinks(
+      PATHS.ECOSYSTEM_EXPLORER.entriesContentPath as string,
+      PATHS.ECOSYSTEM_EXPLORER.path as string,
+      'public-goods-dweb',
+    )
+  })
+
+  it('should check links with science-climate-space category', () => {
+    verifyAllEcosystemProjectLinks(
+      PATHS.ECOSYSTEM_EXPLORER.entriesContentPath as string,
+      PATHS.ECOSYSTEM_EXPLORER.path as string,
+      'science-climate-space',
+    )
+  })
+
+  it('should check links with storage category', () => {
+    verifyAllEcosystemProjectLinks(
+      PATHS.ECOSYSTEM_EXPLORER.entriesContentPath as string,
+      PATHS.ECOSYSTEM_EXPLORER.path as string,
+      'storage',
+    )
+  })
+
+  it('should check links with tooling-productivity category', () => {
+    verifyAllEcosystemProjectLinks(
+      PATHS.ECOSYSTEM_EXPLORER.entriesContentPath as string,
+      PATHS.ECOSYSTEM_EXPLORER.path as string,
+      'tooling-productivity',
     )
   })
 })
--- cypress/support/getAllSlugsUtil.ts
+++ cypress/support/getAllSlugsUtil.ts
@@ -1,8 +1,51 @@
+import path from 'path'
+
+import matter from 'gray-matter'
+
 export function getAllSlugs(
   directoryPath: string,
+  category: string,
 ): Cypress.Chainable<string[]> {
-  return cy.task<string[]>('readDir', directoryPath).then((files: string[]) => {
-    const slugs = files.map((file) => file.replace('.md', ''))
-    return cy.wrap(slugs)
+  return cy.task<string[]>('readDir', directoryPath).then((files) => {
+    const slugs: string[] = []
+
+    const fileReadPromises = files.map((file) => {
+      const filePath = path.join(directoryPath, file)
+
+      return cy.task('readFile', filePath).then((content) => {
+        const parsedContent = matter(content as string)
+        if (parsedContent.data.category === category) {
+          slugs.push(file.replace('.md', ''))
+        }
+      })
+    })
+
+    return cy.wrap(Promise.all(fileReadPromises).then(() => slugs))
+  })
+}
+
+export function getAllEventSlugs(
+  directoryPath: string,
+): Cypress.Chainable<string[]> {
+  return cy.task<string[]>('readDir', directoryPath).then((files) => {
+    const slugs: string[] = []
+
+    const fileReadPromises = files.map((file) => {
+      const filePath = path.join(directoryPath, file)
+
+      return cy.task('readFile', filePath).then((content) => {
+        const parsedContent = matter(content as string)
+
+        const isValidExternalLink =
+          !parsedContent.data['external-link'] ||
+          typeof parsedContent.data['external-link'] === 'string'
+
+        if (isValidExternalLink) {
+          slugs.push(file.replace('.md', ''))
+        }
+      })
+    })
+
+    return cy.wrap(Promise.all(fileReadPromises).then(() => slugs))
   })
 }
