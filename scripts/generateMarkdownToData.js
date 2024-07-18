e1f8ec9807918de5d6db442e25fc1b2f8e0f7ef8
Barbara Peric <b.peric10@gmail.com>
2024-07-18 15:34:55 +0100

    add generation of cmsConfigSchema file

--- scripts/generateMarkdownToData.js
+++ scripts/generateMarkdownToData.js
@@ -5,19 +5,32 @@ const yaml = require('js-yaml')
 
 const {
   pascalCase,
-  camelCase,
+  snakeCase,
   startCase,
+  camelCase,
   generateFile,
   createDir,
   writeFile,
 } = require('./generateMarkdownToDataHelpers.js')
 
 const CONFIG_FILE_PATH = path.resolve(__dirname, '../public/admin/config.yml')
+const CMS_CONFIG_SCHEMA_PATH = path.resolve(
+  __dirname,
+  '../src/app/_data/cmsConfigSchema.json',
+)
 
 const TEMPLATE_PATHS = {
   getData: path.resolve(
     __dirname,
     '../templates/generateMarkdownToData/getData.ts',
+  ),
+  mapMarkdownToData: path.resolve(
+    __dirname,
+    '../templates/generateMarkdownToData/mapMarkdownToData.ts',
+  ),
+  types: path.resolve(
+    __dirname,
+    '../templates/generateMarkdownToData/types.ts',
   ),
 }
 
@@ -39,6 +52,8 @@ if (!configCollection) {
 
 const OUTPUT_DIRS = {
   getDataOutput: path.join(__dirname, '../src/app/_utils'),
+  mapMarkdownToDataOutput: path.join(__dirname, '../src/app/_utils'),
+  typesOutput: path.join(__dirname, '../src/app/_types'),
 }
 
 Object.values(OUTPUT_DIRS).forEach(createDir)
@@ -52,23 +67,20 @@ generateFile(
   collectionName,
 )
 
-const generateTypesAndMapMarkdownToDataFiles = async (collectionName) => {
-  const configCollection = config.collections.find(
-    (collection) => collection.name === collectionName,
-  )
+generateFile(
+  TEMPLATE_PATHS.types,
+  path.join(OUTPUT_DIRS.typesOutput, `${snakeCase(collectionName)}Types.ts`),
+  collectionName,
+)
 
-  if (!configCollection) {
-    console.error(`Configuration for ${collectionName} not found in config.yml`)
-    process.exit(1)
-  }
-
+const generateMapToMarkdowndAndTypesFiles = async (collectionName) => {
   const pascalName = pascalCase(collectionName)
   const camelName = camelCase(collectionName)
 
   const outputDir = path.join(__dirname, '../src/app/_utils')
   createDir(outputDir)
 
-  // Generate mapping file
+  // Generate the mapping file
   const fields = configCollection.fields
   const mappingFilePath = path.join(
     outputDir,
@@ -127,9 +139,59 @@ export type ${pascalName}Data = {
 
   await writeFile(typeFilePath, typeContent)
   console.log(`Type definition file created successfully at ${typeFilePath}.`)
+
+  // Update cmsConfigSchema.json
+  const cmsConfigSchema = JSON.parse(
+    fs.readFileSync(CMS_CONFIG_SCHEMA_PATH, 'utf8'),
+  )
+  const existingCollectionIndex = cmsConfigSchema.collections.findIndex(
+    (collection) => collection.name === collectionName,
+  )
+
+  const newCollection = {
+    name: configCollection.name,
+    label: configCollection.label,
+    folder: configCollection.folder,
+    label_singular: configCollection.label_singular,
+    create: configCollection.create,
+    extension: configCollection.extension,
+    slug: configCollection.slug,
+    fields: configCollection.fields.map((field) => {
+      const baseField = {
+        name: field.name,
+        label: field.label,
+        widget: field.widget,
+      }
+      if (field.required !== undefined) {
+        baseField.required = field.required
+      }
+      if (field.hint) {
+        baseField.hint = field.hint
+      }
+      if (field.options) {
+        baseField.options = field.options
+      }
+      if (field.widget === 'object' && field.fields) {
+        baseField.fields = field.fields
+      }
+      return baseField
+    }),
+  }
+
+  if (existingCollectionIndex !== -1) {
+    cmsConfigSchema.collections[existingCollectionIndex] = newCollection
+  } else {
+    cmsConfigSchema.collections.push(newCollection)
+  }
+
+  fs.writeFileSync(
+    CMS_CONFIG_SCHEMA_PATH,
+    JSON.stringify(cmsConfigSchema, null, 2),
+  )
+  console.log(`cmsConfigSchema.json updated successfully.`)
 }
 
-generateTypesAndMapMarkdownToDataFiles(collectionName)
+generateMapToMarkdowndAndTypesFiles(collectionName)
   .then(() =>
     console.log(`Constants and functions generated for ${collectionName}`),
   )
