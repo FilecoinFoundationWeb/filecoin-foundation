a83a2f072219e7e6e0c1356d0d00e4e46810ac40
Barbara Peric <b.peric10@gmail.com>
2024-07-26 10:19:13 +0100

    github action and test for decap-cms

---
+++ cypress/integration/cms-content_spec.cy.ts
@@ -0,0 +1,27 @@
+describe('Check Image Format', () => {
+  const contentUrl = Cypress.env('CONTENT_URL')
+
+  beforeEach(() => {
+    if (!contentUrl) {
+      throw new Error('CONTENT_URL environment variable is not set')
+    }
+    cy.visit(contentUrl)
+  })
+
+  it('should have images in WebP format', () => {
+    cy.get('img')
+      .should('have.attr', 'srcset')
+      .then((srcset: any) => {
+        const sources = srcset
+          .split(',')
+          .map((src: any) => src.trim().split(' ')[0])
+        sources.forEach((source: any) => {
+          expect(source).to.match(/\.webp$/)
+        })
+      })
+
+    cy.get('img')
+      .should('have.attr', 'src')
+      .and('match', /\.webp$/)
+  })
+})
