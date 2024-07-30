940ccf0759129d3e0e0a1ffedab1fdceaf53fc9a
Mirha Masala <mirha@hey.com>
2024-07-30 13:50:56 +0100

    Disable Sentry

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -4,19 +4,15 @@
 
 import * as Sentry from '@sentry/nextjs'
 
-Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 0.05,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-
-  replaysOnErrorSampleRate: 1.0,
-
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.01,
-  environment: process.env.NODE_ENV || 'production',
-})
+if (process.env.NEXT_PUBLIC_DISABLE_SENTRY !== 'true') {
+  Sentry.init({
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+    tracesSampleRate: 0.05,
+    debug: false,
+    replaysOnErrorSampleRate: 1.0,
+    replaysSessionSampleRate: 0.01,
+    environment: process.env.NODE_ENV || 'production',
+  })
+} else {
+  console.log('Sentry is disabled on the client.')
+}
