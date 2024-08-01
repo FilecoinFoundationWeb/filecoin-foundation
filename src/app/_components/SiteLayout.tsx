49612621082b281f61ebdad8c5f5dd3b0b8dea86
Charly MARTIN <charly@hey.com>
2024-08-01 10:37:45 +0100

    [UXIT-1135] Use GitHub as Decap backend (#441)
    * Use GitHub as Decap backend
    
    * CR: Remove NetlifyIdentityManager
    
    * CR: Add base_url
    
    ---------
    
    Co-authored-by: Mirha Masala <mirha@hey.com>
    Co-authored-by: Mirha Masala <26620750+mirhamasala@users.noreply.github.com>

--- src/app/_components/SiteLayout.tsx
+++ src/app/_components/SiteLayout.tsx
@@ -3,7 +3,6 @@ import { Archivo } from 'next/font/google'
 import { BreakpointDebugger } from '@/components/_dev_BreakpointDebugger'
 import { Footer } from '@/components/Footer'
 import { Navigation } from '@/components/Navigation'
-import { NetlifyIdentityManager } from '@/components/NetlifyIdentityManager'
 
 import { LayoutProps } from '@/layout'
 
@@ -16,8 +15,6 @@ export function SiteLayout({ children }: LayoutProps) {
   return (
     <html lang="en" className={archivo.className}>
       <body className="m-auto flex max-w-[1032px] flex-col justify-between bg-brand-800 px-6 pb-6 pt-8 tracking-wide text-brand-100">
-        <NetlifyIdentityManager />
-
         <Navigation />
 
         <main>{children}</main>
