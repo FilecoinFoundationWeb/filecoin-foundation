5f52f8d3596e3f02d1fc7980502d003c3aeee8cf
Barbara Peric <b.peric10@gmail.com>
2024-08-01 13:43:20 +0200

    remove schema folder

--- src/app/_schemas/newsletterSchema.ts
+++
@@ -1,8 +0,0 @@
-import { z, ZodSchema } from 'zod'
-
-export const NewsletterSchema: ZodSchema = z.object({
-  email: z
-    .string()
-    .min(1, { message: 'This field has to be filled.' })
-    .email('This is not a valid email.'),
-})
