cd98d304819dacea2552426438b82c3fc975b160
Mirha Masala <mirha@hey.com>
2024-08-05 20:21:50 +0100

    CR: Refactor

--- cypress/support/verifyLinksUtil.ts
+++ cypress/support/verifyLinksUtil.ts
@@ -1,25 +1,42 @@
+const REQUEST_DENIED_STATUS_CODE = 999
+const CLIENT_ERROR_THRESHOLD = 400
+
 export function verifyLinks(path: string) {
   cy.visit(path)
 
   cy.get('a').each(($link) => {
     const href = $link.prop('href')
 
-    if (href && !href.includes('mailto:')) {
-      const linkUrl = new URL(href, window.location.origin)
-      const isExternal = linkUrl.origin !== window.location.origin
+    if (!href || href.includes('mailto:')) return
 
-      if (isExternal) {
-        cy.request({ url: href, failOnStatusCode: false }).then((response) => {
-          if (!response || response.status >= 400) {
-            let errorMessage = response
-              ? `Status code: ${response.status}`
-              : 'No response received or request timed out'
-            if (response && response.status !== 999) {
-              cy.log(`Broken link: ${href} - ${errorMessage}`)
-            }
-          }
-        })
-      }
+    const linkUrl = new URL(href, window.location.origin)
+    const isExternalLink = linkUrl.origin !== window.location.origin
+
+    if (isExternalLink) {
+      verifyExternalLink(href)
     }
   })
+}
+
+function verifyExternalLink(url: string) {
+  cy.request({ url, failOnStatusCode: false }).then((response) => {
+    if (response.status === REQUEST_DENIED_STATUS_CODE) {
+      const errorMessage = getErrorMessage(response)
+      cy.log(`Ignored link: ${url} - ${errorMessage}`)
+      return
+    }
+
+    if (isClientOrServerError(response.status)) {
+      const errorMessage = getErrorMessage(response)
+      cy.log(`Broken link: ${url} - ${errorMessage}`)
+    }
+  })
+}
+
+function isClientOrServerError(status: number) {
+  return status >= CLIENT_ERROR_THRESHOLD
+}
+
+function getErrorMessage(response: Cypress.Response<any>) {
+  return `Status code: ${response.status} (${response.statusText})`
 }
