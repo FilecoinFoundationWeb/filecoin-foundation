5414e980c72e0e80e7f056545926e7186ed55adc
Barbara Peric <b.peric10@gmail.com>
2024-08-21 13:53:06 +0100

    fix test

--- cypress/e2e/critical/digest_page_spec.cy.ts
+++ cypress/e2e/critical/digest_page_spec.cy.ts
@@ -1,6 +1,6 @@
-import { PATHS } from '../../src/app/_constants/paths'
-import { testPageMetadata } from '../support/test-utils'
-import { verifyLinks } from '../support/verifyLinksUtil'
+import { PATHS } from '@/constants/paths'
+import { testPageMetadata } from '@/support/test-utils'
+import { verifyLinks } from '@/support/verifyLinksUtil'
 
 describe('Digest Page', () => {
   it('should check metadata', () => {
