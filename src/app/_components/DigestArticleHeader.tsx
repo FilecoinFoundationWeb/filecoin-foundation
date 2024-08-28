1adf836890b189c8b67a528da41f84401c4d8674
Mirha Masala <mirha@hey.com>
2024-08-28 13:44:11 +0100

    CR: Refactor with ArticleHeader

--- src/app/_components/DigestArticleHeader.tsx
+++ src/app/_components/DigestArticleHeader.tsx
@@ -1,12 +1,7 @@
-import { DynamicImage } from '@/components/DynamicImage'
-import { Heading } from '@/components/Heading'
+import { ArticleHeader } from '@/components/ArticleHeader'
 import { TagLabel } from '@/components/TagLabel'
 
 import { type DigestArticleData } from '@/types/digestTypes'
-
-import { buildImageSizeProp } from '@/utils/buildImageSizeProp'
-
-import { graphicsData } from '@/data/graphicsData'
 
 type DigestArticleHeaderProps = Pick<
   DigestArticleData,
@@ -21,35 +16,20 @@ export function DigestArticleHeader({
   image,
 }: DigestArticleHeaderProps) {
   return (
-    <header className="space-y-8">
-      <div className="space-y-4">
+    <ArticleHeader image={image && { src: image.src, alt: image.alt || '' }}>
+      <>
         <div className="space-x-3">
           <TagLabel borderColor="brand-100">{`Issue ${issueNumber}`}</TagLabel>
           <TagLabel>{`Article ${articleNumber}`}</TagLabel>
         </div>
-        <Heading tag="h1" variant="4xl">
-          {title}
-        </Heading>
+        <ArticleHeader.Title>{title}</ArticleHeader.Title>
         <div>
           <span className="text-sm text-blue-100">
             {formatAuthors(authors)}
           </span>
         </div>
-      </div>
-
-      <div className="relative aspect-video">
-        <DynamicImage
-          fill
-          priority
-          quality={100}
-          src={image?.src || ''}
-          alt={image?.alt || ''}
-          className="rounded-lg"
-          sizes={buildImageSizeProp({ startSize: '100vw', md: '680px' })}
-          fallback={graphicsData.imageFallback}
-        />
-      </div>
-    </header>
+      </>
+    </ArticleHeader>
   )
 }
 
