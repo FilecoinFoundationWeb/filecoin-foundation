9b6d42ccbabec6f152ef0b5cf1ab97e4232ab204
Mirha Masala <26620750+mirhamasala@users.noreply.github.com>
2024-08-29 20:47:13 +0100

    [UXIT-1461] Update Security Page (#591)
    * Adapt page header
    
    * Update Bug Bounty Program section
    
    * Include Web3 Security Maturity Model section
    
    * Update footer CTA
    
    * Remove Audit section
    
    * Include Developer Support Section
    
    * CR
    
    * Refactor CTAButtonGroup
    
    * CR
    
    * CR - Refactor PageHeader
    
    * CR

--- src/app/_components/PageHeader.tsx
+++ src/app/_components/PageHeader.tsx
@@ -1,6 +1,9 @@
 import { clsx } from 'clsx'
 
-import { Button } from '@/components/Button'
+import {
+  type CTAButtonGroupProps,
+  CTAButtonGroup,
+} from '@/components/CTAButtonGroup'
 import {
   DescriptionText,
   type DescriptionTextType,
@@ -11,8 +14,6 @@ import { Meta, type MetaDataType } from '@/components/Meta'
 import { SectionDivider } from '@/components/SectionDivider'
 import { StaticImage, type StaticImageProps } from '@/components/StaticImage'
 
-import { type CTAProps } from '@/types/sharedProps/ctaType'
-
 import { buildImageSizeProp } from '@/utils/buildImageSizeProp'
 
 type TitleProps = {
@@ -21,14 +22,48 @@ type TitleProps = {
 
 type PageHeaderProps = {
   title: TitleProps['children']
-  description?: DescriptionTextType
-  cta?: CTAProps | [CTAProps, CTAProps]
-  metaData?: MetaDataType
-  isFeatured?: boolean
   image: StaticImageProps | DynamicImageProps
+  isFeatured?: boolean
+  metaData?: MetaDataType
+  description?: DescriptionTextType
+  cta?: CTAButtonGroupProps['cta']
 }
 
-function Title({ children }: TitleProps) {
+export function PageHeader({
+  title,
+  image,
+  isFeatured = false,
+  metaData,
+  description,
+  cta,
+}: PageHeaderProps) {
+  return (
+    <header className="grid gap-4">
+      {isFeatured && <SectionDivider title="Featured" />}
+
+      <div className="flex flex-col gap-6 lg:flex-row">
+        <div className="flex flex-col gap-4 lg:w-1/2">
+          <PageHeader.Title>{title}</PageHeader.Title>
+
+          {metaData && (
+            <span className="mb-2">
+              <Meta metaData={metaData} />
+            </span>
+          )}
+
+          {description && <DescriptionText>{description}</DescriptionText>}
+          {cta && <CTAButtonGroup cta={cta} />}
+        </div>
+
+        <div className="lg:w-1/2">
+          <PageHeader.Image image={image} />
+        </div>
+      </div>
+    </header>
+  )
+}
+
+PageHeader.Title = function Title({ children }: TitleProps) {
   return (
     <Heading tag="h1" variant="4xl">
       {children}
@@ -36,11 +71,9 @@ function Title({ children }: TitleProps) {
   )
 }
 
-const aspectRatioStyle = 'aspect-video'
-const borderStyle = 'rounded-lg border border-brand-100'
-const imageSizes = buildImageSizeProp({ startSize: '100vw', lg: '490px' })
-
-function PageHeaderImage({ image }: Pick<PageHeaderProps, 'image'>) {
+PageHeader.Image = function PageHeaderImage({
+  image,
+}: Pick<PageHeaderProps, 'image'>) {
   if (!image) return null
 
   const isDynamicImage = 'src' in image
@@ -51,80 +84,15 @@ function PageHeaderImage({ image }: Pick<PageHeaderProps, 'image'>) {
   const commonProps = {
     priority: true,
     quality: 100,
-    sizes: imageSizes,
-    className: clsx(aspectRatioStyle, borderStyle),
+    sizes: buildImageSizeProp({ startSize: '100vw', lg: '490px' }),
+    className: clsx('h-full w-full', 'rounded-lg border border-brand-100'),
   }
 
   const renderImage = (ImageComponent: React.ElementType) => (
-    <div className={clsx('relative', aspectRatioStyle)}>
-      <ImageComponent
-        {...image}
-        fill={isDynamicImage}
-        {...commonProps}
-        className={clsx('h-full w-full', borderStyle)}
-      />
+    <div className={clsx('relative', 'aspect-video')}>
+      <ImageComponent {...image} fill={isDynamicImage} {...commonProps} />
     </div>
   )
 
   return isDynamicImage ? renderImage(DynamicImage) : renderImage(StaticImage)
 }
-
-export function PageHeader({
-  title,
-  description,
-  cta,
-  image,
-  metaData,
-  isFeatured = false,
-}: PageHeaderProps) {
-  const ctaArray = Array.isArray(cta) ? cta : [cta]
-
-  return (
-    <header className="grid gap-4">
-      {isFeatured && <SectionDivider title="Featured" />}
-      <div className="flex flex-col gap-6 lg:flex-row">
-        <div className="flex flex-col gap-4 lg:w-1/2">
-          <Title>{title}</Title>
-
-          {metaData && (
-            <span className="mb-2">
-              <Meta metaData={metaData} />
-            </span>
-          )}
-
-          {description && <DescriptionText>{description}</DescriptionText>}
-
-          {ctaArray && (
-            <div
-              className={clsx(
-                'grid gap-4',
-                ctaArray.length === 1
-                  ? 'lg:grid-cols-1'
-                  : 'sm:grid-cols-2 sm:gap-3 lg:grid-cols-1 lg:gap-4',
-              )}
-            >
-              {ctaArray.map(
-                (button, index) =>
-                  button && (
-                    <Button
-                      key={index}
-                      href={button.href}
-                      variant={index === 0 ? 'primary' : 'ghost'}
-                    >
-                      {button.text}
-                    </Button>
-                  ),
-              )}
-            </div>
-          )}
-        </div>
-
-        <div className="lg:w-1/2">
-          <PageHeaderImage image={image} />
-        </div>
-      </div>
-    </header>
-  )
-}
-
-PageHeader.Title = Title
