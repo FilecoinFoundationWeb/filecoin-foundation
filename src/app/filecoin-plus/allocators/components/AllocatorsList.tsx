9d2f9db79ad67a8485a2f3277f3de25b4bf52fdc
Barbara Peric <b.peric10@gmail.com>
2024-09-27 15:55:46 +0100

    add no results component to allocator list

--- src/app/filecoin-plus/allocators/components/AllocatorsList.tsx
+++ src/app/filecoin-plus/allocators/components/AllocatorsList.tsx
@@ -1,17 +1,20 @@
 'use client'
 
+import { CloudSlash } from '@phosphor-icons/react/dist/ssr'
 import {
   useReactTable,
   getCoreRowModel,
   getSortedRowModel,
 } from '@tanstack/react-table'
 
+import { FIL_PLUS_URLS } from '@/constants/siteMetadata'
+
+import { Button } from '@/components/Button'
+import { NoResultsMessage } from '@/components/NoResultsMessage'
 import { TableBody } from '@/components/Table/TableBody'
 import { TableHead } from '@/components/Table/TableHead'
 
 import { mapAllocatorsToTableData } from '../data/allocatorTable'
-
-import { NoAllocatorsComponent } from './NoAllocatorsComponents'
 
 export type AllocatorProps = {
   name: string
@@ -40,7 +43,18 @@ export function AllocatorsList({ allocators }: AllocatorsListProps) {
   const noAllocators = allocators.length === 0
 
   if (noAllocators) {
-    return <NoAllocatorsComponent />
+    return (
+      <NoResultsMessage title="Allocator Data Unavailable" icon={CloudSlash}>
+        <p className="max-w-md text-center">
+          We’re having trouble fetching the allocator data. For the latest
+          information, please check the Allocators list on the Filecoin Plus
+          Registry.
+        </p>
+        <Button href={FIL_PLUS_URLS.allocators.site} variant="primary">
+          Go To FIlecoin Plus Registry
+        </Button>
+      </NoResultsMessage>
+    )
   }
 
   return (
