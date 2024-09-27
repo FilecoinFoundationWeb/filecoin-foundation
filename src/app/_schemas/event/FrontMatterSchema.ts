430e8517ceca7d3dd5f0b9430be0198f3ebb9d17
Mirha Masala <mirha@hey.com>
2024-09-27 09:55:39 +0100

    Extend Event with speakers

--- src/app/_schemas/event/FrontMatterSchema.ts
+++ src/app/_schemas/event/FrontMatterSchema.ts
@@ -6,6 +6,7 @@ import {
 } from '@/utils/categoryUtils'
 
 import { DynamicBaseDataSchema } from '@/schemas/dynamicDataBaseSchema'
+import { SpeakersSchema } from '@/schemas/event/SpeakerSchema'
 import { SponsorsSchema } from '@/schemas/event/SponsorSchema'
 
 const { validCategoryIds } = getEventsCategorySettings()
@@ -32,5 +33,6 @@ export const FrontMatterSchema = DynamicBaseDataSchema.extend({
     .optional(),
   startDate: z.coerce.date(),
   endDate: z.coerce.date().optional(),
+  speakers: SpeakersSchema.optional(),
   sponsors: SponsorsSchema.optional(),
 })
