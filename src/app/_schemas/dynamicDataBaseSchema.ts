5b54a0707fe6018e4f2d771050035ba3d6210d75
Charly Martin <charly@hey.com>
2024-10-02 17:49:46 +0100

    fixup! Rework zod validation to allow strict

--- src/app/_schemas/dynamicDataBaseSchema.ts
+++ src/app/_schemas/dynamicDataBaseSchema.ts
@@ -3,23 +3,23 @@ import { z } from 'zod'
 import { ImagePropsSchema } from '@/schemas/imagePropsSchema'
 import { SeoMetadataSchema } from '@/schemas/seoMetadataSchema'
 
-export const DynamicBaseDataSchema = z
+export const DynamicBaseDataSchema = z.object({
+  createdOn: z.coerce.date(),
+  updatedOn: z.coerce.date().optional(),
+  publishedOn: z.coerce.date().optional(),
+  image: ImagePropsSchema.optional(),
+  seo: SeoMetadataSchema,
+})
+
+// Temporary schema duplication until all schemas are migrated to kebab-case to match the CMS
+export const BaseDataSchemaKebabCase = z
   .object({
-    createdOn: z.coerce.date(),
-    updatedOn: z.coerce.date().optional(),
-    publishedOn: z.coerce.date().optional(),
+    'created-on': z.coerce.date(),
+    'updated-on': z.coerce.date().optional(),
+    'published-on': z.coerce.date().optional(),
     image: ImagePropsSchema.optional(),
     seo: SeoMetadataSchema,
   })
   .strict()
-
-// Temporary schema duplication until all schemas are migrated to kebab-case to match the CMS
-export const BaseDataSchemaKebabCase = z.object({
-  'created-on': z.coerce.date(),
-  'updated-on': z.coerce.date().optional(),
-  'published-on': z.coerce.date().optional(),
-  image: ImagePropsSchema.optional(),
-  seo: SeoMetadataSchema,
-})
 
 export type DynamicBaseData = z.infer<typeof DynamicBaseDataSchema>
