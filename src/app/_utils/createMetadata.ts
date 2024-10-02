c616ebd5a2016b3102674b4d4d9d1f3e7422e561
Mirha Masala <mirha@hey.com>
2024-10-02 08:36:56 +0100

    Refactor createMetadata function to validate and enrich SEO metadata using zod schema

--- src/app/_utils/createMetadata.ts
+++ src/app/_utils/createMetadata.ts
@@ -2,7 +2,10 @@ import { type Metadata as NextMetadata } from 'next'
 
 import type { DynamicPathValues, PathValues } from '@/constants/paths'
 
-import type { SeoMetadata } from '@/schemas/seoMetadataSchema'
+import {
+  type SeoMetadata,
+  SeoMetadataSchema,
+} from '@/schemas/seoMetadataSchema'
 
 type CreateMetadataProps = {
   seo: SeoMetadata
@@ -15,23 +18,44 @@ export function createMetadata({
   path,
   overrideDefaultTitle = false,
 }: CreateMetadataProps): NextMetadata {
-  const { title, description, image } = seo
+  const parsedSEO = SeoMetadataSchema.parse(seo)
 
-  return {
-    title: overrideDefaultTitle ? { absolute: title } : title,
-    description,
-    openGraph: {
-      title: seo.og?.title || title,
-      description,
-      images: seo.og?.image || image,
+  const enrichedSEO = {
+    title: parsedSEO.title,
+    description: parsedSEO.description,
+    og: {
+      title: parsedSEO.og?.title || parsedSEO.title,
+      image: parsedSEO.og?.image || parsedSEO.image,
     },
     twitter: {
-      card: seo.twitter?.card,
-      site: seo.twitter?.site,
-      creator: seo.twitter?.creator,
+      card: parsedSEO.twitter?.card,
+      site: parsedSEO.twitter?.site,
+      creator: parsedSEO.twitter?.creator,
+    },
+  }
+
+  const parsedEnrichedSEO = SeoMetadataSchema.parse(enrichedSEO)
+
+  const meta = {
+    title: overrideDefaultTitle
+      ? { absolute: parsedEnrichedSEO.title }
+      : parsedEnrichedSEO.title,
+    description: parsedEnrichedSEO.description,
+    openGraph: {
+      title: parsedEnrichedSEO.og?.title,
+      description: parsedEnrichedSEO.description,
+      images: parsedEnrichedSEO.og?.image,
+    },
+    twitter: {
+      card: parsedEnrichedSEO.twitter?.card,
+      site: parsedEnrichedSEO.twitter?.site,
+      creator: parsedEnrichedSEO.twitter?.creator,
     },
     alternates: {
       canonical: path,
     },
   }
+
+  console.log({ seo, parsedSEO, enrichedSEO, parsedEnrichedSEO, meta })
+  return meta
 }
