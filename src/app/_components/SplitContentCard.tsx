925e9a1293e1de0b241512dda4154eeaf14323d4
Mirha Masala <mirha@hey.com>
2024-10-07 10:01:42 +0100

    Create SplitContentCard component

---
+++ src/app/_components/SplitContentCard.tsx
@@ -0,0 +1,47 @@
+import Image from 'next/image'
+
+import { clsx } from 'clsx'
+
+import type { StaticImageProps } from '@/types/sharedProps/imageType'
+
+import { buildImageSizeProp } from '@/utils/buildImageSizeProp'
+
+import { BasicCard } from '@/components/BasicCard'
+
+type SplitContentCardProps = {
+  index: number
+  image: StaticImageProps
+  as?: React.ElementType
+  children: React.ReactNode
+}
+
+export function SplitContentCard({
+  index,
+  image,
+  as: Tag = 'div',
+  children,
+}: SplitContentCardProps) {
+  const isImagePositionOdd = index % 2 === 1
+
+  return (
+    <Tag
+      className={clsx(
+        'flex flex-col gap-4 lg:flex-row lg:gap-6',
+        isImagePositionOdd && 'lg:flex-row-reverse',
+      )}
+    >
+      <Image
+        src={image.data}
+        alt={image.alt}
+        className="h-48 flex-shrink-0 rounded-lg object-cover sm:h-64 lg:h-auto lg:w-1/3"
+        sizes={buildImageSizeProp({
+          startSize: '100vw',
+          lg: '33vw',
+        })}
+      />
+      <div className="flex-grow">
+        <BasicCard>{children}</BasicCard>
+      </div>
+    </Tag>
+  )
+}
