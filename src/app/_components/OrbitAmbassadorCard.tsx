0bf16608a88b0e08903c4fbd33ab2debeaf1bc2b
Mirha Masala <mirha@hey.com>
2024-10-07 10:41:03 +0100

    Scout Refactor OrbitAmbassadorCard with SplitContentCard

--- src/app/_components/OrbitAmbassadorCard.tsx
+++ src/app/_components/OrbitAmbassadorCard.tsx
@@ -1,10 +1,4 @@
-import Image from 'next/image'
-
-import { clsx } from 'clsx'
-
-import { buildImageSizeProp } from '@/utils/buildImageSizeProp'
-
-import { BasicCard } from '@/components/BasicCard'
+import { SplitContentCard } from '@/components/SplitContentCard'
 
 import type { AmbassadorData } from '@/orbit/data/ambassadorsData'
 
@@ -19,38 +13,17 @@ export function OrbitAmbassadorCard({
   testimonial,
   location,
 }: OrbitAmbassadorCardProps) {
-  const isImagePositionOdd = index % 2 === 1
-
   return (
-    <article
-      className={clsx(
-        'flex flex-col gap-4 lg:flex-row lg:gap-6',
-        isImagePositionOdd && 'lg:flex-row-reverse',
-      )}
-    >
-      <Image
-        src={image.data}
-        alt={image.alt}
-        className="h-48 flex-shrink-0 rounded-lg object-cover sm:h-64 lg:h-auto lg:w-1/3"
-        sizes={buildImageSizeProp({
-          startSize: '100vw',
-          lg: '33vw',
-        })}
-      />
-
-      <div className="flex-grow">
-        <BasicCard>
-          <div className="space-y-6">
-            {testimonial.map((text, index) => (
-              <p key={index}>{text}</p>
-            ))}
-            <div>
-              <p className="font-bold">{name}</p>
-              <p className="text-brand-300">— {location}</p>
-            </div>
-          </div>
-        </BasicCard>
+    <SplitContentCard index={index} image={image} as="article">
+      <div className="space-y-6">
+        {testimonial.map((text, index) => (
+          <p key={index}>{text}</p>
+        ))}
+        <div>
+          <p className="font-bold">{name}</p>
+          <p className="text-brand-300">— {location}</p>
+        </div>
       </div>
-    </article>
+    </SplitContentCard>
   )
 }
