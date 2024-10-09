93178e0f5188361a743336f4a552f400dd1f2d57
Mirha Masala <mirha@hey.com>
2024-10-09 13:21:52 +0100

    Style desktop Schedule with TabGroup

--- src/app/events/[slug]/components/Schedule.tsx
+++ src/app/events/[slug]/components/Schedule.tsx
@@ -1,3 +1,5 @@
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
+
 import type { Event } from '@/types/eventType'
 
 import { BasicCard } from '@/components/BasicCard'
@@ -12,40 +14,52 @@ type ScheduleProps = {
 
 export function Schedule({ schedule }: ScheduleProps) {
   return (
-    <>
-      {schedule!.days.map((day) => {
-        return (
-          <div key={formatDate(day.date)} className="grid gap-6">
-            <span className="font-bold text-brand-300">
-              {formatDate(day.date)}
-            </span>
-            <BasicCard key={formatDate(day.date)}>
+    <TabGroup className="grid gap-6">
+      <TabList className="-m-2 flex gap-4 overflow-auto p-2">
+        {schedule!.days.map((day) => (
+          <Tab
+            key={formatDate(day.date)}
+            className="whitespace-nowrap rounded-lg p-3 font-bold text-brand-300 focus:brand-outline data-[hover]:bg-brand-700 data-[selected]:bg-brand-700 data-[selected]:text-brand-400"
+          >
+            {formatDate(day.date)}
+          </Tab>
+        ))}
+      </TabList>
+      <TabPanels>
+        {schedule!.days.map((day) => (
+          <TabPanel
+            key={formatDate(day.date)}
+            className="rounded-lg focus:brand-outline"
+          >
+            <div className="grid gap-4">
               {day.events.map((event) => (
-                <div key={event.title} className="grid gap-6 lg:grid-cols-3">
-                  <div className="flex gap-6 text-brand-300 lg:flex-col lg:gap-4">
-                    <div className="text-sm font-bold">
-                      <span>{formatTime(event.start)}</span>
-                      {event.end && <span> – {formatTime(event.end)}</span>}
+                <BasicCard key={event.title}>
+                  <div className="grid gap-6 lg:grid-cols-3">
+                    <div className="flex gap-6 text-brand-300 lg:flex-col lg:gap-1">
+                      <div className="text-sm font-bold">
+                        <span>{formatTime(event.start)}</span>
+                        {event.end && <span> – {formatTime(event.end)}</span>}
+                      </div>
+                      <span className="text-sm">{event.location}</span>
                     </div>
-                    <span className="text-sm">{event.location}</span>
+                    <div className="lg:col-span-2">
+                      <Heading tag="h3" variant="lg">
+                        {event.title}
+                      </Heading>
+                      <p className="mb-4 mt-2 max-w-readable">
+                        {event.description}
+                      </p>
+                      {event.url && (
+                        <TextLink href={event.url}>View Details</TextLink>
+                      )}
+                    </div>
                   </div>
-                  <div key={event.title} className="lg:col-span-2">
-                    <Heading tag="h3" variant="lg">
-                      {event.title}
-                    </Heading>
-                    <p className="mb-4 mt-2 max-w-readable">
-                      {event.description}
-                    </p>
-                    {event.url && (
-                      <TextLink href={event.url}>View Details</TextLink>
-                    )}
-                  </div>
-                </div>
+                </BasicCard>
               ))}
-            </BasicCard>
-          </div>
-        )
-      })}
-    </>
+            </div>
+          </TabPanel>
+        ))}
+      </TabPanels>
+    </TabGroup>
   )
 }
