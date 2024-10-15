1cec579d7370962a4454d36c6041308b256361a0
Barbara Peric <b.peric10@gmail.com>
2024-10-15 09:39:42 +0100

    upgrade headless version and remove aria lables

--- src/app/_components/ListboxButton.tsx
+++ src/app/_components/ListboxButton.tsx
@@ -3,23 +3,12 @@ import React from 'react'
 import { ListboxButton as HeadlessUIListboxButton } from '@headlessui/react'
 
 type ListboxButtonProps = {
-  ariaLabel: string
-  open: boolean
   children: React.ReactNode
 }
 
-export function ListboxButton({
-  ariaLabel,
-  open,
-  children,
-}: ListboxButtonProps) {
+export function ListboxButton({ children }: ListboxButtonProps) {
   return (
-    <HeadlessUIListboxButton
-      aria-haspopup="listbox"
-      aria-expanded={open}
-      aria-label={ariaLabel}
-      className="inline-flex w-full items-center justify-between gap-2 rounded-lg border border-brand-300 p-3 text-brand-300 focus:brand-outline hover:border-current hover:text-brand-400 md:min-w-40"
-    >
+    <HeadlessUIListboxButton className="inline-flex w-full items-center justify-between gap-2 rounded-lg border border-brand-300 p-3 text-brand-300 focus:brand-outline hover:border-current hover:text-brand-400 md:min-w-40">
       {children}
     </HeadlessUIListboxButton>
   )
