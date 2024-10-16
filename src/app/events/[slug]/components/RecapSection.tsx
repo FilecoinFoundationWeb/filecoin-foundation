2efbe10c1c0f2a3e0ca0183b55a21e0a9fbf270c
Mirha Masala <mirha@hey.com>
2024-10-16 11:32:18 +0100

    Include description

--- src/app/events/[slug]/components/RecapSection.tsx
+++ src/app/events/[slug]/components/RecapSection.tsx
@@ -12,7 +12,11 @@ type RecapSectionProps = {
 
 export function RecapSection({ youtubeEmbedUrl }: RecapSectionProps) {
   return (
-    <PageSection kicker="Event Recap" title="Thank You for Joining Us">
+    <PageSection
+      kicker="Event Recap"
+      title="Thank You for Joining Us"
+      description="This event has concluded, and we hope you had a fantastic experience! Stay tuned for our upcoming events and see what exciting things are on the horizon."
+    >
       <YouTubeVideoEmbed videoUrl={youtubeEmbedUrl} />
       <Button className="sm:self-center" href={PATHS.EVENTS.path}>
         View Upcoming Events
