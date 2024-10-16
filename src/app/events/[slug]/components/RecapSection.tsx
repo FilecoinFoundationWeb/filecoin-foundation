ef1ddd47f88ef548856e005abcd56944bec4fbc8
Mirha Masala <mirha@hey.com>
2024-10-16 10:42:01 +0100

    Include View Upcoming Events

--- src/app/events/[slug]/components/RecapSection.tsx
+++ src/app/events/[slug]/components/RecapSection.tsx
@@ -1,3 +1,6 @@
+import { PATHS } from '@/constants/paths'
+
+import { Button } from '@/components/Button'
 import { PageSection } from '@/components/PageSection'
 import { YouTubeVideoEmbed } from '@/components/YouTubeVideoEmbed'
 
@@ -5,6 +8,9 @@ export function RecapSection() {
   return (
     <PageSection kicker="Event Recap" title="Thank You for Joining Us">
       <YouTubeVideoEmbed videoUrl="https://www.youtube.com/embed/m-lWAQqLI6A" />
+      <Button className="sm:self-center" href={PATHS.EVENTS.path}>
+        View Upcoming Events
+      </Button>
     </PageSection>
   )
 }
