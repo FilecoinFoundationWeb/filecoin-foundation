8204d26ca9334a5e35d8e14775428070c7cbb5a6
Charly Martin <charly@hey.com>
2024-10-22 15:49:56 +0100

    Remove FormListbox

--- src/app/security/maturity-model/components/MobileTableOfContents.tsx
+++ src/app/security/maturity-model/components/MobileTableOfContents.tsx
@@ -1,6 +1,12 @@
 'use client'
 
-import { FormListbox } from '@/components/Form/FormListbox/FormListbox'
+import { Listbox as HeadlessUIListbox } from '@headlessui/react'
+import { ArrowsDownUp, CaretDown } from '@phosphor-icons/react/dist/ssr'
+
+import { Icon } from '@/components/Icon'
+import { ListboxButton } from '@/components/ListboxButton'
+import { ListboxOption } from '@/components/ListboxOption'
+import { ListboxOptions } from '@/components/ListboxOptions'
 
 import { coreFunctionsData } from '../data/coreFunctionsData'
 import { scrollToSection } from '../utils/scrollToSection'
@@ -14,30 +20,37 @@ const options = coreFunctionsData.map(({ slug, title }) => ({
 export function MobileTableOfContents() {
   const { updateHash, isSectionActive, getHashFromSlug } = useUrlHash()
 
+  const selectedOption = options.find((option) => isSectionActive(option.id))
+
+  return (
+    <nav aria-label="Table of Contents" className="w-full max-w-sm">
+      <HeadlessUIListbox
+        value={selectedOption}
+        onChange={(value) => handleChange(value.id)}
+      >
+        <ListboxButton>
+          <div className="inline-flex w-full items-center gap-2">
+            <Icon component={ArrowsDownUp} />
+            <span className="grow truncate text-left">
+              {selectedOption?.name || 'Select a section'}
+            </span>
+            <Icon component={CaretDown} size={16} weight="bold" />
+          </div>
+        </ListboxButton>
+
+        <ListboxOptions position="left">
+          {options.map((option) => (
+            <ListboxOption key={option.id} option={option} />
+          ))}
+        </ListboxOptions>
+      </HeadlessUIListbox>
+    </nav>
+  )
+
   function handleChange(slug: string) {
     updateHash(slug)
 
     const sectionHash = getHashFromSlug(slug)
     scrollToSection(sectionHash)
   }
-
-  const selectedItem = coreFunctionsData.find((item) =>
-    isSectionActive(item.slug),
-  )
-
-  return (
-    <nav aria-label="Table of Contents" className="w-full max-w-sm">
-      <FormListbox
-        hideLabel
-        label="Table of Contents"
-        placeholder="Select a section"
-        options={options}
-        value={{
-          id: selectedItem?.slug || '',
-          name: selectedItem?.title || '',
-        }}
-        onChange={(option) => handleChange(option.id)}
-      />
-    </nav>
-  )
 }
