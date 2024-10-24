2827bc28c33f6842ce621742b5155ec186bbcc29
Charly Martin <charly@hey.com>
2024-10-24 15:35:52 +0100

    Remove global search and implement name search

---
+++ src/app/filecoin-plus/allocators/components/NameSearchFilter.tsx
@@ -0,0 +1,20 @@
+import type { Column } from '@tanstack/react-table'
+
+import { SearchInput } from '@/components/SearchInput'
+
+import type { Allocator } from '../schemas/AllocatorSchema'
+
+export type NameFilterProps = {
+  column: Column<Allocator>
+}
+
+export function NameSearchFilter({ column }: NameFilterProps) {
+  const columnFilterValue = column.getFilterValue()?.toString() || ''
+
+  return (
+    <SearchInput
+      query={columnFilterValue}
+      onChange={(value) => column.setFilterValue(value)}
+    />
+  )
+}
