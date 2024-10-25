dd6214fcf66839b89116c9e5227a61afbee36ff7
Charly Martin <charly@hey.com>
2024-10-25 17:11:08 +0100

    fixup! Derive technologies from CMS

--- src/app/ecosystem-explorer/schemas/ProjectTechSchema.ts
+++ src/app/ecosystem-explorer/schemas/ProjectTechSchema.ts
@@ -4,6 +4,7 @@ import type { CMSFieldConfig } from '@/types/cmsConfig'
 
 import configJson from '@/data/cmsConfigSchema.json'
 
+import { createCategorySchema } from '@/utils/categoryUtils'
 import { findOrThrow } from '@/utils/findOrThrow'
 
 const ecosystemCollection = findOrThrow(
@@ -14,13 +15,8 @@ const ecosystemCollection = findOrThrow(
 const ecosystemFields = ecosystemCollection.fields as Array<CMSFieldConfig>
 
 const techField = findOrThrow(ecosystemFields, (field) => field.name === 'tech')
-
 const options = techField.options as unknown as Array<string>
 
-const [firstOption, ...otherOptions] = options
-const OptionEnum = z.enum([firstOption, ...otherOptions])
+const OptionValueSchema = createCategorySchema(options)
 
-export const ProjectTechSchema = z
-  .array(OptionEnum)
-  .nonempty()
-  .max(options.length)
+export const ProjectTechSchema = z.array(OptionValueSchema).nonempty()
