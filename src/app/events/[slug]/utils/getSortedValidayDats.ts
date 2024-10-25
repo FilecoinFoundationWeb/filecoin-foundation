1d272eebdcc91ff8414399d1e68aaaaad273d935
Mirha Masala <mirha@hey.com>
2024-10-25 10:43:27 +0100

    Ensure consistent UTC handling in event sorting for valid days

--- src/app/events/[slug]/utils/getSortedValidayDats.ts
+++ src/app/events/[slug]/utils/getSortedValidayDats.ts
@@ -1,3 +1,5 @@
+import { UTCDate } from '@date-fns/utc'
+
 import type { Event } from '../../types/eventType'
 
 export function getSortedValidDays(schedule: NonNullable<Event['schedule']>) {
@@ -6,8 +8,11 @@ export function getSortedValidDays(schedule: NonNullable<Event['schedule']>) {
     .map((day) => ({
       ...day,
       events: day.events.sort(
-        (a, b) => new Date(a.start).getTime() - new Date(b.start).getTime(),
+        (a, b) =>
+          new UTCDate(a.start).getTime() - new UTCDate(b.start).getTime(),
       ),
     }))
-    .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
+    .sort(
+      (a, b) => new UTCDate(a.date).getTime() - new UTCDate(b.date).getTime(),
+    )
 }
