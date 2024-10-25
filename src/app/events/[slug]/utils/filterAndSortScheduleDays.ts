dbc2a7dd8710aba0d6acb52da5ab7d68361e777b
Mirha Masala <26620750+mirhamasala@users.noreply.github.com>
2024-10-25 12:19:32 +0100

    [UXIT-1664] Events Page - Fix Event Schedule Days Sorting (#757)
    * Filter and sort schedule days with events by date and time
    
    * Create getSortedValidDays util function
    
    * Ensure consistent UTC handling in event sorting for valid days
    
    * Refactor
    
    * Update src/app/events/[slug]/utils/filterAndSortScheduleDays.ts
    
    Co-authored-by: Charly MARTIN <charly@hey.com>
    
    * CR
    
    ---------
    
    Co-authored-by: Charly MARTIN <charly@hey.com>

---
+++ src/app/events/[slug]/utils/filterAndSortScheduleDays.ts
@@ -0,0 +1,27 @@
+import { UTCDate } from '@date-fns/utc'
+import { compareAsc } from 'date-fns'
+
+import type { Event } from '../../types/eventType'
+
+export function filterAndSortScheduleDays(
+  schedule: NonNullable<Event['schedule']>,
+) {
+  const daysWithEvents = schedule.days.filter((day) => day.events.length > 0)
+
+  const daysWithEventsSortedByTime = daysWithEvents.map((day) => ({
+    ...day,
+    events: day.events.toSorted((a, b) => {
+      const startA = new UTCDate(a.start)
+      const startB = new UTCDate(b.start)
+      return compareAsc(startA, startB)
+    }),
+  }))
+
+  const sortedDays = daysWithEventsSortedByTime.toSorted((a, b) => {
+    const dateA = new UTCDate(a.date)
+    const dateB = new UTCDate(b.date)
+    return compareAsc(dateA, dateB)
+  })
+
+  return sortedDays
+}
