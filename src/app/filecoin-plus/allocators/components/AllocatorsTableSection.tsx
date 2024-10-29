c9900244615c5cc23e42705bb37518f9e194cc94
Charly Martin <charly@hey.com>
2024-10-29 13:33:07 +0000

    fixup! Remove dummy data

--- src/app/filecoin-plus/allocators/components/AllocatorsTableSection.tsx
+++ src/app/filecoin-plus/allocators/components/AllocatorsTableSection.tsx
@@ -8,16 +8,20 @@ import { NoDataAvailableMessage } from './NoDataAvailableMessage'
 export async function AllocatorsTableSection() {
   try {
     const allocators = await getAllocators()
+
     if (!allocators.length) {
       return <NoDataAvailableMessage />
     }
+
     return <AllocatorsTableWithFilters data={allocators} />
   } catch (error) {
     console.error('Error fetching or validating allocators:', error)
+
     Sentry.captureException(error, {
       tags: { component: 'AllocatorsTableSection' },
       extra: { message: 'Error fetching or validating allocators' },
     })
+
     return <NoDataAvailableMessage />
   }
 }
