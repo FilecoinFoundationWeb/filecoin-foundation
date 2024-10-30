e5beb058563931da3a9f7060a2a3e69e21a6f143
Charly MARTIN <charly@hey.com>
2024-10-30 16:16:59 +0000

    [UXIT-1638] Allocators Sort (#765)
    * Implement select filters for region and type
    
    * Rename extractUniqueValues
    
    * fixup! Implement select filters for region and type
    
    * fixup! Implement select filters for region and type
    
    * Create getColumnOrThrow utils
    
    * Rework SelectFilter
    
    * Rework createUniqueOptionsFromData
    
    * Move mb-6 to parent
    
    * Pass table to AllocatorsTableFilters
    
    * fixup! Rework SelectFilter
    
    * WIP
    
    * Clean
    
    * Remove dummy data
    
    * fixup! Remove dummy data
    
    * Fix sorting with explicit argument
    
    * Rework sort config
    
    * Simplify sort config
    
    ---------
    
    Co-authored-by: Mirha Masala <26620750+mirhamasala@users.noreply.github.com>

--- src/app/filecoin-plus/allocators/components/AllocatorsTableFilters.tsx
+++ src/app/filecoin-plus/allocators/components/AllocatorsTableFilters.tsx
@@ -1,11 +1,13 @@
 import type { Table } from '@tanstack/react-table'
 
+import { NAME_COLUMN_SORT_CONFIG } from '../constants/sorting'
 import type { Allocator } from '../schemas/AllocatorSchema'
 import { generateSortedOptions } from '../utils/generateSortedOptions'
 import { getColumnOrThrow } from '../utils/getColumnOrThrow'
 
 import { SearchFilter } from './SearchFilter'
 import { SelectFilter } from './SelectFilter'
+import { SelectSort } from './SelectSort'
 
 type AllocatorsTableFiltersProps = {
   table: Table<Allocator>
@@ -28,18 +30,25 @@ export function AllocatorsTableFilters({ table }: AllocatorsTableFiltersProps) {
       </div>
 
       <div className="flex shrink-0 grow gap-4">
-        <div className="w-1/2 sm:w-40 md:w-44 lg:w-56">
+        <div className="w-1/2 sm:w-36">
           <SelectFilter
             column={typeColumn}
             options={typeOptions}
             defaultOptionLabel="All Types"
           />
         </div>
-        <div className="w-1/2 sm:w-40 md:w-44 lg:w-56">
+        <div className="w-1/2 sm:w-36">
           <SelectFilter
             column={locationColumn}
             options={locationOptions}
             defaultOptionLabel="All Regions"
+          />
+        </div>
+        <div className="shrink md:min-w-60">
+          <SelectSort
+            column={nameColumn}
+            options={NAME_COLUMN_SORT_CONFIG.options}
+            defaultOption={NAME_COLUMN_SORT_CONFIG.defaultOption}
           />
         </div>
       </div>
