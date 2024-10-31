36667d4e6573e2682643d13e8580babf73065f2a
Barbara Peric <b.peric10@gmail.com>
2024-10-31 12:37:23 +0000

    remove tailwind class

--- tailwind.config.js
+++ tailwind.config.js
@@ -127,12 +127,6 @@ module.exports = {
             paddingRight: theme('padding.10'),
           },
         },
-        '.touch-target': {
-          padding: theme('padding.2'),
-        },
-        '.offset-touch-target': {
-          margin: `-${theme('padding.2')}`,
-        },
       })
     }),
   ],
