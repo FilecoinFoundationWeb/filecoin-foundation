95b67c78054d9e8ff1ae99c6ddad59bfcc279daa
Charly MARTIN <charly@hey.com>
2024-11-05 11:43:41 +0000

    [UXIT-1678] Add Allocators Table Tooltips (#785)
    * Add Radix popover as click tooltip
    
    - Added Radix Tailwind styles
    - Added Radix popover configured as a click-based tooltip
    
    * Add HeaderWithTooltip on allocators table
    
    * Scout clean: Add focus state to table
    
    * Renaming
    
    * Rename Tooltip component
    
    * Remove tooltip.css file
    
    * Rename touchTarget
    
    * Rename SPs to Storage Providers
    
    * Make AllocatorsTable auto layout
    
    * Rework TableHeaderWithTooltip
    
    * Remove default gap of 8 px
    
    * Extract animations in separate scss files.
    
    * CR
    
    ---------
    
    Co-authored-by: Mirha Masala <mirha@hey.com>

--- src/app/filecoin-plus/allocators/data/allocatorsTableColumnsData.tsx
+++ src/app/filecoin-plus/allocators/data/allocatorsTableColumnsData.tsx
@@ -1,5 +1,7 @@
 import { createColumnHelper } from '@tanstack/react-table'
 
+import { TooltipHeader } from '@/components/Table/TooltipHeader'
+
 import { ApplyLink } from '../components/ApplyLink'
 import type { Allocator } from '../schemas/AllocatorSchema'
 
@@ -9,46 +11,44 @@ export const allocatorsTableColumnsData = [
   columnHelper.accessor('name', {
     header: 'Organization Name',
     cell: (info) => info.getValue(),
-    meta: {
-      headerCellStyle: 'w-72 sm:w-80 lg:w-96',
-      bodyCellStyle: 'truncate',
-    },
     filterFn: 'includesString',
   }),
   columnHelper.accessor('metapathway_type', {
-    header: 'Type',
+    header: () => (
+      <TooltipHeader
+        title="Type"
+        description="The allocation method used to distribute DataCap to clients."
+      />
+    ),
     cell: (info) => info.getValue(),
-    meta: {
-      headerCellStyle: 'w-44',
-    },
     filterFn: 'includesString',
   }),
   columnHelper.accessor('location', {
     header: 'Region',
     cell: (info) => info.getValue(),
-    meta: {
-      headerCellStyle: 'w-44',
-    },
     filterFn: 'includesString',
   }),
   columnHelper.accessor('application.required_replicas', {
-    header: 'Required Replicas',
+    header: () => (
+      <TooltipHeader
+        title="Required Replicas"
+        description="The minimum number of data copies that must be stored."
+      />
+    ),
     cell: (info) => info.getValue(),
-    meta: {
-      headerCellStyle: 'w-44',
-    },
   }),
   columnHelper.accessor('application.required_sps', {
-    header: 'Required SPs',
+    header: () => (
+      <TooltipHeader
+        title="Required Storage Providers"
+        description="The minimum number of distinct storage providers needed for data storage."
+      />
+    ),
     cell: (info) => info.getValue(),
-    meta: {
-      headerCellStyle: 'w-44',
-    },
   }),
   columnHelper.accessor('application.allocation_bookkeeping', {
     header: 'Apply',
     meta: {
-      headerCellStyle: 'w-32',
       bodyCellStyle: 'text-brand-300',
     },
     cell: (info) => {
