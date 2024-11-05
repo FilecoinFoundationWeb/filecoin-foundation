bc64c41ea7b0d35b781a793e40e2538c4730d2ec
Barbara Peric <b.peric10@gmail.com>
2024-11-05 12:41:53 +0000

    refactor getDatacap stats function

--- src/app/filecoin-plus/allocators/utils/getDatacapStats.ts
+++ src/app/filecoin-plus/allocators/utils/getDatacapStats.ts
@@ -1,16 +1,25 @@
+import { API_URLS } from '@/constants/apiUrls'
+
 import { DatacapSchema, type Datacap } from '../schemas/DatacapSchema'
 
-const api = 'https://api.datacapstats.io/api/getVerifiers'
+async function fetchDatacapStats(): Promise<any> {
+  const response = await fetch(API_URLS.datacapStats)
+  if (!response.ok) {
+    throw new Error(`Failed to fetch datacap stats, status: ${response.status}`)
+  }
+  return response.json()
+}
+
+function parseDatacapData(data: any): Array<Datacap> {
+  return data.map((item: any) => {
+    return DatacapSchema.parse(item)
+  })
+}
 
 export async function getDatacapStats(): Promise<Array<Datacap>> {
   try {
-    const response = await fetch(api)
-    const { data } = await response.json()
-    const datacapStats = data.map((item: any) => {
-      return DatacapSchema.parse(item)
-    })
-
-    return datacapStats
+    const { data } = await fetchDatacapStats()
+    return parseDatacapData(data)
   } catch (error) {
     console.error(error)
     return []
