510f0b4958ba3ee3d797c33c1e9fcfe78849c6db
Mirha Masala <mirha@hey.com>
2024-11-06 09:07:13 +0000

    Use MarkdownPage component

--- src/app/employee-privacy-policy/page.tsx
+++ src/app/employee-privacy-policy/page.tsx
@@ -4,29 +4,21 @@ import { attributes, body } from '@/content/pages/employee-privacy-policy.md'
 
 import { createMetadata } from '@/utils/createMetadata'
 
-import { MarkdownContent } from '@/components/MarkdownContent'
-import { PageHeader } from '@/components/PageHeader'
-import { StructuredDataScript } from '@/components/StructuredDataScript'
+import { MarkdownPage } from '@/components/MarkdownPage'
 
 import { generateStructuredData } from './utils/generateStructuredData'
 
-const { header, seo } = attributes
-
 export const metadata = createMetadata({
-  seo,
+  seo: attributes.seo,
   path: PATHS.EMPLOYEE_PRIVACY_POLICY.path,
 })
 
 export default function EmployeePrivacyPolicy() {
-  const { title } = header
-
   return (
-    <article>
-      <StructuredDataScript structuredData={generateStructuredData(seo)} />
-      <header className="mb-6">
-        <PageHeader.Title>{title}</PageHeader.Title>
-      </header>
-      <MarkdownContent>{body}</MarkdownContent>
-    </article>
+    <MarkdownPage
+      attributes={attributes}
+      body={body}
+      generateStructuredData={generateStructuredData}
+    />
   )
 }
--- src/app/terms-of-use/page.tsx
+++ src/app/terms-of-use/page.tsx
@@ -4,26 +4,21 @@ import { attributes, body } from '@/content/pages/terms-of-use.md'
 
 import { createMetadata } from '@/utils/createMetadata'
 
-import { MarkdownContent } from '@/components/MarkdownContent'
-import { PageHeader } from '@/components/PageHeader'
-import { StructuredDataScript } from '@/components/StructuredDataScript'
+import { MarkdownPage } from '@/components/MarkdownPage'
 
 import { generateStructuredData } from './utils/generateStructuredData'
 
-const { header, seo } = attributes
-
-export const metadata = createMetadata({ seo, path: PATHS.TERMS_OF_USE.path })
+export const metadata = createMetadata({
+  seo: attributes.seo,
+  path: PATHS.TERMS_OF_USE.path,
+})
 
 export default function Terms() {
-  const { title } = header
-
   return (
-    <article>
-      <StructuredDataScript structuredData={generateStructuredData(seo)} />
-      <header className="mb-6">
-        <PageHeader.Title>{title}</PageHeader.Title>
-      </header>
-      <MarkdownContent>{body}</MarkdownContent>
-    </article>
+    <MarkdownPage
+      attributes={attributes}
+      body={body}
+      generateStructuredData={generateStructuredData}
+    />
   )
 }
