7b6ef5276aeb25ad743c89c88e2af28a1bb409b9
Mirha Masala <mirha@hey.com>
2024-11-06 09:04:24 +0000

    Create MarkdownPage component

---
+++ src/app/_components/MarkdownPage.tsx
@@ -0,0 +1,37 @@
+import type { WithContext, Thing } from 'schema-dts'
+
+import type { PageAttributes } from '@/types/attributesTypes'
+
+import type { SeoMetadata } from '@/schemas/SeoMetadataSchema'
+
+import { MarkdownContent } from '@/components/MarkdownContent'
+import { PageHeader } from '@/components/PageHeader'
+import { StructuredDataScript } from '@/components/StructuredDataScript'
+
+type MarkdownPageProps = {
+  attributes: PageAttributes
+  body: string
+  children?: React.ReactNode
+  generateStructuredData: (seo: SeoMetadata) => WithContext<Thing>
+}
+
+export function MarkdownPage({
+  attributes,
+  body,
+  generateStructuredData,
+
+  children,
+}: MarkdownPageProps) {
+  const { header, seo } = attributes
+
+  return (
+    <article>
+      <StructuredDataScript structuredData={generateStructuredData(seo)} />
+      <header className="mb-6">
+        <PageHeader.Title>{header.title}</PageHeader.Title>
+      </header>
+      <MarkdownContent>{body}</MarkdownContent>
+      {children}
+    </article>
+  )
+}
--- src/app/security/coordinated-disclosure-policy/page.tsx
+++ src/app/security/coordinated-disclosure-policy/page.tsx
@@ -7,30 +7,22 @@ import {
 
 import { createMetadata } from '@/utils/createMetadata'
 
-import { MarkdownContent } from '@/components/MarkdownContent'
-import { PageHeader } from '@/components/PageHeader'
-import { StructuredDataScript } from '@/components/StructuredDataScript'
+import { MarkdownPage } from '@/components/MarkdownPage'
 
 import { generateStructuredData } from './utils/generateStructuredData'
 
-const { header, seo } = attributes
-
 export const metadata = createMetadata({
-  seo,
+  seo: attributes.seo,
   path: PATHS.COORDINATED_DISCLOSURE_POLICY.path,
   overrideDefaultTitle: true,
 })
 
 export default function CoordinatedDisclosurePolicy() {
-  const { title } = header
-
   return (
-    <article>
-      <StructuredDataScript structuredData={generateStructuredData(seo)} />
-      <header className="mb-6">
-        <PageHeader.Title>{title}</PageHeader.Title>
-      </header>
-      <MarkdownContent>{body}</MarkdownContent>
-    </article>
+    <MarkdownPage
+      attributes={attributes}
+      body={body}
+      generateStructuredData={generateStructuredData}
+    />
   )
 }
