931f63b9c9c74916d16fbd2c60c3ec187e84d811
Barbara Peric <b.peric10@gmail.com>
2024-11-07 10:30:33 +0000

    refactor ListboxButton component and set width in FilterContainer

--- src/app/_components/ListboxButton.tsx
+++ src/app/_components/ListboxButton.tsx
@@ -5,22 +5,10 @@ import { CaretDown } from '@phosphor-icons/react/dist/ssr'
 
 import { Icon, type IconProps } from '@/components/Icon'
 
-type ListboxButtonWrapperProps = {
-  children: React.ReactNode
-}
-
 type ListboxButtonProps = {
   text: string
   prefixIcon?: IconProps['component']
   isCompact?: boolean
-}
-
-function ListboxButtonWrapper({ children }: ListboxButtonWrapperProps) {
-  return (
-    <HeadlessUIListboxButton className="inline-flex w-full items-center justify-between gap-2 rounded-lg border border-brand-300 bg-brand-800 p-3 text-brand-300 focus:brand-outline hover:border-current hover:text-brand-400">
-      {children}
-    </HeadlessUIListboxButton>
-  )
 }
 
 export function ListboxButton({
@@ -29,20 +17,21 @@ export function ListboxButton({
   isCompact,
 }: ListboxButtonProps) {
   return (
-    <ListboxButtonWrapper>
+    <HeadlessUIListboxButton className="inline-flex w-full items-center justify-between gap-2 rounded-lg border border-brand-300 bg-brand-800 p-3 text-brand-300 focus:brand-outline hover:border-current hover:text-brand-400">
       <div
         className={`${
-          isCompact ? 'hidden md:flex' : 'flex'
-        } w-full items-center justify-between gap-2 md:inline-flex`}
+          isCompact ? 'hidden' : 'flex'
+        } w-full items-center justify-between gap-2`}
       >
-        {prefixIcon && <Icon component={prefixIcon} />}
-        <span className="min-w-0 truncate">{text}</span>
+        <div className="flex w-3/4 gap-2">
+          {prefixIcon && <Icon component={prefixIcon} />}
+          <span className="min-w-0 truncate">{text}</span>
+        </div>
         <Icon component={CaretDown} size={16} weight="bold" />
       </div>
-
-      <div className={`${isCompact ? 'flex md:hidden' : 'hidden'}`}>
+      <div className={`${isCompact ? 'flex' : 'hidden'}`}>
         <Icon component={prefixIcon ?? CaretDown} />
       </div>
-    </ListboxButtonWrapper>
+    </HeadlessUIListboxButton>
   )
 }
