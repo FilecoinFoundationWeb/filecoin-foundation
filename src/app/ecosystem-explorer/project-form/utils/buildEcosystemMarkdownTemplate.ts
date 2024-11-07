520113da33e1ee87cfa55cd8cdce89b95dd18eea
Charly Martin <charly@hey.com>
2024-11-07 17:37:52 +0000

    Rework buildEcosystemMarkdownTemplate

--- src/app/ecosystem-explorer/project-form/utils/buildEcosystemMarkdownTemplate.ts
+++ src/app/ecosystem-explorer/project-form/utils/buildEcosystemMarkdownTemplate.ts
@@ -1,13 +1,16 @@
 import type { EcosystemProject } from '@/ecosystem-explorer/types/ecosystemProjectType'
 
+type Subcategory = EcosystemProject['subcategories'][number]
+type Tech = EcosystemProject['tech'][number]
+
 export type MarkdownTemplateParams = {
   fullName: EcosystemProject['fullName']
   email: EcosystemProject['email']
   title: EcosystemProject['title']
   image: NonNullable<EcosystemProject['image']>
   category: EcosystemProject['category']
-  subcategories: NonNullable<EcosystemProject['subcategories']>
-  tech: EcosystemProject['tech']
+  subcategories: Array<Subcategory>
+  tech: Array<Tech>
   description: EcosystemProject['description']
   content: NonNullable<EcosystemProject['content']>
   yearJoined: NonNullable<EcosystemProject['yearJoined']>
@@ -15,73 +18,55 @@ export type MarkdownTemplateParams = {
   createdOn: EcosystemProject['createdOn']
   updatedOn: NonNullable<EcosystemProject['updatedOn']>
   publishedOn: NonNullable<EcosystemProject['publishedOn']>
-  // Optional values
   videoUrl?: EcosystemProject['videoUrl']
   repo?: EcosystemProject['repo']
   twitter?: EcosystemProject['twitter']
   featuredContent?: EcosystemProject['featuredContent']
 }
 
-type OptionalValues = {
-  repo: MarkdownTemplateParams['repo']
-  'video-url': MarkdownTemplateParams['videoUrl']
-  twitter: MarkdownTemplateParams['twitter']
-  'featured-content': MarkdownTemplateParams['featuredContent']
-}
-
 export function buildEcosystemMarkdownTemplate(data: MarkdownTemplateParams) {
   const cleanDescription = stripLineBreaks(data.description)
 
   return `---
-${renderValue('title', data.title)}
-${renderValue('created-on', data.createdOn.toISOString())}
-${renderValue('updated-on', data.updatedOn.toISOString())}
-${renderValue('published-on', data.publishedOn.toISOString())}
-${renderValue('email', data.email)}
-${renderValue('full-name', data.fullName)}
+${renderLine('title', data.title)}
+${renderLine('created-on', data.createdOn.toISOString())}
+${renderLine('updated-on', data.updatedOn.toISOString())}
+${renderLine('published-on', data.publishedOn.toISOString())}
+${renderLine('email', data.email)}
+${renderLine('full-name', data.fullName)}
 image:
-  ${renderValue('src', data.image.src)}
-${renderValue('category', data.category)}
+  ${renderLine('src', data.image.src)}
+${renderLine('category', data.category)}
 ${renderArray('subcategories', data.subcategories)}
-${renderValue('description', cleanDescription)}
-${renderValue('website', data.website)}
 ${renderArray('tech', data.tech)}
-${renderValue('year-joined', data.yearJoined.toISOString())}
-${renderOptionalValues({
-  repo: data.repo,
-  'video-url': data.videoUrl,
-  twitter: data.twitter,
-  'featured-content': data.featuredContent,
-})}
+${renderLine('description', cleanDescription)}
+${renderLine('website', data.website)}
+${renderLine('year-joined', data.yearJoined.toISOString())}
+${renderOptionalLine('repo', data.repo)}
+${renderOptionalLine('video-url', data.videoUrl)}
+${renderOptionalLine('twitter', data.twitter)}
+${renderOptionalLine('featured-content', data.featuredContent)}
 seo:
-  ${renderValue('title', data.title)}
-  ${renderValue('description', cleanDescription)}
+  ${renderLine('title', data.title)}
+  ${renderLine('description', cleanDescription)}
 ---
 
 ${data.content.trim()}
 `
 }
 
-function renderValue(key: string, value: string) {
+function renderLine(key: string, value: string) {
   return `${key}: ${value.trim()}`
+}
+
+function renderOptionalLine(key: string, value: string | undefined) {
+  if (!value) return ''
+  return renderLine(key, value)
 }
 
 function renderArray(key: string, array: Array<string>) {
   if (array.length === 0) return ''
   return `${key}:\n${array.map((item) => `  - ${item.trim()}`).join('\n')}`
-}
-
-function renderOptionalValues(values: OptionalValues) {
-  const entries = Object.entries(values)
-  const truthyValueEntries = entries.filter(([, value]) => Boolean(value))
-
-  if (truthyValueEntries.length === 0) {
-    return ''
-  }
-
-  return truthyValueEntries
-    .map(([key, value]) => renderValue(key, value!))
-    .join('\n')
 }
 
 function stripLineBreaks(value: string) {
