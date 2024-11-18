f57b96aaef544525ba95bd5d8233fb41d56fb05f
Charly Martin <charly@hey.com>
2024-11-18 17:30:32 +0000

    Add touch target to FormCheckbox

--- src/app/_components/Form/FormCheckbox.tsx
+++ src/app/_components/Form/FormCheckbox.tsx
@@ -1,5 +1,6 @@
 import { Checkbox, type CheckboxProps, Field, Label } from '@headlessui/react'
 import { Check } from '@phosphor-icons/react/dist/ssr'
+import { clsx } from 'clsx'
 
 import { Icon } from '@/components/Icon'
 
@@ -7,20 +8,50 @@ type ExcludedHeadlessUIProps = 'className'
 
 export type FormCheckboxProps = {
   label: string
+  labelSize?: 'sm' | 'md'
 } & Omit<CheckboxProps, ExcludedHeadlessUIProps>
 
-export function FormCheckbox({ label, ...rest }: FormCheckboxProps) {
+const TOUCH_TARGET = {
+  base: 'size-5',
+  augmentingClass: 'p-3.5',
+  offsettingClass: '-m-3.5',
+}
+
+export function FormCheckbox({
+  label,
+  labelSize = 'md',
+  ...rest
+}: FormCheckboxProps) {
   return (
     <Field className="flex items-center gap-3">
       <Checkbox
         {...rest}
-        className="group size-5 rounded bg-brand-100 p-0.5 text-brand-100 focus:brand-outline data-[disabled]:cursor-not-allowed data-[checked]:bg-brand-400"
+        className={clsx(
+          'group cursor-pointer focus:outline-none',
+          TOUCH_TARGET.augmentingClass,
+          TOUCH_TARGET.offsettingClass,
+        )}
       >
-        <span className="hidden group-data-[checked]:block">
-          <Icon component={Check} size={16} weight="bold" />
-        </span>
+        <div
+          className={clsx(
+            TOUCH_TARGET.base,
+            'rounded bg-brand-100 p-0.5 text-brand-100 group-focus:brand-outline group-data-[disabled]:cursor-not-allowed group-data-[checked]:bg-brand-400',
+          )}
+        >
+          <span className="hidden group-data-[checked]:block">
+            <Icon component={Check} size={16} weight="bold" />
+          </span>
+        </div>
       </Checkbox>
-      <Label className="text-brand-100">{label}</Label>
+      <Label
+        className={clsx(
+          'text-brand-100',
+          labelSize === 'md' && 'text-base',
+          labelSize === 'sm' && 'text-sm',
+        )}
+      >
+        {label}
+      </Label>
     </Field>
   )
 }
