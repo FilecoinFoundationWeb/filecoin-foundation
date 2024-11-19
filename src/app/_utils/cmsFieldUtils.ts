9332012d41b3857a3885fb0eb0fe672e624b5bfa
Barbara Peric <b.peric10@gmail.com>
2024-11-19 13:27:46 +0000

    refactor cmsFieldUtils

--- src/app/_utils/cmsFieldUtils.ts
+++ src/app/_utils/cmsFieldUtils.ts
@@ -2,24 +2,23 @@ import { type CMSCollectionName, type CMSFieldOption } from '@/types/cmsConfig'
 
 import { getCMSFieldOptions, getCollectionConfig } from '@/utils/cmsConfigUtils'
 
-function formatFieldOptions(options: Array<CMSFieldOption>) {
+type FieldParams = {
+  collectionName: CMSCollectionName
+  fieldName: string
+}
+
+export function getFieldSettings({ collectionName, fieldName }: FieldParams) {
+  const { fields } = getCollectionConfig(collectionName)
+  const cmsOptions = getCMSFieldOptions(fields, fieldName)
+  const options = mapCMSOptionsToComponentFormat(cmsOptions)
+  const validIds = options.map((setting) => setting.id)
+
+  return { options, validIds }
+}
+
+function mapCMSOptionsToComponentFormat(options: Array<CMSFieldOption>) {
   return options.map((option) => ({
     id: option.value,
     name: option.label,
   }))
-}
-
-export function getFieldSettings({
-  collectionName,
-  fieldName,
-}: {
-  collectionName: CMSCollectionName
-  fieldName: string
-}) {
-  const { fields } = getCollectionConfig(collectionName)
-  const rawOptions = getCMSFieldOptions(fields, fieldName)
-  const options = formatFieldOptions(rawOptions)
-  const validIds = options.map((setting) => setting.id)
-
-  return { options, validIds }
 }
