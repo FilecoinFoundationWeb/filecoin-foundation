e94c63a5d7b543278de429da0304a9fd12724364
Mirha Masala <mirha@hey.com>
2024-11-20 10:15:36 +0000

    CR - Refine LocationSchema

--- src/app/events/schemas/FrontMatterSchema.ts
+++ src/app/events/schemas/FrontMatterSchema.ts
@@ -5,6 +5,7 @@ import { createEnumSchema } from '@/utils/zodUtils'
 
 import { DynamicBaseDataSchema } from '@/schemas/DynamicDataBaseSchema'
 
+import { LocationSchema } from './LocationSchema'
 import { ScheduleSchema } from './ScheduleSchema'
 import { SpeakersSchema } from './SpeakerSchema'
 import { SponsorsSchema } from './SponsorSchema'
@@ -13,24 +14,14 @@ const { validIds: validCategoryIds } = getFieldSettings({
   collectionName: 'event_entries',
   fieldName: 'category',
 })
-const { validIds: validRegionIds } = getFieldSettings({
-  collectionName: 'event_entries',
-  fieldName: 'location.region',
-})
 
 const CategorySchema = createEnumSchema(validCategoryIds)
-const RegionSchema = createEnumSchema(validRegionIds)
 
 export const EventFrontMatterSchema = DynamicBaseDataSchema.extend({
   title: z.string(),
   category: CategorySchema,
   description: z.string().optional(),
-  location: z
-    .object({
-      primary: z.string(),
-      region: RegionSchema.optional(),
-    })
-    .strict(),
+  location: LocationSchema,
   'external-link': z
     .object({
       url: z.string().url(),
