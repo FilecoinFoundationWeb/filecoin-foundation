1c56791759cea8189bdbbfec0e699db9dbf516c0
Barbara Peric <b.peric10@gmail.com>
2024-12-04 10:44:31 -0500

    Add aditional analytics and refactor

--- src/app/not-found-analytics.tsx
+++ src/app/not-found-analytics.tsx
@@ -1,17 +1,20 @@
 'use client'
 
-import Script from 'next/script'
+import { useEffect } from 'react'
 
 import { usePlausible } from 'next-plausible'
 
-export default function NotFoundAnalytics() {
+export function NotFoundAnalytics() {
   const plausible = usePlausible()
 
-  return (
-    <Script
-      onReady={() =>
-        plausible('404', { props: { path: window.location.pathname } })
-      }
-    />
-  )
+  useEffect(() => {
+    plausible('404', {
+      props: {
+        path: window.location.pathname,
+        referrer: document.referrer,
+      },
+    })
+  }, [plausible])
+
+  return null
 }
