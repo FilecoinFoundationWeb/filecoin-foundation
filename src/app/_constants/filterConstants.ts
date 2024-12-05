4d3b7e145faa440c107d508a6727d36762480a36
Charly Martin <charly@hey.com>
2024-12-05 11:29:49 +0000

    Add LocationFilter component and add related constants

--- src/app/_constants/filterConstants.ts
+++ src/app/_constants/filterConstants.ts
@@ -4,3 +4,8 @@ export const DEFAULT_CATEGORY_FILTER_OPTION = {
   id: DEFAULT_FILTER_ID,
   name: 'All Categories',
 } as const
+
+export const DEFAULT_LOCATION_FILTER_OPTION = {
+  id: DEFAULT_FILTER_ID,
+  name: 'All Locations',
+} as const
