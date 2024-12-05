20d6fa8c75496792373d5deb8099229fbe2f2fab
Charly Martin <charly@hey.com>
2024-12-05 11:27:52 +0000

    Rename DEFAULT_FILTER_OPTION to DEFAULT_CATEGORY_FILTER_OPTION

--- src/app/_constants/filterConstants.ts
+++ src/app/_constants/filterConstants.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_FILTER_ID = 'all'
 
-export const DEFAULT_FILTER_OPTION = {
+export const DEFAULT_CATEGORY_FILTER_OPTION = {
   id: DEFAULT_FILTER_ID,
   name: 'All Categories',
 } as const
--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -5,7 +5,7 @@ import { MagnifyingGlass } from '@phosphor-icons/react/dist/ssr'
 
 import type { NextServerSearchParams } from '@/types/searchParams'
 
-import { DEFAULT_FILTER_OPTION } from '@/constants/filterConstants'
+import { DEFAULT_CATEGORY_FILTER_OPTION } from '@/constants/filterConstants'
 import { PATHS } from '@/constants/paths'
 import { CATEGORY_KEY } from '@/constants/searchParams'
 
@@ -109,7 +109,7 @@ export default function Events({ searchParams }: Props) {
   const categoryOptionsWithCount = useListboxOptions({
     collectionName: 'event_entries',
     fieldName: 'category',
-    defaultOption: DEFAULT_FILTER_OPTION,
+    defaultOption: DEFAULT_CATEGORY_FILTER_OPTION,
     entries: searchResults,
   })
 
