ef5772ae6cb99d913a7a70481b646066d445b50f
Charly Martin <charly@hey.com>
2024-12-05 14:26:41 +0000

    Add location matching utility to filterUtils

--- src/app/_utils/filterUtils.ts
+++ src/app/_utils/filterUtils.ts
@@ -2,9 +2,22 @@ type WithCategory = {
   category: string
 }
 
+type WithLocation = {
+  location: {
+    region?: string
+  }
+}
+
 export function entryMatchesCategoryQuery<Entry extends WithCategory>(
   entry: Entry,
   query?: string,
 ) {
   return entry.category === query
+}
+
+export function entryMatchesLocationQuery<Entry extends WithLocation>(
+  entry: Entry,
+  query?: string,
+) {
+  return entry.location.region === query
 }
