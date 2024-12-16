11c94bf77d9cfbc44c912753592c6de018ab250c
Mirha Masala <mirha@hey.com>
2024-12-16 15:32:53 +0000

    Refactor variant styles

--- src/app/_components/TagLabel.tsx
+++ src/app/_components/TagLabel.tsx
@@ -2,27 +2,34 @@ import { clsx } from 'clsx'
 
 import { Icon, type IconProps } from './Icon'
 
+const styles = {
+  base: 'inline-flex max-w-fit gap-1 rounded-lg border bg-brand-800 px-2 py-1 text-xs font-semibold capitalize text-brand-100',
+  variants: {
+    primary: {
+      border: 'border-brand-100',
+    },
+    secondary: {
+      border: 'border-brand-500',
+    },
+    callout: {
+      border: 'border-red-400',
+    },
+  },
+} as const
+
+type TagVariant = keyof typeof styles.variants
+
 export type TagProps = {
-  borderColor?: 'brand-100' | 'brand-500'
+  variant?: TagVariant
   icon?: IconProps['component']
   children: string
 }
 
-const borderStyles = {
-  'brand-100': 'border-brand-100',
-  'brand-500': 'border-brand-500',
-}
-
-export function TagLabel({
-  borderColor = 'brand-500',
-  icon,
-  children,
-}: TagProps) {
-  const baseStyles =
-    'inline-flex max-w-fit gap-1 rounded-lg border bg-brand-800 px-2 py-1 text-xs font-semibold capitalize text-brand-100'
+export function TagLabel({ variant = 'secondary', icon, children }: TagProps) {
+  const { base, variants } = styles
 
   return (
-    <span className={clsx(baseStyles, borderStyles[borderColor])}>
+    <span className={clsx(base, variants[variant].border)}>
       {icon && <Icon component={icon} size={16} />}
       {children}
     </span>
