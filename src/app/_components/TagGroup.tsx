c91059557b6bbc3c3301823885e2405dc0f7f9cc
Mirha Masala <mirha@hey.com>
2024-12-17 05:26:01 +0000

    Refactor - Simplify TagGroup

--- src/app/_components/TagGroup.tsx
+++ src/app/_components/TagGroup.tsx
@@ -2,26 +2,20 @@ import { TagGroupContainer } from '@/components/TagComponents/TagGroupContainer'
 import { type TagProps, TagLabel } from '@/components/TagLabel'
 
 export type TagGroupProps = {
-  labels: TagProps['children'] | Array<TagProps['children']>
+  labels: TagProps['children'] | [TagProps['children'], TagProps['children']]
 }
 
 export function TagGroup({ labels }: TagGroupProps) {
-  const labelsArray = (Array.isArray(labels) ? labels : [labels]).filter(
-    Boolean,
-  )
+  const normalizedLabels = Array.isArray(labels) ? labels : [labels]
 
-  if (labelsArray.length === 0) {
-    throw new Error('At least one label must be provided.')
-  }
-
-  if (labelsArray.length === 1) {
-    return <TagLabel>{labelsArray[0]}</TagLabel>
+  if (normalizedLabels.length === 1) {
+    return <TagLabel>{normalizedLabels[0]}</TagLabel>
   }
 
   return (
     <TagGroupContainer>
-      {labelsArray.map((label, index) => (
-        <TagLabel key={label} variant={index === 0 ? 'primary' : 'secondary'}>
+      {normalizedLabels.map((label, index) => (
+        <TagLabel key={index} variant={index === 0 ? 'primary' : 'secondary'}>
           {label}
         </TagLabel>
       ))}
