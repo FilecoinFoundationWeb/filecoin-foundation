ccd330d832bf71c4b435d6cac83868acf28dca5c
Mirha Masala <mirha@hey.com>
2024-12-18 13:24:55 +0000

    Nit - Destructure event

--- src/app/events/[slug]/components/ScheduleSection/EventDetails.tsx
+++ src/app/events/[slug]/components/ScheduleSection/EventDetails.tsx
@@ -10,42 +10,48 @@ import { formatTime } from '../../utils/dateUtils'
 
 import { Participants } from './Participants'
 
-export function EventDetails(event: Event) {
+export function EventDetails({
+  start,
+  end,
+  location,
+  tag,
+  title,
+  description,
+  moderators,
+  speakers,
+  url,
+}: Event) {
   return (
     <BasicCard>
       <div className="grid gap-6 lg:grid-cols-3">
         <div className="flex gap-6 text-brand-300 lg:flex-col lg:gap-1">
           <div className="text-sm font-bold">
-            <span>{formatTime(event.start)}</span>
-            {event.end && <span> – {formatTime(event.end)}</span>}
+            <span>{formatTime(start)}</span>
+            {end && <span> – {formatTime(end)}</span>}
           </div>
-          <span className="text-sm">{event.location}</span>
+          <span className="text-sm">{location}</span>
         </div>
 
         <div className="space-y-4 lg:col-span-2">
-          {event.tag && <TagLabel>{event.tag}</TagLabel>}
+          {tag && <TagLabel>{tag}</TagLabel>}
 
           <div className="space-y-2">
             <Heading tag="h3" variant="lg">
-              {event.title}
+              {title}
             </Heading>
 
-            {event.description && (
-              <p className="max-w-readable">{event.description}</p>
+            {description && <p className="max-w-readable">{description}</p>}
+
+            {moderators && (
+              <Participants title="Moderator" participants={moderators} />
             )}
 
-            {event.moderators && (
-              <Participants title="Moderator" participants={event.moderators} />
-            )}
-
-            {event.speakers && (
-              <Participants title="Speaker" participants={event.speakers} />
+            {speakers && (
+              <Participants title="Speaker" participants={speakers} />
             )}
           </div>
 
-          {event.url && (
-            <SmartTextLink href={event.url}>View Details</SmartTextLink>
-          )}
+          {url && <SmartTextLink href={url}>View Details</SmartTextLink>}
         </div>
       </div>
     </BasicCard>
