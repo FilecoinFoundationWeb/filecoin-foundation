c073557f93476637440c37881f54cc4409b158d5
Mirha Masala <26620750+mirhamasala@users.noreply.github.com>
2024-12-19 15:42:15 +0000

    [UXIT-1889] Events Page · Adapt Past Event Tag (#918)
    * Update event tag label for concluded events to 'past'
    
    * Create TabGroupContainer component
    
    * Refactor variant styles
    
    * Use TagGroupContainer
    
    * CR
    
    * Refactor - Default to primary variant
    
    * Refactor using TagGroupContainer
    
    * Rename label to labels to better reflect its purpose
    
    * Refactor - Simplify TagGroup
    
    * [UXIT-1889] Scout PR · Move Governance Calendar Cards Logic [skip percy] (#920)
    
    * Move CalelanderCards to local Governance folder
    
    * Move Calender component to own file
    
    * fixup! Move CalelanderCards to local Governance folder
    
    * Abstract CalendarCard
    
    * User EventType
    
    * Move schemas
    
    * Move getCalendarEvents
    
    * CR
    
    * CR
    
    * Apply suggestions from code review
    
    Co-authored-by: Barbara Peric <50910606+barbaraperic@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Barbara Peric <50910606+barbaraperic@users.noreply.github.com>

--- src/app/blog/[slug]/components/BlogPostHeader.tsx
+++ src/app/blog/[slug]/components/BlogPostHeader.tsx
@@ -4,7 +4,7 @@ import { getCategoryLabel } from '@/utils/categoryUtils'
 import { formatDate } from '@/utils/dateUtils'
 
 import { ArticleHeader } from '@/components/ArticleHeader'
-import { TagLabel } from '@/components/TagLabel'
+import { TagLabel } from '@/components/TagComponents/TagLabel'
 
 import type { BlogPost } from '../../types/blogPostType'
 
@@ -29,7 +29,9 @@ export function BlogPostHeader({
       <TagLabel>
         {getCategoryLabel({ collectionName: 'blog_posts', category })}
       </TagLabel>
+
       <ArticleHeader.Title>{title}</ArticleHeader.Title>
+
       {publishedOn && (
         <span className="inline-block text-blue-300">
           {formatDate(publishedOn)}
--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -203,7 +203,7 @@ export default function Events({ searchParams }: Props) {
                           title={title}
                           borderColor="brand-400"
                           textIsClamped={true}
-                          tagLabel={tagLabel}
+                          tags={[{ text: tagLabel }]}
                           metaData={getMetaData({
                             startDate,
                             endDate,
