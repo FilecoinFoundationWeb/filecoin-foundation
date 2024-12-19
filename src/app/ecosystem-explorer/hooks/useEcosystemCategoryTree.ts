57e65bec789460df64f02506058d2480d6962a5f
Charly Martin <charly@hey.com>
2024-12-19 15:48:21 +0000

    Rework useEcosystemCategoryTree

--- src/app/ecosystem-explorer/hooks/useEcosystemCategoryTree.ts
+++ src/app/ecosystem-explorer/hooks/useEcosystemCategoryTree.ts
@@ -1,44 +1,55 @@
 import { useMemo } from 'react'
 
+import { categoryGroupMap } from '../constants/categoryGroupMap'
 import type { EcosystemProject } from '../types/ecosystemProjectType'
 import { getEcosystemCMSCategories } from '../utils/getEcosystemCMSCategories'
 
 type UseEcosystemCategoryProps<Entry extends EcosystemProject> = {
-  categories: ReturnType<typeof getEcosystemCMSCategories>['categories']
-  subcategories: ReturnType<typeof getEcosystemCMSCategories>['subcategories']
+  categories: ReturnType<typeof getEcosystemCMSCategories>
   entries: Array<Entry>
 }
+
 export function useEcosystemCategoryTree<Entry extends EcosystemProject>({
   entries,
   categories,
-  subcategories,
 }: UseEcosystemCategoryProps<Entry>) {
-  const entriesPerSubcategory = useMemo(() => {
-    const counts = new Map()
+  const entriesPerCategory = useMemo(() => {
+    const counts = new Map<string, number>()
 
-    entries.forEach((entry) => {
-      const subcategory = entry.subcategory
-      counts.set(subcategory, (counts.get(subcategory) || 0) + 1)
+    entries.forEach(({ category }) => {
+      counts.set(category, (counts.get(category) || 0) + 1)
     })
 
     return counts
   }, [entries])
 
-  const categoryTree = useMemo(
+  const categoriesWithCount = useMemo(
     () =>
       categories.map(({ value, label }) => ({
         value,
         label,
-        subcategories: subcategories
-          .filter(({ parent_category }) => parent_category === value)
-          .map(({ value, label }) => ({
-            value,
-            label,
-            count: entriesPerSubcategory.get(value) || 0,
-          })),
+        count: entriesPerCategory.get(value) || 0,
       })),
-    [categories, subcategories, entriesPerSubcategory],
+    [categories, entriesPerCategory],
   )
 
+  const categoryTree = useMemo(() => {
+    const groupedCategories = Object.groupBy(categoriesWithCount, (item) =>
+      getCategoryGroupNameOrThrow(item.value),
+    )
+
+    return Object.entries(groupedCategories)
+  }, [categoriesWithCount])
+
   return categoryTree
+}
+
+function getCategoryGroupNameOrThrow(category: string) {
+  const categoryGroupName = categoryGroupMap.get(category)
+
+  if (!categoryGroupName) {
+    throw new Error(`categoryGroupName not found for category: ${category}`)
+  }
+
+  return categoryGroupName
 }
