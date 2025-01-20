6b37ede182b0fdfcba0fc39a441cf4b6b9b8375a
Charly Martin <charly@hey.com>
2025-01-20 14:54:33 +0000

    Refactor ErrorTester component to simulate global error after a delay

--- apps/site/src/app/_components/ErrorTester.tsx
+++ apps/site/src/app/_components/ErrorTester.tsx
@@ -5,7 +5,9 @@ import { useEffect } from 'react'
 export function ErrorTester() {
   useEffect(() => {
     // This will bubble up to global-error.tsx
-    throw new Error('Simulated global error')
+    setTimeout(() => {
+      throw new Error('Simulated global error')
+    }, 1_000)
   }, [])
 
   return null
