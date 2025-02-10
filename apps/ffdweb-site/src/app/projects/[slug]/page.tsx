6e9b24ea341c16203e6a715e18070c6ff496ca60
Mirha Masala <mirha@hey.com>
2025-02-10 14:52:47 +0000

    Scaffold Projects Pages

---
+++ apps/ffdweb-site/src/app/projects/[slug]/page.tsx
@@ -0,0 +1,24 @@
+export default function Project() {
+  return (
+    <>
+      <header>
+        <h1></h1>
+      </header>
+
+      <section>
+        <h2>About the Project</h2>
+        <p></p>
+      </section>
+
+      <section>
+        <h2>Partnership Details</h2>
+        <p></p>
+      </section>
+
+      <div>
+        <a href="#">Website</a>
+        <a href="#">Read Blog Post</a>
+      </div>
+    </>
+  )
+}
