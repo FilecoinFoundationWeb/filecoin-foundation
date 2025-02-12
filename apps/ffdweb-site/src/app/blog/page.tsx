d99a44451013bae29854e0495e2e5c592f27a78c
Mirha Masala <26620750+mirhamasala@users.noreply.github.com>
2025-02-12 16:04:08 +0100

    [FFDW][UXIT-2107] Scaffold Pages [skip percy] (#1087)
    * Include homepage text
    
    * Scaffold About Page
    
    * Scaffold Projects Pages
    
    * Scaffold Learning Resources Page
    
    * Scaffold Blog Pages
    
    * Scaffold FAQ Page
    
    * Scaffold Digest Pages
    
    * Scaffold Terms of Use page
    
    * Scaffold Privacy Policy page
    
    * Fix CMS featured entry paths (#1075)
    
    * Update featured entry paths to use full directory paths
    
    * Update file path validation in fileUtils to use full workspace root path
    
    * Fix Ecosystem Explorer path
    
    * Update Event “filecoin-consensus-hong-kong” (#1089)
    
    * Update Event “filecoin-ethdenver-2025” (#1082)
    
    * Update Event “filecoin-ethdenver-2025”
    
    * Update Event “filecoin-ethdenver-2025”
    
    * Clean photos
    
    * Minor formatting fixes
    
    ---------
    
    Co-authored-by: Charly Martin <charly@hey.com>
    
    * [UXIT-2092] Fix Github Action (#1086)
    
    * Test: reviewers show in the message
    
    * Test: show reviewers
    
    * Fix: remove sync
    
    * CR
    
    ---------
    
    Co-authored-by: Charly MARTIN <charly@hey.com>
    Co-authored-by: elsanguyen1 <109809478+elsanguyen1@users.noreply.github.com>
    Co-authored-by: YuliiaFilecoin <yuliia@fil.org>
    Co-authored-by: Barbara Peric <50910606+barbaraperic@users.noreply.github.com>

---
+++ apps/ffdweb-site/src/app/blog/page.tsx
@@ -0,0 +1,13 @@
+export default function Blog() {
+  return (
+    <>
+      <section>
+        <span>Featured</span>
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        <time>Lorem ipsum dolor sit amet.</time>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <button>Read Featured Post</button>
+      </section>
+    </>
+  )
+}
---
+++ apps/ffdweb-site/src/app/terms-of-use/page.tsx
@@ -0,0 +1,7 @@
+export default function TermsOfUse() {
+  return (
+    <>
+      <h1>Terms of Use</h1>
+    </>
+  )
+}
