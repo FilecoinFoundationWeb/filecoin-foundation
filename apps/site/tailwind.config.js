b7b498045f0d40d22927399fff5091cbf8a494b5
Mirha Masala <mirha@hey.com>
2025-02-14 06:17:53 +0000

    Working version

--- apps/site/tailwind.config.js
+++ apps/site/tailwind.config.js
@@ -2,7 +2,10 @@ const plugin = require('tailwindcss/plugin')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: [
+    './src/**/**/*.{js,ts,jsx,tsx,mdx}',
+    '../../packages/ui/src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {
       colors: {
