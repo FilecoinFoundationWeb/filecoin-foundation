352e210b8eea1c5ac623d4d1bcfad68de71a3371
Charly Martin <charly@hey.com>
2025-02-17 13:07:54 +0200

    Enforce strict validation for event base frontmatter schema

--- apps/site/src/app/events/schemas/EventBaseFrontmatterSchema.ts
+++ apps/site/src/app/events/schemas/EventBaseFrontmatterSchema.ts
@@ -1,9 +1,11 @@
 import { z } from 'zod'
 
-export const EventBaseFrontmatterSchema = z.object({
-  title: z.string(),
-  'start-date': z.coerce.date(),
-  'end-date': z.coerce.date().optional(),
-  description: z.string().optional(),
-  'external-link': z.string().url().optional(),
-})
+export const EventBaseFrontmatterSchema = z
+  .object({
+    title: z.string(),
+    'start-date': z.date(),
+    'end-date': z.date().optional(),
+    description: z.string().optional(),
+    'external-link': z.string().url().optional(),
+  })
+  .strict()
