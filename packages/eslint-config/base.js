862c5f7bbe702d50ab763bb91e9c1a5100f7f7c0
Mirha Masala <26620750+mirhamasala@users.noreply.github.com>
2025-02-20 15:06:01 +0000

    Update packages/eslint-config/base.js
    Co-authored-by: Charly MARTIN <charly@hey.com>

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js'
+import eslintJs from '@eslint/js'
+import eslintTs from 'typescript-eslint'
 import eslintConfigPrettier from 'eslint-config-prettier'
-import turboPlugin from 'eslint-plugin-turbo'
-import tseslint from 'typescript-eslint'
-import onlyWarn from 'eslint-plugin-only-warn'
+import eslintPluginTurbo from 'eslint-plugin-turbo'
+import eslintPluginOnlyWarn from 'eslint-plugin-only-warn'
 
 export const config = [
   js.configs.recommended,
