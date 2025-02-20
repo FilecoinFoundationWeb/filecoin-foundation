d26abf7b3ffef1e752ddb62741dec4ee80dc90b0
Charly Martin <charly@hey.com>
2025-02-20 15:35:49 +0200

    Rename thisYear variable to currentYear in YearJoinedSchema

--- apps/site/src/app/ecosystem-explorer/schemas/YearJoinedSchema.ts
+++ apps/site/src/app/ecosystem-explorer/schemas/YearJoinedSchema.ts
@@ -2,10 +2,10 @@ import { z } from 'zod'
 
 import { FILECOIN_CREATION_YEAR } from '@/ecosystem-explorer/project-form/utils/getYearOptions'
 
-const thisYear = new Date().getFullYear()
+const currentYear = new Date().getFullYear()
 
 export const YearJoinedSchema = z
   .number()
   .int()
   .min(FILECOIN_CREATION_YEAR)
-  .max(thisYear)
+  .max(currentYear)
