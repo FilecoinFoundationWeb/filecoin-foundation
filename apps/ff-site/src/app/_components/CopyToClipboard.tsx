532239ed537459efe042094a1c1ebb9136e6b2c8
Barbara Peric <50910606+barbaraperic@users.noreply.github.com>
2025-02-24 17:14:47 +0100

    [UXIT-1701] Add tooltips to Share Post icons (#1137)
    * feat: tooltip and share icons, rename popover, and accessibility improvements for notification dialog
    
    * fix: renaming in allocatorsData

--- apps/ff-site/src/app/_components/CopyToClipboard.tsx
+++ apps/ff-site/src/app/_components/CopyToClipboard.tsx
@@ -14,6 +14,7 @@ import { NOTIFICATION_DIALOG_DURATION_MS } from '@/constants/notificationDialogD
 
 import { Icon } from '@/components/Icon'
 import { NotificationDialog } from '@/components/NotificationDialog/NotificationDialog'
+import { Tooltip } from '@/components/Tooltip/Tooltip'
 
 type CopyToClipboardProps = {
   text: string
@@ -51,15 +52,17 @@ export function CopyToClipboard({
         setIsOpen={setIsCopied}
         title={notificationTitle}
       />
-      <Button
-        className={clsx(
-          'focus:brand-outline hover:text-brand-400',
-          TOUCH_TARGET.touchAreaPadding,
-        )}
-        onClick={() => handleCopy(text)}
-      >
-        <Icon component={Link} size={32} weight="light" />
-      </Button>
+      <Tooltip description="Copy link to clipboard" side="bottom">
+        <Button
+          className={clsx(
+            'hover:text-brand-400 focus:brand-outline',
+            TOUCH_TARGET.touchAreaPadding,
+          )}
+          onClick={() => handleCopy(text)}
+        >
+          <Icon component={Link} size={32} weight="light" />
+        </Button>
+      </Tooltip>
     </>
   )
 }
