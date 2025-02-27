f8e93e0a2e761a85bcff712e5dff127b4a164618
Mirha Masala <mirha@hey.com>
2025-02-27 09:44:05 +0000

    Move NoResultMessage components

--- packages/ui/src/NoResultsMessage.tsx
+++ packages/ui/src/NoResultsMessage.tsx
@@ -2,13 +2,13 @@ import { Button } from '@filecoin-foundation/ui/Button'
 import { Heading } from '@filecoin-foundation/ui/Heading'
 import { Icon, type IconProps } from '@filecoin-foundation/ui/Icon'
 
-import type { CTAProps } from '@/types/ctaType'
+import type { CTAProps } from '@filecoin-foundation/utils/types/ctaType'
 
-import { BASE_DOMAIN } from '@/constants/siteMetadata'
 type NoResultsMessageProps = {
   icon: IconProps['component']
   title: string
   message: string
+  baseDomain: string
   cta?: CTAProps
 }
 
@@ -16,11 +16,12 @@ export function NoResultsMessage({
   icon,
   title,
   message,
+  baseDomain,
   cta,
 }: NoResultsMessageProps) {
   return (
-    <div className="flex flex-col items-center gap-4 text-brand-200">
-      <span className="grid size-16 place-items-center rounded-full bg-brand-700 text-brand-300">
+    <div className="text-brand-200 flex flex-col items-center gap-4">
+      <span className="bg-brand-700 text-brand-300 grid size-16 place-items-center rounded-full">
         <Icon component={icon} />
       </span>
       <Heading tag="h3" variant="xl">
@@ -28,7 +29,7 @@ export function NoResultsMessage({
       </Heading>
       <p className="max-w-md text-center">{message}</p>
       {cta && (
-        <Button href={cta.href} baseDomain={BASE_DOMAIN}>
+        <Button href={cta.href} baseDomain={baseDomain}>
           {cta.text}
         </Button>
       )}
