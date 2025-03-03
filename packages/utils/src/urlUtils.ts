689e75e4051e8662357cf9873f3a93c1f6e528c8
Charly Martin <charly@hey.com>
2025-03-03 09:34:06 +0000

    fixup! Refactor search params types and imports across project

--- packages/utils/src/urlUtils.ts
+++ packages/utils/src/urlUtils.ts
@@ -1,11 +1,11 @@
-type Params = Record<string, string | string[] | undefined>
+import type { QueryParams } from './types/urlTypes'
 
-export function normalizeQueryParam(params: Params, key: string) {
+export function normalizeQueryParam(params: QueryParams, key: string) {
   const param = getFirstQueryParam(params, key)
   return param?.trim().toLowerCase()
 }
 
-function getFirstQueryParam(params: Params, key: string) {
+function getFirstQueryParam(params: QueryParams, key: string) {
   const param = params[key]
   return Array.isArray(param) ? param[0] : param
 }
