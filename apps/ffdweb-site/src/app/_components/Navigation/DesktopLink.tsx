d1d9b47864ce88590446ce637083b41e2d3ba066
Charly Martin <charly@hey.com>
2025-03-03 16:17:07 +0000

    Adds desktop navigation

---
+++ apps/ffdweb-site/src/app/_components/Navigation/DesktopLink.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import Link, { type LinkProps } from 'next/link'
+import { usePathname } from 'next/navigation'
+
+type DesktopLinkProps = {
+  label: string
+  href: LinkProps['href']
+}
+
+export function DesktopLink({ href, label, ...rest }: DesktopLinkProps) {
+  const pathname = usePathname()
+  const isActive = pathname.startsWith(href.toString())
+
+  return (
+    <Link
+      href={href}
+      aria-label={`Go to ${label} page`}
+      className="focus:brand-outline aria-current:text-brand-primary-300 px-4 py-2.5 text-neutral-200 hover:bg-neutral-800"
+      aria-current={isActive ? 'true' : 'false'}
+      {...rest}
+    >
+      {label}
+    </Link>
+  )
+}
