63fb8b8257490840c877e8d55b7362e1b08241d3
Barbara Peric <b.peric10@gmail.com>
2025-03-04 12:07:25 +0000

    feat: add privacy policy and terms of use

--- apps/ffdweb-site/src/app/privacy-policy/page.tsx
+++ apps/ffdweb-site/src/app/privacy-policy/page.tsx
@@ -1,11 +1,24 @@
+import { StructuredDataScript } from '@filecoin-foundation/ui/StructuredDataScript'
+
 import { createMetadata } from '@/utils/createMetadata'
 
+import { generateStructuredData } from '@/blog/utils/generateStructuredData'
+
 export default function PrivacyPolicy() {
-  return <h1>Privacy Policy</h1>
+  return (
+    <>
+      <StructuredDataScript structuredData={generateStructuredData(seo)} />
+      <h1>Privacy Policy</h1>
+    </>
+  )
 }
 
-export const metadata = createMetadata({
+const seo = {
   metaTitle: 'Privacy Policy',
   metaDescription:
     'Discover how Filecoin Foundation safeguards your data. Read our comprehensive Privacy Policy for detailed information.',
+}
+
+export const metadata = createMetadata({
+  ...seo,
 })
--- apps/ffdweb-site/src/app/terms-of-use/page.tsx
+++ apps/ffdweb-site/src/app/terms-of-use/page.tsx
@@ -1,15 +1,24 @@
+import { StructuredDataScript } from '@filecoin-foundation/ui/StructuredDataScript'
+
 import { createMetadata } from '@/utils/createMetadata'
+
+import { generateStructuredData } from '@/blog/utils/generateStructuredData'
 
 export default function TermsOfUse() {
   return (
     <>
+      <StructuredDataScript structuredData={generateStructuredData(seo)} />
       <h1>Terms of Use</h1>
     </>
   )
 }
 
-export const metadata = createMetadata({
+const seo = {
   metaTitle: 'Terms of Use',
   metaDescription:
     'Understand the terms and conditions of using Filecoin Foundation services. Read our detailed Terms of Use for more information.',
+}
+
+export const metadata = createMetadata({
+  ...seo,
 })
