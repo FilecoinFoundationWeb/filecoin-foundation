1e73fbed3d1f31b36f1e76d19db51bd76714ba5d
Barbara Peric <b.peric10@gmail.com>
2025-03-04 12:01:14 +0000

    refactor: move constants to shared library and reuse seo description from homepage

---
+++ packages/utils/src/constants/structuredDataConstants.ts
@@ -0,0 +1,5 @@
+export const SCHEMA_CONTEXT_URL = 'https://schema.org'
+export const SCHEMA_EVENT_ATTENDANCE_MODE_ONLINE_URL =
+  'https://schema.org/OnlineEventAttendanceMode'
+export const SCHEMA_EVENT_ATTENDANCE_MODE_OFFLINE_URL =
+  'https://schema.org/OfflineEventAttendanceMode'
