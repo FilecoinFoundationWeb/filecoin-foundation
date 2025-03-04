64f8c07e6750e570174474c3e401a68d5dde9d54
Mirha Masala <26620750+mirhamasala@users.noreply.github.com>
2025-03-04 12:51:40 +0100

    [FFDW][UXIT-2291][UXIT-2292] Create MarkdownPage component [skip percy] (#1174)
    * Extract MarkdownPage component to shared UI library
    
    * Markdown handling
    
    * Make optional
    
    * Remove heading
    
    * Use MarkdownPage component
    
    * CR

--- apps/ffdweb-site/src/app/privacy-policy/page.tsx
+++ apps/ffdweb-site/src/app/privacy-policy/page.tsx
@@ -1,11 +1,29 @@
+import { MarkdownPage } from '@filecoin-foundation/ui/MarkdownPage'
+
 import { createMetadata } from '@/utils/createMetadata'
 
-export default function PrivacyPolicy() {
-  return <h1>Privacy Policy</h1>
-}
+import { MarkdownContent } from '@/components/MarkdownContent'
+import { PageHeader } from '@/components/PageHeader'
 
-export const metadata = createMetadata({
+import privacyPolicyMarkdown from './privacy-policy.md'
+
+const { body } = privacyPolicyMarkdown
+
+const SEO = {
   metaTitle: 'Privacy Policy',
   metaDescription:
     'Discover how Filecoin Foundation safeguards your data. Read our comprehensive Privacy Policy for detailed information.',
+} as const
+
+export default function PrivacyPolicy() {
+  return (
+    <MarkdownPage
+      PageHeaderTitle={<PageHeader.Title>{SEO.metaTitle}</PageHeader.Title>}
+      MarkdownContent={<MarkdownContent>{body}</MarkdownContent>}
+    />
+  )
+}
+
+export const metadata = createMetadata({
+  ...SEO,
 })
--- apps/ffdweb-site/src/app/terms-of-use/page.tsx
+++ apps/ffdweb-site/src/app/terms-of-use/page.tsx
@@ -1,15 +1,29 @@
+import { MarkdownPage } from '@filecoin-foundation/ui/MarkdownPage'
+
 import { createMetadata } from '@/utils/createMetadata'
+
+import { MarkdownContent } from '@/components/MarkdownContent'
+import { PageHeader } from '@/components/PageHeader'
+
+import termsOfUseMarkdown from './terms-of-use.md'
+
+const { body } = termsOfUseMarkdown
+
+const SEO = {
+  metaTitle: 'Terms of Use',
+  metaDescription:
+    'Understand the terms and conditions of using Filecoin Foundation services. Read our detailed Terms of Use for more information.',
+} as const
 
 export default function TermsOfUse() {
   return (
-    <>
-      <h1>Terms of Use</h1>
-    </>
+    <MarkdownPage
+      PageHeaderTitle={<PageHeader.Title>{SEO.metaTitle}</PageHeader.Title>}
+      MarkdownContent={<MarkdownContent>{body}</MarkdownContent>}
+    />
   )
 }
 
 export const metadata = createMetadata({
-  metaTitle: 'Terms of Use',
-  metaDescription:
-    'Understand the terms and conditions of using Filecoin Foundation services. Read our detailed Terms of Use for more information.',
+  ...SEO,
 })
