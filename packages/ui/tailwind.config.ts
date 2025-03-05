3b8b6591469ebedda35f9ee43e00bb3484fd6e5a
Barbara Peric <b.peric10@gmail.com>
2025-03-05 16:34:59 +0000

    Revert "feat: tailwind config"
    This reverts commit 60233fac24260cd091535fe66401849bb32f69db.

--- packages/ui/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import sharedConfig from '@filecoin-foundation/tailwind-config'
-import type { Config } from 'tailwindcss'
-
-const config: Pick<Config, 'prefix' | 'presets' | 'content'> = {
-  content: ['./src/**/*.tsx'],
-  presets: [sharedConfig],
-}
-
-export default config
