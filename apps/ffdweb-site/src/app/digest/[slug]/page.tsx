8e9fb3ffb16a104fea2ec41d8ea7ec65bf27f711
Mirha Masala <mirha@hey.com>
2025-03-06 16:31:10 +0000

    Create DigestArticle

--- apps/ffdweb-site/src/app/digest/[slug]/page.tsx
+++ apps/ffdweb-site/src/app/digest/[slug]/page.tsx
@@ -1,35 +1,60 @@
+import { ArticleLayout } from '@filecoin-foundation/ui/Article/ArticleLayout'
+import { DigestArticleHeader } from '@filecoin-foundation/ui/DigestArticleHeader'
 import { PageLayout } from '@filecoin-foundation/ui/PageLayout'
 
-// import { ArticleLayout } from '@filecoin-foundation/ui/Article/ArticleLayout'
-// import { DigestArticleHeader } from '@filecoin-foundation/ui/DigestArticleHeader'
-
-// import { graphicsData } from '@/data/graphicsData'
+import { graphicsData } from '@/data/graphicsData'
 
 import { createMetadata } from '@/utils/createMetadata'
 
-// import { MarkdownContent } from '@/components/MarkdownContent'
+import { MarkdownContent } from '@/components/MarkdownContent'
 
-export default function DigestArticle() {
-  return <PageLayout gap="large">Digest Article</PageLayout>
-  // return (
-  //   <ArticleLayout>
-  //     <DigestArticleHeader
-  //       title={title}
-  //       issueNumber={issueNumber}
-  //       articleNumber={articleNumber}
-  //       authors={authors}
-  //       image={{
-  //         ...(image || graphicsData.imageFallback.data),
-  //         alt: '',
-  //       }}
-  //     />
-  //     {content && <MarkdownContent>{content}</MarkdownContent>}
-  //   </ArticleLayout>
-  // )
+import {
+  getDigestArticleData,
+  getDigestArticlesData,
+} from '../utils/getDigestArticleData'
+
+type DigestArticleProps = {
+  params: Promise<SlugParams>
 }
 
-export const metadata = createMetadata({
-  metaTitle: '', // [Headline of Article] | FFDW
-  metaDescription: '', // [Article Standfirst]
-  overrideTitle: true,
-})
+export default async function DigestArticle(props: DigestArticleProps) {
+  const { slug } = await props.params
+  const data = await getDigestArticleData(slug)
+
+  const { title, issueNumber, articleNumber, image, authors, content } = data
+
+  return (
+    <PageLayout>
+      {/* <StructuredDataScript structuredData={generateStructuredData(data)} /> */}
+      <ArticleLayout>
+        <DigestArticleHeader
+          title={title}
+          issueNumber={issueNumber}
+          articleNumber={articleNumber}
+          authors={authors}
+          image={{
+            ...(image || graphicsData.imageFallback.data),
+            alt: '',
+          }}
+        />
+        {content && <MarkdownContent>{content}</MarkdownContent>}
+      </ArticleLayout>
+    </PageLayout>
+  )
+}
+
+export async function generateStaticParams() {
+  const entries = await getDigestArticlesData()
+  return entries.map(({ slug }) => ({ slug }))
+}
+
+export async function generateMetadata(props: DigestArticleProps) {
+  const { slug } = await props.params
+  const data = await getDigestArticleData(slug)
+
+  return createMetadata({
+    metaTitle: `${data.title} | FFDW`,
+    metaDescription: data.seo.description,
+    overrideTitle: true,
+  })
+}
