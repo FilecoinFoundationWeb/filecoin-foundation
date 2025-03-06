b2160bef194527770ce4df0779d9dbf04eb0c879
Mirha Masala <mirha@hey.com>
2025-03-06 09:56:03 +0000

    Move CardGrid component to shared UI library

---
+++ packages/ui/src/CardGrid.tsx
@@ -0,0 +1,45 @@
+import { clsx } from 'clsx'
+
+type GridColumnConfig =
+  | 'smTwoLgThree'
+  | 'smTwo'
+  | 'mdTwo'
+  | 'lgTwo'
+  | 'smThree'
+  | 'mdThree'
+  | 'lgThree'
+
+type CardGridProps = {
+  cols: GridColumnConfig
+  hasGridAutoRows?: boolean
+  as?: React.ElementType
+  children: React.ReactNode
+}
+
+const baseGridStyles = 'grid grid-cols-1 gap-4'
+const extendedGridStyles: Record<GridColumnConfig, string> = {
+  smTwoLgThree: 'sm:grid-cols-2 sm:gap-6 lg:grid-cols-3',
+  smTwo: 'sm:grid-cols-2 sm:gap-6',
+  mdTwo: 'md:grid-cols-2 md:gap-6',
+  lgTwo: 'lg:grid-cols-2 lg:gap-6',
+  smThree: 'sm:grid-cols-3 sm:gap-6',
+  mdThree: 'md:grid-cols-3 md:gap-6',
+  lgThree: 'lg:grid-cols-3 lg:gap-6',
+}
+
+export function CardGrid({
+  cols,
+  hasGridAutoRows = true,
+  as: Tag = 'ul',
+  children,
+}: CardGridProps) {
+  const autoRowsStyles = hasGridAutoRows && 'auto-rows-fr'
+
+  return (
+    <Tag
+      className={clsx(baseGridStyles, extendedGridStyles[cols], autoRowsStyles)}
+    >
+      {children}
+    </Tag>
+  )
+}
