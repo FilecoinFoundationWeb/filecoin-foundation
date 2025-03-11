07f16910cba9e651648d22f04dd59777b497a51d
Mirha Masala <mirha@hey.com>
2025-03-11 08:49:03 +0000

    Show project

--- apps/ffdweb-site/src/app/projects/[slug]/page.tsx
+++ apps/ffdweb-site/src/app/projects/[slug]/page.tsx
@@ -1,34 +1,55 @@
+import { ArticleLayout } from '@filecoin-foundation/ui/Article/ArticleLayout'
 import { PageLayout } from '@filecoin-foundation/ui/PageLayout'
+import { ExternalTextLink } from '@filecoin-foundation/ui/TextLink/ExternalTextLink'
 import { InternalTextLink } from '@filecoin-foundation/ui/TextLink/InternalTextLink'
+import { type SlugParams } from '@filecoin-foundation/utils/types/paramsTypes'
 
 import { createMetadata } from '@/utils/createMetadata'
 
-export default function Project() {
+import { getProjectData } from '../utils/getProjectData'
+
+type ProjectProps = {
+  params: Promise<SlugParams>
+}
+
+export default async function Project(props: ProjectProps) {
+  const { slug } = await props.params
+  const data = await getProjectData(slug)
+  const { title, image, description, externalLink, featuredContent } = data
+
   return (
     <PageLayout gap="large">
-      <header>
-        <h1></h1>
-      </header>
+      <ArticleLayout>
+        <h1>{title}</h1>
 
-      <section>
-        <h2>About the Project</h2>
-        <p></p>
-      </section>
+        {externalLink && (
+          <ExternalTextLink href={externalLink}>
+            Visit Project Website
+          </ExternalTextLink>
+        )}
 
-      <section>
-        <h2>Partnership Details</h2>
-        <p></p>
-      </section>
+        {featuredContent && (
+          <InternalTextLink href={featuredContent}>
+            Read Blog Post
+          </InternalTextLink>
+        )}
 
-      <div>
-        <InternalTextLink href="#">Website</InternalTextLink>
-        <InternalTextLink href="#">Read Blog Post</InternalTextLink>
-      </div>
+        <div className="prose">
+          <h2>About the Project</h2>
+          <p>{description}</p>
+        </div>
+      </ArticleLayout>
     </PageLayout>
   )
 }
 
-export const metadata = createMetadata({
-  metaTitle: '', // [Name of Project] | FFDW
-  metaDescription: '', // [Description of project]
-})
+export async function generateMetadata(props: ProjectProps) {
+  const { slug } = await props.params
+  const { title, description } = await getProjectData(slug)
+
+  return createMetadata({
+    metaTitle: `${title} | FFDW`,
+    metaDescription: description,
+    overrideTitle: true,
+  })
+}
