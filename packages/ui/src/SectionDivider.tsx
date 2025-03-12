9ea7f021f4b2a02bb7a9b17c3bef298c5a875e78
Charly Martin <charly@hey.com>
2025-03-12 09:42:20 +0000

    Simplify SectionDivider component styling

--- packages/ui/src/SectionDivider.tsx
+++ packages/ui/src/SectionDivider.tsx
@@ -4,11 +4,9 @@ export type SectionDividerProps = {
 
 export function SectionDivider({ title }: SectionDividerProps) {
   return (
-    <div className="flex max-h-max items-center gap-2">
-      <span className="text-brand-300 text-sm font-light uppercase">
-        {title}
-      </span>
-      <div className="bg-brand-300 h-px flex-1" />
+    <div className="section-divider flex items-center gap-2">
+      <span className="text-sm font-light uppercase">{title}</span>
+      <hr className="flex-1" />
     </div>
   )
 }
