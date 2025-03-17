52e8d1ed444c54e7b926671f97d0ca0b40160bab
Mirha Masala <mirha@hey.com>
2025-03-17 11:55:05 +0000

    CR - Use CategorySchema

--- apps/ffdweb-site/src/app/projects/schemas/ProjectFrontmatterSchema.ts
+++ apps/ffdweb-site/src/app/projects/schemas/ProjectFrontmatterSchema.ts
@@ -1,10 +1,20 @@
 import { DynamicBaseDataSchema } from '@filecoin-foundation/utils/schemas/DynamicDataBaseSchema'
+import { createEnumSchema } from '@filecoin-foundation/utils/zodUtils'
 import { z } from 'zod'
+
+import { getCMSFieldOptionsAndValidIds } from '@/utils/getCMSFieldOptionsAndValidIds'
+
+const { validIds: validCategoryIds } = getCMSFieldOptionsAndValidIds({
+  collectionName: 'projects',
+  fieldName: 'category',
+})
+
+const CategorySchema = createEnumSchema(validCategoryIds)
 
 export const ProjectFrontmatterSchema = DynamicBaseDataSchema.extend({
   title: z.string(),
+  category: CategorySchema,
   description: z.string(),
-  category: z.string(),
   website: z.string(),
   'featured-content': z.string().optional(),
   'active-partnership': z.boolean().optional().default(false),
