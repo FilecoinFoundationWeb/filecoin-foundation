5f793f412bbe48373f86a5c2c653c112e564d7dc
Charly Martin <charly@hey.com>
2025-03-17 14:21:17 +0000

    Remove unnecessary 'as const' assertions

--- apps/ffdweb-site/src/app/_constants/siteMetadata.ts
+++ apps/ffdweb-site/src/app/_constants/siteMetadata.ts
@@ -1,8 +1,7 @@
-const BASE_DOMAIN = 'ffdweb.org' as const
-const BASE_URL = `https://${BASE_DOMAIN}` as const
-const ORGANIZATION_NAME =
-  'Filecoin Foundation for the Decentralized Web' as const
-const ORGANIZATION_NAME_SHORT = 'FFDW' as const
+const BASE_DOMAIN = 'ffdweb.org'
+const BASE_URL = `https://${BASE_DOMAIN}`
+const ORGANIZATION_NAME = 'Filecoin Foundation for the Decentralized Web'
+const ORGANIZATION_NAME_SHORT = 'FFDW'
 
 const SEO = {
   title: 'Rebuilding the Internet for Good',
