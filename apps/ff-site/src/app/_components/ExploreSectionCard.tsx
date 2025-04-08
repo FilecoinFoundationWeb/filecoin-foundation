0df82fef6ee797e7e2888ef734ac3327523c0bc2
Barbara Peric <b.peric10@gmail.com>
2025-04-08 14:44:59 +0100

    style: hover bg color

--- apps/ff-site/src/app/_components/ExploreSectionCard.tsx
+++ apps/ff-site/src/app/_components/ExploreSectionCard.tsx
@@ -16,7 +16,7 @@ export function ExploreSectionCard({
   children,
 }: ExploreSectionCardProps) {
   return (
-    <li className="relative flex flex-col justify-between gap-3 rounded-lg border border-brand-500 bg-brand-700/10 p-4 backdrop-blur-md">
+    <li className="relative flex flex-col justify-between gap-3 rounded-lg border border-brand-500 bg-brand-700/10 p-4 backdrop-blur-md hover:bg-brand-400/20">
       <div className={clsx('flex flex-col gap-3', cta && 'mb-10')}>
         <Heading {...heading} />
         {children && <p>{children}</p>}
