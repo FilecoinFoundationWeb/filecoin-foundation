f865b2086fb7940aec879f90dada3ea795bb9308
Charly Martin <charly@hey.com>
2025-04-21 17:57:22 +0100

    Rename digest_slug.cy.ts

---
+++ apps/ffdweb-site/cypress/e2e/digest_slug.cy.ts
@@ -0,0 +1,30 @@
+import { PATHS } from '@/constants/paths'
+import { BASE_URL } from '@/constants/siteMetadata'
+
+import { tests } from '@/cypress/support'
+import type {
+  GenericEntryFrontmatter,
+  GetEntryOptions,
+} from '@/cypress/tasks/getEntryFrontmatter'
+import { getMetaTitleWithSuffix } from '@/cypress/utils/getMetaTitleWithSuffix'
+
+const CONTENT_FOLDER = 'digest'
+
+describe('Digest Slug Page', () => {
+  it(tests.metadata.prompt, () => {
+    cy.task<string>('getRandomSlug', CONTENT_FOLDER).then((slug) => {
+      cy.task<GenericEntryFrontmatter>('getEntryFrontmatter', {
+        collection: CONTENT_FOLDER,
+        slug,
+      } satisfies GetEntryOptions).then(({ title, seo }) => {
+        const seoTitle = seo.title || title
+        tests.metadata.fn({
+          path: `${PATHS.DIGEST.path}/${slug}`,
+          title: getMetaTitleWithSuffix(seoTitle),
+          description: seo.description,
+          baseUrl: BASE_URL,
+        })
+      })
+    })
+  })
+})
