35ddc319cc5598db8c1ce37b2b1cad6f2f91d80e
Mirha Masala <mirha@hey.com>
2025-04-21 11:46:38 +0100

    Create footer component

--- apps/uxit/src/app/_components/Footer.tsx
+++ apps/uxit/src/app/_components/Footer.tsx
@@ -1,8 +1,14 @@
+import { ExternalTextLink } from '@filecoin-foundation/ui/TextLink/ExternalTextLink'
+
 export function Footer() {
   return (
-    <footer className="mt-8 border-t border-gray-200 py-4 text-center text-sm text-gray-500">
-      <p>
-        © {new Date().getFullYear()} Filecoin Foundation. All rights reserved.
+    <footer className="fixed right-0 bottom-0 left-0 border-t border-gray-200 bg-white py-4 text-center text-sm text-slate-500">
+      <p className="text-center text-sm">
+        &copy; {new Date().getFullYear()} Filecoin Foundation. Content on this
+        site is licensed under a{' '}
+        <ExternalTextLink href="https://creativecommons.org/licenses/by/4.0/">
+          Creative Commons Attribution 4.0 International license
+        </ExternalTextLink>
       </p>
     </footer>
   )
