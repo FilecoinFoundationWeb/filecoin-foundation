9c55f1440fa36c790cb0e595e0e6a9226b991963
Mirha Masala <mirha@hey.com>
2025-04-21 11:46:28 +0100

    Create header component

---
+++ apps/uxit/src/app/_components/Header.tsx
@@ -0,0 +1,19 @@
+function Header() {
+  return (
+    <header className="flex flex-col items-center gap-5 text-center">
+      <span className="font-semibold text-indigo-800">UXIT</span>
+      <h1 className="sm:text-6x flex flex-col items-center gap-2 text-4xl font-bold tracking-tight text-slate-900">
+        User-Focused Solutions for the
+        <br />
+        <span className="text-indigo-700">Future of Filecoin</span>
+      </h1>
+      <p className="max-w-2xl text-lg leading-8 text-slate-600">
+        The Filecoin Foundation User Experience Improvement Team (UXIT) is
+        dedicated to creating thoughtful, user-centered solutions that drive the
+        evolution of the Filecoin ecosystem.
+      </p>
+    </header>
+  )
+}
+
+export default Header
