97533e44d39a2a9ffa65b47cd5378e5694a6e335
Charly Martin <charly@hey.com>
2025-04-22 14:51:54 +0100

    Use new metadata testing function

--- apps/ff-site/cypress/e2e/fil_plus_page_spec.cy.ts
+++ apps/ff-site/cypress/e2e/fil_plus_page_spec.cy.ts
@@ -1,19 +1,33 @@
 import { PATHS } from '@/constants/paths'
+import { BASE_URL } from '@/constants/siteMetadata'
 
-import { testPageMetadata } from '@/support/test-utils'
-import { verifyLinks } from '@/support/verifyLinksUtil'
+import { tests } from '@/cypress/support'
+import { verifyLinks } from '@/cypress/support/verifyLinksUtil'
+import type { PageFrontmatterSeo } from '@/cypress/tasks/getPageFrontmatterSeo'
+import { getMetaTitleTemplate } from '@/cypress/utils/getMetaTitleTemplate'
+
+const { mainContentPath, path } = PATHS.FIL_PLUS
 
 describe('Filecoin Plus Page', () => {
-  it('should check metadata', () => {
-    testPageMetadata(PATHS.FIL_PLUS)
+  it(tests.metadata.prompt, () => {
+    cy.task<PageFrontmatterSeo>('getPageFrontmatterSeo', mainContentPath).then(
+      (seo) => {
+        tests.metadata.fn({
+          path,
+          title: getMetaTitleTemplate(seo.title),
+          description: seo.description,
+          baseUrl: BASE_URL,
+        })
+      },
+    )
   })
 
   it('should check links', () => {
-    verifyLinks(PATHS.FIL_PLUS.path)
+    verifyLinks(path)
   })
 
   it('should match visual snapshot', () => {
-    cy.visit(PATHS.FIL_PLUS.path)
+    cy.visit(path)
     cy.percySnapshot()
   })
 })
--- apps/ff-site/cypress/e2e/grants_page_spec.cy.ts
+++ apps/ff-site/cypress/e2e/grants_page_spec.cy.ts
@@ -1,21 +1,32 @@
 import { PATHS } from '@/constants/paths'
+import { BASE_URL } from '@/constants/siteMetadata'
 
-import { testPageMetadata } from '@/support/test-utils'
-import { verifyLinks } from '@/support/verifyLinksUtil'
+import { tests } from '@/cypress/support'
+import { verifyLinks } from '@/cypress/support/verifyLinksUtil'
+import type { PageFrontmatterSeo } from '@/cypress/tasks/getPageFrontmatterSeo'
+
+const { mainContentPath, path } = PATHS.GRANTS
 
 describe('Grants Page', () => {
-  it('should check metadata', () => {
-    testPageMetadata(PATHS.GRANTS, {
-      overrideDefaultTitle: true,
-    })
+  it(tests.metadata.prompt, () => {
+    cy.task<PageFrontmatterSeo>('getPageFrontmatterSeo', mainContentPath).then(
+      (seo) => {
+        tests.metadata.fn({
+          path,
+          title: seo.title,
+          description: seo.description,
+          baseUrl: BASE_URL,
+        })
+      },
+    )
   })
 
   it('should check links', () => {
-    verifyLinks(PATHS.GRANTS.path)
+    verifyLinks(path)
   })
 
   it('should match visual snapshot', () => {
-    cy.visit(PATHS.GRANTS.path)
+    cy.visit(path)
     cy.percySnapshot()
   })
 })
