40c5a485ee4c4a760f725bef39d06cd37fbe3100
Mirha Masala <mirha@hey.com>
2025-04-30 09:10:11 +0100

    Set up import order for utils package

--- packages/utils/eslint.config.js
+++ packages/utils/eslint.config.js
@@ -1,3 +1,50 @@
-import { config } from '@filecoin-foundation/eslint-config/base'
+import pluginImport from 'eslint-plugin-import'
 
-export default config
+import { config as baseConfig } from '@filecoin-foundation/eslint-config/base'
+
+/** @type {import("eslint").Linter.Config} */
+export default [
+  ...baseConfig,
+  {
+    plugins: {
+      import: pluginImport,
+    },
+    rules: {
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+          ],
+          pathGroups: [
+            { pattern: '{fs,path}', group: 'builtin', position: 'before' },
+            { pattern: 'react', group: 'external', position: 'before' },
+            { pattern: 'next', group: 'external', position: 'before' },
+            {
+              pattern: '@filecoin-foundation/**',
+              group: 'external',
+              position: 'after',
+            },
+            { pattern: '#**', group: 'internal', position: 'before' },
+            {
+              pattern: '*.+(css|scss|sass|less)',
+              group: 'index',
+              position: 'after',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
+]
