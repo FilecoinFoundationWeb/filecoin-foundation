235d5578247912c11c5bebee5ee698338117bd9a
Charly MARTIN <charly@hey.com>
2025-05-08 15:33:51 +0100

    Refactor start-date and end-date to be string instead of Date (#1410)
    * Refactor start-date and end-date to be string instead of Date
    
    * Rework for clarity

--- packages/utils/src/dateUtils.ts
+++ packages/utils/src/dateUtils.ts
@@ -1,6 +1,6 @@
 import { format } from 'date-fns'
 
-export function formatDate(date: Date, formatString = 'MMM d, yyyy') {
+export function formatDate(date: Date | string, formatString = 'MMM d, yyyy') {
   return format(date, formatString)
 }
 
@@ -23,4 +23,13 @@ export function getUTCMidnightToday() {
   return new Date(
     Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate()),
   )
+}
+
+export function getTodayISODateOnly() {
+  const now = new Date()
+  return getISODateOnly(now)
+}
+
+export function getISODateOnly(date: Date) {
+  return date.toISOString().split('T')[0]
 }
