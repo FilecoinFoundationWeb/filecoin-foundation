1be1a8c7b208844c0971f075fa4d947146c057be
Mirha Masala <mirha@hey.com>
2025-05-09 10:59:01 +0100

    Revert changes

--- packages/ui/src/CardGrid.tsx
+++ packages/ui/src/CardGrid.tsx
@@ -8,7 +8,6 @@ type GridColumnConfig =
   | 'smThree'
   | 'mdThree'
   | 'lgThree'
-  | 'lgFour'
 
 type CardGridProps = {
   as: 'ul' | 'section' | 'div'
@@ -26,7 +25,6 @@ const extendedGridStyles: Record<GridColumnConfig, string> = {
   smThree: 'sm:grid-cols-3 sm:gap-6',
   mdThree: 'md:grid-cols-3 md:gap-6',
   lgThree: 'lg:grid-cols-3 lg:gap-6',
-  lgFour: 'lg:grid-cols-4 lg:gap-6',
 }
 
 export function CardGrid({
