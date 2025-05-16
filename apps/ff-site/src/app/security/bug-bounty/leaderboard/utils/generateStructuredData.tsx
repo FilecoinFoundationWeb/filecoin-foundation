5eb61064a09ea6b7cf4915b41bd9e55ffc5892c6
Mirha Masala <mirha@hey.com>
2025-05-16 13:37:24 +0100

    Create new Leaderboard page

---
+++ apps/ff-site/src/app/security/bug-bounty/leaderboard/utils/generateStructuredData.tsx
@@ -0,0 +1,16 @@
+import type { WebPage, WithContext } from 'schema-dts'
+
+import type { SeoMetadata } from '@filecoin-foundation/utils/schemas/SeoMetadataSchema'
+
+import { PATHS } from '@/constants/paths'
+
+import { generateWebPageStructuredData } from '@/utils/generateWebPageStructuredData'
+
+
+export function generateStructuredData(seo: SeoMetadata): WithContext<WebPage> {
+  return generateWebPageStructuredData({
+    title: seo.title,
+    description: seo.description,
+    path: PATHS.SECURITY_BUG_BOUNTY_LEADERBOARD.path,
+  })
+}
---
+++ apps/ff-site/src/content/pages/security/bug-bounty/leaderboard.md
@@ -0,0 +1,8 @@
+---
+header:
+  title: "Leaderboard"
+  description: "Top security researchers who have contributed to improving the Filecoin ecosystem. It recognizes participants based on the points they’ve earned by submitting valid reports and uncovering vulnerabilities."
+seo:
+  title: "Leaderboard"
+  description: "Top security researchers who have contributed to improving the Filecoin ecosystem. It recognizes participants based on the points they’ve earned by submitting valid reports and uncovering vulnerabilities."
+---
