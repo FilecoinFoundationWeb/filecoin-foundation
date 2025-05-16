9710a0c8ef383a723f41d3bb19a548c994247403
Mirha Masala <mirha@hey.com>
2025-05-16 08:52:25 +0100

    CR

--- apps/uxit/src/app/_utils/getSiteAuditReportsData.ts
+++ apps/uxit/src/app/_utils/getSiteAuditReportsData.ts
@@ -8,25 +8,27 @@ import { siteMetadataList } from '@/site-audit-reports/data/siteMetadataList'
 const publicDir = path.join(process.cwd(), 'public')
 
 export async function getSiteAuditReportsData() {
-  return siteMetadataList.map(({ id }) => {
-    const indexPath = path.join(
-      publicDir,
-      PATHS.SITE_AUDIT_REPORTS.path,
-      id,
-      'index.html',
-    )
+  return Promise.all(
+    siteMetadataList.map(async ({ id }) => {
+      const indexPath = path.join(
+        publicDir,
+        PATHS.SITE_AUDIT_REPORTS.path,
+        id,
+        'index.html',
+      )
 
-    let updatedOn: Date
-    try {
-      const stats = fs.statSync(indexPath)
-      updatedOn = stats.mtime
-    } catch {
-      updatedOn = new Date()
-    }
+      let updatedOn: Date
+      try {
+        const stats = await fs.promises.stat(indexPath)
+        updatedOn = stats.mtime
+      } catch {
+        updatedOn = new Date()
+      }
 
-    return {
-      slug: id,
-      updatedOn,
-    }
-  })
+      return {
+        slug: id,
+        updatedOn,
+      }
+    }),
+  )
 }
