34cc39f5a0d724ca92b4126324667c9b70e4a3f3
Mirha Masala <mirha@hey.com>
2025-05-20 12:53:39 +0100

    Refactor FF PageHeader - optimize conditional rendering
    Remove unnecessary div wrapper when sectionDividerTitle is not present

--- apps/ff-site/src/app/_components/PageHeader.tsx
+++ apps/ff-site/src/app/_components/PageHeader.tsx
@@ -10,13 +10,14 @@ type PageHeaderProps = SharedPageHeaderProps & {
 }
 
 export function PageHeader({ sectionDividerTitle, ...props }: PageHeaderProps) {
+  if (!sectionDividerTitle) {
+    return <SharedPageHeader {...props} />
+  }
+
   return (
     <div className="flex flex-col gap-4">
-      {sectionDividerTitle && <SectionDivider title={sectionDividerTitle} />}
+      <SectionDivider title={sectionDividerTitle} />
       <SharedPageHeader {...props} />
     </div>
   )
 }
-
-PageHeader.Title = SharedPageHeader.Title
-PageHeader.Image = SharedPageHeader.Image
