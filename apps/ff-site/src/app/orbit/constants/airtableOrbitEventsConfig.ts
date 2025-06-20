2150fb690edbb6a6cf29834c01b4562bc9301ecf
Charly Martin <charly@hey.com>
2025-06-20 15:04:37 +0100

    Update Airtable config with new FIL-B Orbit database IDs

--- apps/ff-site/src/app/orbit/constants/airtableOrbitEventsConfig.ts
+++ apps/ff-site/src/app/orbit/constants/airtableOrbitEventsConfig.ts
@@ -1,12 +1,16 @@
+// Link to table IDs
+// https://airtable.com/apprCQqrHfze7O5S7/api/docs#javascript/table:event%20request
+
 export const AIRTABLE_ORBIT_EVENTS_CONFIG = {
-  BASE_ID: 'appAGdqyYrqoFNuPI',
+  BASE_ID: 'apprCQqrHfze7O5S7',
   EVENTS_TABLE_ID: 'tblnrsxREgBikWu9A',
   FIELDS: {
+    TITLE: 'fldhD7ClKauhqeD79',
     CITY: 'fldQJZQAS4Jm0eiiL',
     START_DATE: 'flddTSgPuKgL6h1JG',
     STATUS: 'fldnRqMzqmu7VLUlD',
-    TITLE: 'fldhD7ClKauhqeD79',
     REGISTRATION_LINK: 'fldlcV4631rFgbdd6',
   },
-  APPROVED_STATUS_VALUE: 'Approved',
 } as const
+
+export const APPROVED_STATUS_VALUE = 'Approved'
