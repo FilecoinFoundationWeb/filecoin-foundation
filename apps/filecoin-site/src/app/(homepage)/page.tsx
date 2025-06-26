28d1b4944d133b739ee8fbc50b5bcd5d9ad4ad9f
Charly Martin <charly@hey.com>
2025-06-26 17:37:49 +0100

    Install Tailwind CSS

--- apps/filecoin-site/src/app/(homepage)/page.tsx
+++ apps/filecoin-site/src/app/(homepage)/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <h1>Filecoin.io V3</h1>
+  return (
+    <main className="p-24">
+      <h1 className="text-4xl font-bold">Filecoin.io V3</h1>
+      <p className="mt-4 text-lg">
+        This is the new Filecoin.io website, built with Next.js and Tailwind.
+      </p>
+    </main>
+  )
 }
