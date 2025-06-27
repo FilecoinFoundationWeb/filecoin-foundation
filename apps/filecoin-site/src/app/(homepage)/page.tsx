a6c8e58f3f245c202bc873b794acb3c8b6a87fb4
Charly MARTIN <charly@hey.com>
2025-06-27 15:02:35 +0100

    Update global styles with custom Tailwind color theme (#1506)

--- apps/filecoin-site/src/app/(homepage)/page.tsx
+++ apps/filecoin-site/src/app/(homepage)/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <main className="p-24">
-      <h1 className="text-4xl font-bold">Filecoin.io V3</h1>
-      <p className="mt-4 text-lg">
+      <h1 className="text-brand-500 text-4xl font-bold">Filecoin.io V3</h1>
+      <p className="mt-4 text-lg text-zinc-600">
         This is the new Filecoin.io website, built with Next.js and Tailwind.
       </p>
     </main>
