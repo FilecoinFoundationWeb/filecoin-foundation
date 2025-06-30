0ded5e5f372a5d3727c3ac5c7dc85397884a1524
Mirha Masala <mirha@hey.com>
2025-06-30 08:29:07 +0100

    Scaffold Offer Storage page

--- apps/filecoin-site/src/app/_constants/paths.ts
+++ apps/filecoin-site/src/app/_constants/paths.ts
@@ -1,12 +1,17 @@
 import { createPathConfig } from '@/utils/createPathConfig'
 
-export type StaticPath = '/' | '/build-on-filecoin' | '/store-data'
+export type StaticPath =
+  | '/'
+  | '/build-on-filecoin'
+  | '/offer-storage'
+  | '/store-data'
 
 export const PATHS = {
-  HOME: createPathConfig('/', 'Home'),
   BUILD_ON_FILECOIN: createPathConfig(
     '/build-on-filecoin',
     'Build on Filecoin',
   ),
+  HOME: createPathConfig('/', 'Home'),
+  OFFER_STORAGE: createPathConfig('/offer-storage', 'Offer Storage'),
   STORE_DATA: createPathConfig('/store-data', 'Store Data'),
 } as const
