aa5c2d2331ec10f2fab2ce01460e0b3e054278a1
Mirha Masala <mirha@hey.com>
2025-06-30 08:27:29 +0100

    Scaffold Build on Filecoin page

--- apps/filecoin-site/src/app/_constants/paths.ts
+++ apps/filecoin-site/src/app/_constants/paths.ts
@@ -1,7 +1,11 @@
 import { createPathConfig } from '@/utils/createPathConfig'
 
-export type StaticPath = '/'
+export type StaticPath = '/' | '/build-on-filecoin'
 
 export const PATHS = {
   HOME: createPathConfig('/', 'Home'),
+  BUILD_ON_FILECOIN: createPathConfig(
+    '/build-on-filecoin',
+    'Build on Filecoin',
+  ),
 } as const
