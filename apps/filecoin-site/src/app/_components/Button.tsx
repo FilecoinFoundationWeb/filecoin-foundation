cf23c13ed6af18a6855164bbebaea2c1298bbee6
Charly Martin <charly@hey.com>
2025-07-01 16:28:01 +0100

    Refactor Button component to use LocalButtonProps and update variant handling

--- apps/filecoin-site/src/app/_components/Button.tsx
+++ apps/filecoin-site/src/app/_components/Button.tsx
@@ -1,7 +1,5 @@
-import {
-  Button as SharedButton,
-  type ButtonProps as SharedButtonProps,
-} from '@filecoin-foundation/ui/Button'
+import { Button as SharedButton } from '@filecoin-foundation/ui/Button'
+import { type LocalButtonProps } from '@filecoin-foundation/utils/types/buttonTypes'
 
 import { BASE_DOMAIN } from '@/constants/siteMetadata'
 
@@ -12,14 +10,15 @@ const variantClasses = {
   ghostDark: 'button--ghost button--ghost--dark',
 } as const
 
-type ButtonProps = Omit<
-  SharedButtonProps<typeof variantClasses>,
-  'baseDomain' | 'variants'
->
+type ButtonProps = LocalButtonProps<typeof variantClasses>
 
-export function Button({ children, ...props }: ButtonProps) {
+export function Button({ children, variant, ...props }: ButtonProps) {
   return (
-    <SharedButton variants={variantClasses} baseDomain={BASE_DOMAIN} {...props}>
+    <SharedButton
+      variants={{ options: variantClasses, selected: variant }}
+      baseDomain={BASE_DOMAIN}
+      {...props}
+    >
       {children}
     </SharedButton>
   )
