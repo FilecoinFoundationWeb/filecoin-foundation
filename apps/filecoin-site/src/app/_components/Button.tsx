8788830252c36cf10b42f3f5acf68cb423e95b58
Charly MARTIN <charly@hey.com>
2025-07-02 15:28:17 +0100

    [UXIT-2937] Add filecoin button variants (#1517)
    * WIP: Add button variants
    
    * Refactor button styles to use double hyphen convention for BEM compatibility
    
    * Add gradient hover to primary button
    
    * fixup! Add gradient hover to primary button
    
    * Refactor Button component to use LocalButtonProps and update variant handling
    
    * fixup! fixup! Add gradient hover to primary button
    
    * fixup! fixup! Add gradient hover to primary button
    
    * Move disabled classes in className to apply to links and buttons
    
    * Refactor button styles to utilize gradient variables for hover effects and improve consistency
    
    * Refactor button styles to improve border handling and simplify ghost button classes

---
+++ apps/filecoin-site/src/app/_components/Button.tsx
@@ -0,0 +1,25 @@
+import { Button as SharedButton } from '@filecoin-foundation/ui/Button'
+import { type LocalButtonProps } from '@filecoin-foundation/utils/types/buttonTypes'
+
+import { BASE_DOMAIN } from '@/constants/siteMetadata'
+
+const variantClasses = {
+  primary: 'button--primary',
+  primaryDark: 'button--primary button--primary--dark',
+  ghost: 'button--ghost',
+  ghostDark: 'button--ghost button--ghost--dark',
+} as const
+
+type ButtonProps = LocalButtonProps<typeof variantClasses>
+
+export function Button({ children, variant, ...props }: ButtonProps) {
+  return (
+    <SharedButton
+      variants={{ options: variantClasses, selected: variant }}
+      baseDomain={BASE_DOMAIN}
+      {...props}
+    >
+      {children}
+    </SharedButton>
+  )
+}
