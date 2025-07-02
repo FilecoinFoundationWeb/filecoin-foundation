b9f18b15de9ceeb3e8110d28213484c1183fea46
Barbara Peric <b.peric10@gmail.com>
2025-07-02 14:23:26 +0100

    [SHARED][UXIT-2654] Implement SharePost Component and Update Blog Page
    * Introduced a new `SharePost` component for sharing articles on social media platforms, enhancing user engagement.
    * Updated `generateShareArticleLinks` utility to allow dynamic selection of social platforms for sharing.
    * Integrated `SharePost` component into the blog page, providing a share functionality for blog posts.
    * Added styles for tooltips in the global CSS to improve UI consistency.

---
+++ apps/filecoin-site/src/app/_components/SharePost.tsx
@@ -0,0 +1,58 @@
+'use client'
+
+import { clsx } from 'clsx'
+
+import { Icon } from '@filecoin-foundation/ui/Icon'
+import { Tooltip } from '@filecoin-foundation/ui/Tooltip/Tooltip'
+import { generateShareArticleLinks } from '@filecoin-foundation/utils/generateShareArticleLinks'
+import type { TouchTarget } from '@filecoin-foundation/utils/types/touchTargetType'
+
+type SharePostProps = {
+  articleTitle: string
+  path: string
+  baseUrl: string
+  sectionTitle?: string
+}
+
+const TOUCH_TARGET: TouchTarget = {
+  touchAreaPadding: 'p-2',
+  touchAreaOffset: '-m-2',
+}
+
+export function SharePost({ articleTitle, path, baseUrl }: SharePostProps) {
+  const articleUrl = `${baseUrl}${path}`
+
+  const socialLinksWithIcons = generateShareArticleLinks({
+    URL: articleUrl,
+    title: articleTitle,
+    BASE_URL: baseUrl,
+    platforms: ['facebook', 'linkedin', 'x'],
+  })
+
+  return (
+    <ul
+      className={clsx(
+        'flex flex-wrap items-center gap-2',
+        TOUCH_TARGET.touchAreaOffset,
+      )}
+    >
+      {socialLinksWithIcons.map(({ label, href, icon }) => (
+        <li key={label} className="inline-flex">
+          <Tooltip description={`Share on ${label}`} side="bottom">
+            <a
+              aria-label={`Share ${articleTitle} on ${label}`}
+              href={href}
+              rel="noopener noreferrer"
+              className={clsx(
+                'social-link focus:brand-outline',
+                TOUCH_TARGET.touchAreaPadding,
+              )}
+            >
+              <Icon component={icon} weight="light" size={32} />
+            </a>
+          </Tooltip>
+        </li>
+      ))}
+    </ul>
+  )
+}
--- apps/filecoin-site/src/app/blog/page.tsx
+++ apps/filecoin-site/src/app/blog/page.tsx
@@ -1,3 +1,14 @@
+import { SharePost } from '@/components/SharePost'
+
 export default function Blog() {
-  return <h1>Blog</h1>
+  return (
+    <div>
+      <h1>Blog</h1>
+      <SharePost
+        articleTitle="Blog Post Title"
+        path="/blog/blog-post-title"
+        baseUrl="https://filecoin.io"
+      />
+    </div>
+  )
 }
