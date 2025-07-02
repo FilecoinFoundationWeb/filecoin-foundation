eeea94ae51fd814fc3ee835f7c21f84c8fd07f85
Barbara Peric <50910606+barbaraperic@users.noreply.github.com>
2025-07-02 16:45:59 +0200

    [FILECOIN][UXIT-2931] Add Mobile Navigation Component (#1519)
    * Add Mobile Navigation
    
    - Introduced a new `MobileNavigation` component to provide a responsive navigation experience for mobile users.
    - Updated `DesktopNavigation` to increase the gap between navigation links for improved spacing.
    - Added a `touchAreaOffset` constant to enhance touch target accessibility in `NavigationLink`.
    - Modified the `Navigation` component to include the new `MobileNavigation`, ensuring a cohesive navigation structure across devices.
    
    * Refactor Navigation Component Imports and Touch Target Padding
    
    * style: add slide-over panel component styles
    
    * fix: update LogoIcon import path in Navigation components
    
    ---------
    
    Co-authored-by: Charly Martin <charly@hey.com>

---
+++ apps/filecoin-site/src/app/_components/Navigation/Navigation.tsx
@@ -0,0 +1,18 @@
+import { LogoLink } from '@filecoin-foundation/ui/LogoLink'
+
+import { LogoIcon } from '@/components/Logo/IconLogo'
+
+import { DesktopNavigation } from './DesktopNavigation'
+import { MobileNavigation } from './MobileNavigation'
+
+export function Navigation() {
+  return (
+    <nav className="mb-9 flex items-center justify-between lg:justify-start lg:gap-24">
+      <div className="shrink-0">
+        <LogoLink logo={LogoIcon} />
+      </div>
+      <MobileNavigation />
+      <DesktopNavigation />
+    </nav>
+  )
+}
