2b339bb86a73430ad8a92ef8170267fcbbeea79a
Charly Martin <charly@hey.com>
2025-07-02 16:34:44 +0100

    Refactor Home component to use Container and PageSection for improved layout

--- apps/filecoin-site/src/app/(homepage)/page.tsx
+++ apps/filecoin-site/src/app/(homepage)/page.tsx
@@ -1,19 +1,21 @@
-import { PageLayout } from '@filecoin-foundation/ui/PageLayout'
-
+import { Container } from '@/components/Container'
 import { Title } from '@/components/PageHeader/Title'
+import { PageSection } from '@/components/PageSection'
 
 export default function Home() {
   return (
-    <PageLayout>
-      <section>
-        <Title backgroundVariant="light">
-          Preserving humanity’s most important information
-        </Title>
-        <p>
-          Keep your data accessible, verifiable, and free from centralized
-          control with the world’s largest decentralized storage network.
-        </p>
-      </section>
-    </PageLayout>
+    <>
+      <Container>
+        <PageSection>
+          <Title backgroundVariant="light">
+            Preserving humanity’s most important information
+          </Title>
+          <p>
+            Keep your data accessible, verifiable, and free from centralized
+            control with the world’s largest decentralized storage network.
+          </p>
+        </PageSection>
+      </Container>
+    </>
   )
 }
