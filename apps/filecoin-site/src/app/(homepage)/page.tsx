6fa3f7c7a52ad683e2b7a5e06d295576e0827348
Mirha Masala <mirha@hey.com>
2025-07-03 11:09:13 +0100

    Scout CR

--- apps/filecoin-site/src/app/(homepage)/page.tsx
+++ apps/filecoin-site/src/app/(homepage)/page.tsx
@@ -1,8 +1,4 @@
-import { GithubLogoIcon } from '@phosphor-icons/react/dist/ssr'
-
 import { PageLayout } from '@filecoin-foundation/ui/PageLayout'
-
-import { Button } from '@/components/Button'
 
 export default function Home() {
   return (
@@ -12,37 +8,6 @@ export default function Home() {
         <p className="mt-4 text-lg text-zinc-600">
           This is the new Filecoin.io website, built with Next.js and Tailwind.
         </p>
-      </section>
-
-      <section className="flex gap-3">
-        <Button variant="primary">Primary</Button>
-        <Button variant="ghost">Ghost</Button>
-        <Button variant="primary" href="https://example.com">
-          Primary Link
-        </Button>
-        <Button variant="ghost" href="https://example.com">
-          Ghost Link
-        </Button>
-      </section>
-
-      <section className="-m-3 flex gap-3 bg-zinc-950 p-3">
-        <Button variant="primaryDark">Primary Dark</Button>
-        <Button variant="ghostDark">Ghost Dark</Button>
-        <Button variant="primaryDark" href="https://example.com">
-          Primary Dark Link
-        </Button>
-        <Button variant="ghostDark" href="https://example.com">
-          Ghost Dark Link
-        </Button>
-      </section>
-
-      <section className="flex gap-3">
-        <Button variant="ghost" icon={GithubLogoIcon}>
-          GitHub
-        </Button>
-        <Button variant="ghostDark" icon={GithubLogoIcon}>
-          GitHub
-        </Button>
       </section>
     </PageLayout>
   )
