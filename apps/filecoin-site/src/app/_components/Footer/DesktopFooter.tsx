d003472a861273163f6dc13f6a2f1573b85c9337
Mirha Masala <mirha@hey.com>
2025-07-08 14:11:23 +0100

    CR

--- apps/filecoin-site/src/app/_components/Footer/DesktopFooter.tsx
+++ apps/filecoin-site/src/app/_components/Footer/DesktopFooter.tsx
@@ -7,19 +7,17 @@ import { Navigation } from './Navigation'
 
 export function DesktopFooter() {
   return (
-    <>
-      <div className="hidden md:block md:pt-20 md:pb-16">
-        <Container>
-          <div className="mb-16 flex justify-between gap-8">
-            <div className="flex-shrink-0">
-              <Logo height={30} />
-            </div>
-            <Navigation />
-            <NewsletterForm />
+    <div className="hidden md:block md:pt-20 md:pb-16">
+      <Container>
+        <div className="mb-16 flex justify-between gap-8">
+          <div className="flex-shrink-0">
+            <Logo height={30} />
           </div>
-        </Container>
-        <LegalSection />
-      </div>
-    </>
+          <Navigation />
+          <NewsletterForm />
+        </div>
+      </Container>
+      <LegalSection />
+    </div>
   )
 }
