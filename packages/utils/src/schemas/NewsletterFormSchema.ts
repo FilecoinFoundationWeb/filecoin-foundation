cfd626452bd50484aedf989fff5757f2b24502dd
Charly Martin <charly@hey.com>
2025-07-10 12:46:10 +0100

    fixup! Refactor NewsletterForm to use centralized schema and types for form validation

--- packages/utils/src/schemas/NewsletterFormSchema.ts
+++ packages/utils/src/schemas/NewsletterFormSchema.ts
@@ -4,7 +4,7 @@ export const NewsletterFormSchema = z.object({
   email: z
     .string()
     .min(1, { message: 'Your email is required' })
-    .email({ message: 'The email format is invalid' }),
+    .email({ message: 'Your email format is invalid' }),
 })
 
 export type NewsletterFormData = z.infer<typeof NewsletterFormSchema>
