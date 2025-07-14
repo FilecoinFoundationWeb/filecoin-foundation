4143026eb53e835c54c67dceea160de9278a1a2d
Mirha Masala <mirha@hey.com>
2025-07-14 15:06:06 +0100

    Style nav based on theme section

--- apps/filecoin-site/src/app/_components/Navigation/NavigationLink.tsx
+++ apps/filecoin-site/src/app/_components/Navigation/NavigationLink.tsx
@@ -27,7 +27,7 @@ export function NavigationLink({ href, label, ...rest }: NavigationLinkProps) {
       className={clsx(
         TOUCH_TARGET_NAV_LINK.touchAreaPadding,
         TOUCH_TARGET_NAV_LINK.touchAreaOffset,
-        'focus:brand-outline inline-block text-base font-semibold text-zinc-600',
+        'navigation-link focus:brand-outline inline-block text-base font-semibold',
       )}
       {...rest}
     >
