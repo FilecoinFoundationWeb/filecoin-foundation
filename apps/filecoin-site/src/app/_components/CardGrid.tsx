2c246a28f6b14aa4e15ab4f1f19f8d60cd16101a
Charly Martin <charly@hey.com>
2025-07-16 17:49:26 +0100

    refactor(offer-storage): Standardize layouts with CardGrid
    Refactor the 'Offer Storage' page to consistently use the `CardGrid` component for its grid layouts. This replaces previous custom `ul` and flexbox implementations, improving code consistency and maintainability.
    
    A new `col2-four` variant has been added to the `CardGrid` component to support the specific layout requirements of this page.

--- apps/filecoin-site/src/app/_components/CardGrid.tsx
+++ apps/filecoin-site/src/app/_components/CardGrid.tsx
@@ -10,6 +10,7 @@ const variants = {
   'col2-one': 'gap-8 md:grid-cols-2',
   'col2-two': 'gap-x-15 gap-y-8 md:grid-cols-2',
   'col2-three': 'gap-10 lg:grid-cols-2',
+  'col2-four': 'gap-15 lg:grid-cols-2',
   'col3-one': 'gap-x-8 gap-y-15 md:grid-cols-2 lg:grid-cols-3',
   'col3-two': 'gap-12 md:grid-cols-2 md:gap-15 lg:grid-cols-3',
   'col3-three': 'gap-x-15 gap-y-20 md:grid-cols-2 xl:grid-cols-3',
