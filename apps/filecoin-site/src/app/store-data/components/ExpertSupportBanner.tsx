0123226abba9d1c92ed03cd0057e4544c17c809c
Charly Martin <charly@hey.com>
2025-07-18 10:06:21 +0100

    fixup! feat(store-data): Add category filter and expert banner

--- apps/filecoin-site/src/app/store-data/components/ExpertSupportBanner.tsx
+++ apps/filecoin-site/src/app/store-data/components/ExpertSupportBanner.tsx
@@ -3,10 +3,12 @@ import { Button } from '@/components/Button'
 export function ExpertSupportBanner() {
   return (
     <div
-      className="flex items-baseline gap-6 bg-zinc-100 px-9 py-6"
+      className="flex flex-wrap items-baseline justify-center gap-6 bg-zinc-100 px-9 py-6 sm:justify-between"
       role="banner"
     >
-      <p>Not sure what storage solution to choose?</p>
+      <p className="text-center sm:text-left">
+        Not sure what storage solution to choose?
+      </p>
 
       <Button href="" variant="primary">
         Talk to an expert
