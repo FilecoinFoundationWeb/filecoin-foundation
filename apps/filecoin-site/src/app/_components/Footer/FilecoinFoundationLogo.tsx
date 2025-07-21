b15d9136a54c7d582aa54a62b0d0748a8fdebe20
Mirha Masala <mirha@hey.com>
2025-07-21 10:56:53 +0100

    Flatten component structure

--- apps/filecoin-site/src/app/_components/Footer/FilecoinFoundationLogo.tsx
+++
@@ -1,9 +0,0 @@
-import LogoAsset from '@/assets/logos/filecoin-foundation-logo.svg'
-
-type FilecoinFoundationLogoProps = React.SVGProps<SVGSVGElement>
-
-export function FilecoinFoundationLogo({
-  height = 24,
-}: FilecoinFoundationLogoProps) {
-  return <LogoAsset height={height} />
-}
