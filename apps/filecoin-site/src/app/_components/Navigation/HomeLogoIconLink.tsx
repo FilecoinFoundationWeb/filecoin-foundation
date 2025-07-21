37ae5ce0a61de441a2fcd905badd0d0bd4f87aec
Mirha Masala <mirha@hey.com>
2025-07-21 11:26:34 +0100

    Refactor HomeIconLogoLionk

---
+++ apps/filecoin-site/src/app/_components/Navigation/HomeLogoIconLink.tsx
@@ -0,0 +1,7 @@
+import { LogoLink } from '@filecoin-foundation/ui/LogoLink'
+
+import IconLogo from '@/assets/logos/filecoin-logo-icon.svg'
+
+export function HomeLogoIconLink() {
+  return <LogoLink logo={IconLogo} logoProps={{ height: 40 }} />
+}
--- apps/filecoin-site/src/app/_components/Navigation/IconLogo.tsx
+++
@@ -1,7 +0,0 @@
-import LogoAsset from '@/assets/logos/filecoin-logo-icon.svg'
-
-type IconLogoProps = React.SVGProps<SVGSVGElement>
-
-export function IconLogo({ height = 40 }: IconLogoProps) {
-  return <LogoAsset height={height} />
-}
