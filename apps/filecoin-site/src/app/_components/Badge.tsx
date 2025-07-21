4f6d32b8562a041e73a118d53cb2fe8edcef8a4e
Charly Martin <charly@hey.com>
2025-07-21 17:33:55 +0100

    Fix Badge component display style by changing width to inline-block

--- apps/filecoin-site/src/app/_components/Badge.tsx
+++ apps/filecoin-site/src/app/_components/Badge.tsx
@@ -4,7 +4,7 @@ export type BadgeProps = {
 
 export function Badge({ children }: BadgeProps) {
   return (
-    <span className="text-brand-950 bg-brand-50 border-brand-100 w-fit rounded-full border px-4 py-1 text-sm font-medium capitalize">
+    <span className="text-brand-950 bg-brand-50 border-brand-100 inline-block rounded-full border px-4 py-1 text-sm font-medium capitalize">
       {children}
     </span>
   )
