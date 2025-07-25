1b4f32edee1cb5f6f2f59c8f4a65efadfc2ed9cf
Mirha Masala <mirha@hey.com>
2025-07-25 14:39:04 +0100

    Scout - Adapt alignment

--- apps/filecoin-site/src/app/store-data/components/ExpertSupportBanner.tsx
+++ apps/filecoin-site/src/app/store-data/components/ExpertSupportBanner.tsx
@@ -3,14 +3,14 @@ import { Button } from '@/components/Button'
 export function ExpertSupportBanner() {
   return (
     <div
-      className="flex flex-wrap items-baseline justify-center gap-6 bg-zinc-100 px-9 py-6 sm:justify-between"
+      className="flex flex-wrap items-baseline gap-6 bg-zinc-100 px-9 py-6"
       role="banner"
     >
       <p className="text-center sm:text-left">
         Not sure what storage solution to choose?
       </p>
 
-      <Button href="" variant="primary">
+      <Button href="#todo" variant="primary">
         Talk to an expert
       </Button>
     </div>
