d6480773db6768920f9cfcf1951e3f7a00a6c6bd
Mirha Masala <mirha@hey.com>
2025-07-28 14:25:37 +0100

    Change styles filecoinVsCloudComparisonData

--- apps/filecoin-site/src/app/learn/components/ComparisonTable/ComparisonTable.tsx
+++ apps/filecoin-site/src/app/learn/components/ComparisonTable/ComparisonTable.tsx
@@ -1,28 +1,18 @@
-import {
-  filecoinFeatures,
-  traditionalCloudFeatures,
-} from '../../data/filecoinVsCloudComparison'
+import { filecoinVsCloudComparison } from '../../data/filecoinVsCloudComparison'
 
-import { Column, type ColumnProps } from './Column'
-
-const columns: Array<ColumnProps> = [
-  {
-    title: 'Filecoin',
-    features: filecoinFeatures,
-    theme: 'dark',
-  },
-  {
-    title: 'Traditional Cloud',
-    features: traditionalCloudFeatures,
-    theme: 'light',
-  },
-]
+import { Column } from './Column'
 
 export function ComparisonTable() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2">
-      {columns.map(({ title, features, theme }) => (
-        <Column key={title} title={title} features={features} theme={theme} />
+      {filecoinVsCloudComparison.map(({ title, features, logo }, index) => (
+        <Column
+          key={title}
+          title={title}
+          features={features}
+          columnIndex={index === 0 ? 1 : 2}
+          logo={logo}
+        />
       ))}
     </div>
   )
