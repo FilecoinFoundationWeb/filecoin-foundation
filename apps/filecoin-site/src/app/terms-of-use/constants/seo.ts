9835d769aef48bb9f3f817747a772f96ffaebb48
Mirha Masala <mirha@hey.com>
2025-07-29 10:33:04 +0100

    Terms of Use Page - Add metadata

---
+++ apps/filecoin-site/src/app/terms-of-use/constants/seo.ts
@@ -0,0 +1,5 @@
+export const TERMS_OF_USE_SEO = {
+  title: 'Terms of Use | Filecoin Decentralized Storage Network',
+  description:
+    "Read Filecoin's terms of use for guidelines on accessing our network, user responsibilities, and intellectual property rights. Ensure compliance while building, storing, or providing decentralized storage services.",
+} as const
--- apps/filecoin-site/src/app/terms-of-use/page.tsx
+++ apps/filecoin-site/src/app/terms-of-use/page.tsx
@@ -1,5 +1,11 @@
+import { PATHS } from '@/constants/paths'
+
+import { createMetadata } from '@/utils/createMetadata'
+
 import { PageHeader } from '@/components/PageHeader'
 import { PageSection } from '@/components/PageSection'
+
+import { TERMS_OF_USE_SEO } from './constants/seo'
 
 export default function TermsOfUse() {
   return (
@@ -8,3 +14,9 @@ export default function TermsOfUse() {
     </PageSection>
   )
 }
+
+export const metadata = createMetadata({
+  title: { absolute: TERMS_OF_USE_SEO.title },
+  description: TERMS_OF_USE_SEO.description,
+  path: PATHS.TERMS_OF_USE.path,
+})
