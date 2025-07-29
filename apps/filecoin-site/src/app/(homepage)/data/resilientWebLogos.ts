48db433e72b45692913ffa1b4fc30255af72e0ed
Mirha Masala <mirha@hey.com>
2025-07-29 10:04:02 +0100

    Add resilient web logos

--- apps/filecoin-site/src/app/(homepage)/data/resilientWebLogos.ts
+++ apps/filecoin-site/src/app/(homepage)/data/resilientWebLogos.ts
@@ -1,31 +1,45 @@
 import { type LogoItemProps } from '@/components/LogoSection/LogoItem'
 
-import FilecoinFoundationLogo from '@/assets/logos/filecoin-foundation-logo.svg'
+import CIDgravityLogo from '@/assets/logos/cidgravity-logo.svg'
+import DCENTLogo from '@/assets/logos/dcent-logo.svg'
+import FutureTechHoldingsLogo from '@/assets/logos/future-tech-holdings-logo.svg'
+// import NDLABSLogo from '@/assets/logos/ndlabs-logo.svg'
+// import RRMGlobalLogo from '@/assets/logos/rrm-global-logo.svg'
+import SwanChainLogo from '@/assets/logos/swan-chain-logo.svg'
 
 export const resilientWebLogos: Array<LogoItemProps> = [
   {
-    logo: FilecoinFoundationLogo,
-    alt: 'Filecoin Foundation Logo',
-    href: 'https://fil.org',
+    logo: CIDgravityLogo,
+    alt: 'CIDgravity Logo',
+    href: 'https://cidgravity.com',
+    size: 50,
   },
   {
-    logo: FilecoinFoundationLogo,
-    alt: 'Filecoin Foundation Logo',
-    href: 'https://fil.org',
+    logo: DCENTLogo,
+    alt: 'DCENT Logo',
+    href: 'https://dcent.nl/',
+    size: 24,
   },
   {
-    logo: FilecoinFoundationLogo,
-    alt: 'Filecoin Foundation Logo',
-    href: 'https://fil.org',
+    logo: FutureTechHoldingsLogo,
+    alt: 'Future Tech Holdings Logo',
+    href: 'https://future-tech-holdings.com/',
+    size: 24,
   },
+  // {
+  //   logo: NDLABSLogo,
+  //   alt: 'ND LABS Logo',
+  //   href: 'https://www.ndlabs.io/',
+  // },
+  // {
+  //   logo: RRMGlobalLogo,
+  //   alt: 'RRM Global Logo',
+  //   href: 'https://www.rrmglobal.io/',
+  // },
   {
-    logo: FilecoinFoundationLogo,
-    alt: 'Filecoin Foundation Logo',
-    href: 'https://fil.org',
-  },
-  {
-    logo: FilecoinFoundationLogo,
-    alt: 'Filecoin Foundation Logo',
-    href: 'https://fil.org',
+    logo: SwanChainLogo,
+    alt: 'Swan Chain Logo',
+    href: 'https://swanchain.io/',
+    size: 40,
   },
 ]
