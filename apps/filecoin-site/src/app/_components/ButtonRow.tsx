2163811e94134f30100546bba72b035b4ff69b81
Mirha Masala <mirha@hey.com>
2025-07-30 08:36:10 +0100

    ButtonRow - Increase spacing between buttons

--- apps/filecoin-site/src/app/_components/ButtonRow.tsx
+++ apps/filecoin-site/src/app/_components/ButtonRow.tsx
@@ -15,7 +15,7 @@ export function ButtonRow({ buttons, centered }: ButtonRowProps) {
   return (
     <div
       className={clsx(
-        'flex flex-col flex-wrap gap-3 sm:flex-row',
+        'flex flex-col flex-wrap gap-6 sm:flex-row',
         centered && 'justify-center',
       )}
     >
