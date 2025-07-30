3187c37072955a5721ac798a373da737f56f243c
Charly Martin <charly@hey.com>
2025-07-30 09:42:53 +0100

    Refactor: Simplify layout on Offer Storage form page
    Remove an `div` wrapper around the `OfferStorageForm` component.

--- apps/filecoin-site/src/app/offer-storage/form/page.tsx
+++ apps/filecoin-site/src/app/offer-storage/form/page.tsx
@@ -10,15 +10,13 @@ export default function OfferStorageFormPage() {
         <Heading tag="h2" variant="5xl-medium">
           Thanks for your interest in offering storage on the Filecoin network.
         </Heading>
-        <p className="pt-6 text-xl leading-8 font-normal text-balance text-zinc-600">
+        <p className="pt-6 pb-25 text-xl leading-8 font-normal text-balance text-zinc-600">
           Please fill out this form below, and a member of the onboarding team
           will reach out to guide you through next steps in becoming a storage
           provider.
         </p>
 
-        <div className="mt-25">
-          <OfferStorageForm />
-        </div>
+        <OfferStorageForm />
       </div>
     </PageSection>
   )
