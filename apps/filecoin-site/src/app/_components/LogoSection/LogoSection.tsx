500417c9ec4721b7db68f343bd3cd15362ad3099
Mirha Masala <mirha@hey.com>
2025-07-31 13:18:41 +0100

    LogoSection - Include responsive designs

--- apps/filecoin-site/src/app/_components/LogoSection/LogoSection.tsx
+++ apps/filecoin-site/src/app/_components/LogoSection/LogoSection.tsx
@@ -11,7 +11,7 @@ type LogoSectionProps = {
 export function LogoSection({ logos, title, tag = 'h2' }: LogoSectionProps) {
   return (
     <section
-      className="logo-section space-y-15 md:space-y-20"
+      className="logo-section flex flex-col items-center gap-15 md:gap-20 lg:items-start"
       aria-labelledby={title ? 'logo-section-title' : undefined}
     >
       {title && (
@@ -19,7 +19,7 @@ export function LogoSection({ logos, title, tag = 'h2' }: LogoSectionProps) {
           {title}
         </Heading>
       )}
-      <ul className="flex flex-wrap justify-start gap-x-16 gap-y-10">
+      <ul className="grid grid-cols-2 justify-items-center gap-15 lg:grid-cols-4 lg:justify-items-start xl:grid-cols-5">
         {logos.map((logoItem, index) => (
           <LogoItem key={`logo-${index}`} {...logoItem} />
         ))}
