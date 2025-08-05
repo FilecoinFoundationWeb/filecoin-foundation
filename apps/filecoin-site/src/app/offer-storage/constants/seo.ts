215e42188e21de03d37697daaf106f0436cfd8cb
Barbara Peric <b.peric10@gmail.com>
2025-08-05 15:27:16 +0200

    Update SEO descriptions for blog, learn, and offer storage pages to enhance clarity and engagement

--- apps/filecoin-site/src/app/offer-storage/constants/seo.ts
+++ apps/filecoin-site/src/app/offer-storage/constants/seo.ts
@@ -2,5 +2,5 @@ export const OFFER_STORAGE_SEO = {
   title:
     'Become a Filecoin Storage Provider | Earn Rewards in Decentralized Network',
   description:
-    'Join Filecoin as a storage provider to power global decentralized infrastructure. Use your data center to earn FIL rewards, diversify revenue, and scale with demand while preserving vital data in a permissionless ecosystem.',
+    'Join the Filecoin network as a storage provider to power global decentralized infrastructure. Use your data center to earn FIL rewards, diversify revenue, and scale with demand while preserving vital data in a permissionless ecosystem.',
 } as const
