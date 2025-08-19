7c9981a9399b1984c440bb207e400b33530cf009
Barbara Peric <50910606+barbaraperic@users.noreply.github.com>
2025-08-19 10:54:47 +0200

    [FILECOIN][UXIT-3134] Rework Community Page [skip percy] (#1661)
    * Rework Community Page
    
    - Introduced five new images for community connection, enhancing visual content.
    - Created ImageGrid component to display images in a responsive grid layout.
    - Updated community page to utilize ImageGrid for showcasing community connection images, improving layout and user engagement.
    
    * CR
    
    * Remove Community Connections Section
    
    ---------
    
    Co-authored-by: Mirha Masala <mirha@hey.com>

--- apps/filecoin-site/src/app/_components/ImageGrid.tsx
+++ apps/filecoin-site/src/app/_components/ImageGrid.tsx
@@ -1,14 +1,45 @@
+import React from 'react'
+
+import type { ImageProps } from 'next/image'
+
 import { clsx } from 'clsx'
 
 type ImageGridProps = {
-  variant: keyof typeof variants
   children: React.ReactNode
+  variant: keyof typeof variants
 }
 
 const variants = {
-  oneMdThree: 'gap-1 grid-cols-1 md:grid-cols-3',
+  oneMdThree: {
+    grid: 'gap-1 grid-cols-1 md:grid-cols-3',
+  },
+  oneMdThreeCollage: {
+    grid: 'gap-1 grid-cols-1 md:grid-cols-3',
+    item: (i: number) =>
+      clsx(
+        'h-full w-full object-cover',
+        i === 0 && 'md:col-span-2 md:row-span-2',
+        i === 2 && 'md:col-start-3 md:row-span-2',
+      ),
+  },
 }
 
-export function ImageGrid({ variant, children }: ImageGridProps) {
-  return <div className={clsx('grid', variants[variant])}>{children}</div>
+export function ImageGrid({ children, variant }: ImageGridProps) {
+  const variantConfig = variants[variant]
+  const gridClass = variantConfig.grid
+
+  return (
+    <div className={clsx('grid', gridClass)}>
+      {'item' in variantConfig
+        ? React.Children.map(children, (child, i) =>
+            React.cloneElement(child as React.ReactElement<ImageProps>, {
+              className: clsx(
+                (child as React.ReactElement<ImageProps>).props.className,
+                variantConfig.item(i),
+              ),
+            }),
+          )
+        : children}
+    </div>
+  )
 }
--- apps/filecoin-site/src/app/community/page.tsx
+++ apps/filecoin-site/src/app/community/page.tsx
@@ -1,4 +1,7 @@
+import Image from 'next/image'
+
 import clsx from 'clsx'
+// import { StructuredDataScript } from '@filecoin-foundation/ui/StructuredDataScript'
 
 import { PATHS } from '@/constants/paths'
 
@@ -9,19 +12,20 @@ import { createMetadata } from '@/utils/createMetadata'
 import { Button } from '@/components/Button'
 import { CardGrid } from '@/components/CardGrid'
 import { CardGridContainer } from '@/components/CardGridContainer'
-import { LinkCard } from '@/components/LinkCard'
+import { ImageGrid } from '@/components/ImageGrid'
 import { Navigation } from '@/components/Navigation/Navigation'
 import { PageHeader } from '@/components/PageHeader'
 import { PageSection } from '@/components/PageSection'
 import { backgroundVariants } from '@/components/Section'
 import { SectionContent } from '@/components/SectionContent'
 import { SectionImage } from '@/components/SectionImage'
+import { SimpleCard } from '@/components/SimpleCard'
 import { SimpleCardWithLogo } from '@/components/SimpleCardWithLogo'
 
 import { COMMUNITY_SEO } from './constants/seo'
-import { communityConnections } from './data/communityConnections'
+import { communityConnectionImages } from './data/communityConnectionImages'
 import { ecosystemResources } from './data/ecosystemResources'
-import { getInvolvedOptions } from './data/getInvolvedOptions'
+import { getInvolvedWithCommunity } from './data/getInvolvedWithCommunity'
 
 export default function BuildOnFilecoin() {
   return (
@@ -38,7 +42,10 @@ export default function BuildOnFilecoin() {
             title="Build, connect and grow with the Filecoin community"
             description="A global network of builders, researchers, advocates and creators is shaping the future of Filecoin. Plug into the community and start contributing in your own way."
             cta={
-              <Button href="#todo" variant="primary">
+              <Button
+                href={`${PATHS.COMMUNITY.path}#get-involved`}
+                variant="primary"
+              >
                 Explore how to get involved
               </Button>
             }
@@ -50,38 +57,37 @@ export default function BuildOnFilecoin() {
 
       <PageSection backgroundVariant="dark">
         <SectionContent
+          title="Get involved"
+          description="Whether you want to contribute, connect or build, there’s a place for you in the Filecoin community."
+        >
+          <CardGridContainer width="6xl">
+            <CardGrid as="ul" variant="smThree">
+              {getInvolvedWithCommunity.map(({ title, description, cta }) => (
+                <SimpleCard
+                  key={title}
+                  as="li"
+                  title={title}
+                  description={description}
+                  cta={cta}
+                />
+              ))}
+            </CardGrid>
+          </CardGridContainer>
+        </SectionContent>
+      </PageSection>
+
+      <PageSection backgroundVariant="light">
+        <SectionContent
           title="Connect with the community"
           description="Connect with a vibrant network of developers building on Filecoin.
           Connect, collaborate, and grow with the ecosystem."
         >
-          <CardGrid as="ul" variant="smTwoLgThreeTight">
-            {communityConnections.map(({ title, description, href, icon }) => (
-              <LinkCard
-                key={title}
-                as="li"
-                title={title}
-                headingTag="h3"
-                description={description}
-                href={href}
-                icon={{ component: icon }}
-              />
+          <ImageGrid variant="oneMdThreeCollage">
+            {communityConnectionImages.map(({ data, alt }) => (
+              <Image key={alt} src={data} alt={alt} />
             ))}
-          </CardGrid>
+          </ImageGrid>
         </SectionContent>
-      </PageSection>
-
-      <PageSection backgroundVariant="dark">
-        <SectionContent
-          title="Explore the ecosystem"
-          description="Visit Filecoin Foundation's Ecosystem Explorer to discover Filecoin projects, get inspired by what others are building, and find your place in the Filecoin ecosystem."
-          cta={
-            <Button href="https://fil.org/ecosystem-explorer" variant="primary">
-              Visit ecosystem explorer
-            </Button>
-          }
-        />
-
-        <SectionImage {...graphicsData.buildOnFilecoinSection} />
       </PageSection>
 
       <PageSection backgroundVariant="light">
@@ -105,26 +111,16 @@ export default function BuildOnFilecoin() {
 
       <PageSection backgroundVariant="dark">
         <SectionContent
-          title="Get involved"
-          description="Whether you're just getting started or looking to deepen your
-          contributions, there are many ways to get involved."
-        >
-          <CardGridContainer width="6xl">
-            <CardGrid as="ul" variant="mdTwoWide">
-              {getInvolvedOptions.map(({ title, description, href, icon }) => (
-                <LinkCard
-                  key={title}
-                  as="li"
-                  title={title}
-                  headingTag="h3"
-                  description={description}
-                  href={href}
-                  icon={{ component: icon, variant: 'filled' }}
-                />
-              ))}
-            </CardGrid>
-          </CardGridContainer>
-        </SectionContent>
+          title="Explore the ecosystem"
+          description="Visit Filecoin Foundation's Ecosystem Explorer to discover Filecoin projects, get inspired by what others are building, and find your place in the Filecoin ecosystem."
+          cta={
+            <Button href="https://fil.org/ecosystem-explorer" variant="primary">
+              Visit ecosystem explorer
+            </Button>
+          }
+        />
+
+        <SectionImage {...graphicsData.buildOnFilecoinSection} />
       </PageSection>
     </>
   )
