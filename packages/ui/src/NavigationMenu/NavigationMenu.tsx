aff72eaed9d4cde5fc3e57d3b79a851957701df3
Charly Martin <charly@hey.com>
2025-08-19 11:05:41 -0400

    refactor: Extract NavigationPopover to ui/NavigationMenu
    This commit moves the `NavigationPopover` component from the `ff-site` application into the shared `ui` package, renaming it to `NavigationMenu` to make it a reusable component.
    
    The `DesktopNavigation` in `ff-site` is updated to use this new shared component, promoting code reuse and consistency. The `mainNavItemStyles` prop has been renamed to `labelClassName` for better clarity.

---
+++ packages/ui/src/NavigationMenu/NavigationMenu.tsx
@@ -0,0 +1,59 @@
+'use client'
+
+import { Popover, PopoverButton, PopoverPanel } from '@headlessui/react'
+import { CaretDownIcon } from '@phosphor-icons/react'
+import { clsx } from 'clsx'
+
+import { Icon } from '@filecoin-foundation/ui/Icon'
+
+type PopOverProps = {
+  label: string
+  labelClassName: string
+  as: React.ElementType
+  children: React.ReactElement
+}
+
+const SPACE_BETWEEN_PANEL_AND_BUTTON = 24
+const SPACE_BETWEEN_PANEL_AND_VIEWPORT = 8
+
+export function NavigationMenu({
+  as,
+  label,
+  labelClassName,
+  children,
+}: PopOverProps) {
+  return (
+    <Popover as={as}>
+      <PopoverButton
+        aria-label={`${label} (opens a navigation menu)`}
+        className={clsx('group', labelClassName)}
+      >
+        <span>{label}</span>
+        <span className="transition-transform group-data-open:rotate-180">
+          <Icon component={CaretDownIcon} size={20} />
+        </span>
+      </PopoverButton>
+      <PopoverPanel
+        transition
+        className="z-10 transition duration-200 ease-out data-closed:translate-y-1 data-closed:opacity-0 data-open:translate-y-0 data-open:opacity-100"
+        anchor={{
+          to: 'bottom',
+          gap: SPACE_BETWEEN_PANEL_AND_BUTTON,
+          padding: SPACE_BETWEEN_PANEL_AND_VIEWPORT,
+        }}
+      >
+        {(props) => (
+          <div
+            className="navigation-menu-panel"
+            onClick={(e) => {
+              e.stopPropagation()
+              props.close()
+            }}
+          >
+            {children}
+          </div>
+        )}
+      </PopoverPanel>
+    </Popover>
+  )
+}
