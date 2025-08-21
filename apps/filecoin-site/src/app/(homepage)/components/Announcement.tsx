067dd2b33434c077d12ffb7f51d799a0b31fe884
Barbara Peric <b.peric10@gmail.com>
2025-08-21 13:21:32 +0100

    Enhance Announcement Component Styles and Add Hover States

--- apps/filecoin-site/src/app/(homepage)/components/Announcement.tsx
+++ apps/filecoin-site/src/app/(homepage)/components/Announcement.tsx
@@ -13,20 +13,20 @@ export type AnnouncementProps = {
 
 export function Announcement({ children, href, centered }: AnnouncementProps) {
   return (
-    <div className={clsx('flex', centered && 'justify-center')}>
+    <div className={clsx('group flex', centered && 'justify-center')}>
       <Link
         href={href}
         aria-label={`${children} - Click to learn more`}
         className="group focus:brand-outline rounded-full"
       >
         <aside
-          className="gap relative flex items-center rounded-full border border-[var(--color-announcement-border)] bg-[var(--color-announcement-background)] p-1 group-focus:bg-[var(--color-announcement-background-hover)] hover:bg-[var(--color-announcement-background-hover)]"
+          className="gap relative flex items-center rounded-full border border-[var(--color-announcement-border)] bg-[var(--color-announcement-background)] p-1 group-hover:bg-[var(--color-announcement-background-hover)] group-focus:bg-[var(--color-announcement-background-hover)]"
           role="banner"
         >
           <span className="ml-4 text-sm font-medium sm:text-base">
             {children}
           </span>
-          <span className="ml-4 grid size-8 shrink-0 place-items-center rounded-full bg-[var(--color-announcement-icon-background)] text-[--color-announcement-icon]">
+          <span className="ml-4 grid size-8 shrink-0 place-items-center rounded-full bg-[var(--color-announcement-icon-background)] text-[--color-announcement-icon] group-hover:bg-[var(--color-announcement-icon-background-hover)] group-focus:bg-[var(--color-announcement-icon-background-hover)]">
             <Icon component={ArrowRightIcon} size={16} />
           </span>
         </aside>
