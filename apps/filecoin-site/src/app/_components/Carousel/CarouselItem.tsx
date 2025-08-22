05f18ff6b76d9c48a27f4b7fe1943a2fff2caa1a
Mirha Masala <mirha@hey.com>
2025-08-22 09:35:31 +0100

    CR - Refactor CarouselItem

---
+++ apps/filecoin-site/src/app/_components/Carousel/CarouselItem.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { clsx } from 'clsx'
+
+import { useCarousel } from './Carousel'
+
+export function CarouselItem({
+  className,
+  ...props
+}: React.ComponentProps<'div'>) {
+  const { orientation } = useCarousel()
+
+  return (
+    <div
+      role="group"
+      aria-roledescription="slide"
+      data-slot="carousel-item"
+      className={clsx(
+        'min-w-0 shrink-0 grow-0 basis-full md:basis-1/3 lg:basis-1/4 xl:basis-1/5',
+        orientation === 'horizontal' ? 'pl-4' : 'pt-4',
+        className,
+      )}
+      {...props}
+    />
+  )
+}
