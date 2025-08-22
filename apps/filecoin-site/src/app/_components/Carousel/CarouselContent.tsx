081c94081aab1e253a763700f035ef7967ced93e
Mirha Masala <mirha@hey.com>
2025-08-22 09:35:18 +0100

    CR - Refactor Carousel Content

---
+++ apps/filecoin-site/src/app/_components/Carousel/CarouselContent.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { clsx } from 'clsx'
+
+import { useCarousel } from './Carousel'
+
+export function CarouselContent({
+  className,
+  ...props
+}: React.ComponentProps<'div'>) {
+  const { carouselRef, orientation } = useCarousel()
+
+  return (
+    <div
+      ref={carouselRef}
+      className="overflow-hidden"
+      data-slot="carousel-content"
+    >
+      <div
+        className={clsx(
+          'flex',
+          orientation === 'horizontal' ? '-ml-4' : '-mt-4 flex-col',
+          className,
+        )}
+        {...props}
+      />
+    </div>
+  )
+}
