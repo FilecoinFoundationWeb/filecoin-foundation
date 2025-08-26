b8588ef1dbcd188b2a0a1527f8cf93b6e79223fc
Mirha Masala <mirha@hey.com>
2025-08-26 12:19:08 +0100

    Provide Storage Onboarding page - Change copy

--- apps/filecoin-site/src/app/provide-storage/onboarding/page.tsx
+++ apps/filecoin-site/src/app/provide-storage/onboarding/page.tsx
@@ -16,13 +16,11 @@ export default function ProvideStorageFormPage() {
       <PageSection backgroundVariant="light">
         <div className="max-w-4xl">
           <Heading tag="h2" className="text-5xl/14 font-medium">
-            Thanks for your interest in offering storage on the Filecoin
-            network.
+            Thanks for your interest in becoming a Filecoin storage provider.
           </Heading>
           <p className="pt-6 pb-25 text-xl/8 text-pretty text-[var(--color-paragraph-text)]">
             Please fill out this form below, and a member of the onboarding team
-            will reach out to guide you through next steps in becoming a storage
-            provider.
+            will reach out to guide you through next steps.
           </p>
 
           <ProvideStorageForm />
