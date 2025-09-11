d51d9af0b723ea4d7c13eb874b3915c990b12c9f
Barbara Peric <b.peric10@gmail.com>
2025-09-11 11:53:02 +0100

    Update default branch in TinaCMS configuration to 'main'

--- apps/filecoin-site/tina/config.ts
+++ apps/filecoin-site/tina/config.ts
@@ -3,10 +3,9 @@ import { defineConfig } from 'tinacms'
 import { blogFields } from './schema/blog'
 
 export default defineConfig({
-  branch: process.env.VERCEL_GIT_COMMIT_REF || '',
+  branch: process.env.VERCEL_GIT_COMMIT_REF || 'main',
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
   token: process.env.TINA_TOKEN,
-
   build: {
     outputFolder: 'admin',
     publicFolder: 'public',
