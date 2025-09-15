6f657070bf5f9e046e4ac47b098f784c2ff8c6cd
Barbara Peric <50910606+barbaraperic@users.noreply.github.com>
2025-09-15 16:53:31 +0100

    Add TinaCMS config file (#1767)
    - Added TinaCMS as a dependency to facilitate content management for the blog.
    - Created a configuration file for TinaCMS to define the content schema and media settings.
    - Introduced a blog schema with fields for title, draft status, body content, excerpt, images, author, publication date, categories, and SEO settings.
    - Updated package.json and turbo.json to include necessary environment variables for TinaCMS.
    
    This integration enhances the content editing experience and allows for better management of blog posts within the application.

---
+++ apps/filecoin-site/tina/config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'tinacms'
+
+import { blogFields } from './schema/blog'
+
+export default defineConfig({
+  branch: 'main',
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  token: process.env.TINA_TOKEN,
+
+  build: {
+    outputFolder: 'admin',
+    publicFolder: 'public',
+  },
+  media: {
+    tina: {
+      mediaRoot: '',
+      publicFolder: 'public',
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: 'post',
+        label: 'Posts',
+        path: 'content/posts',
+        format: 'md',
+        ui: {
+          router: ({ document }) => `/blog/${document._sys.filename}`,
+        },
+        fields: blogFields('post'),
+      },
+    ],
+  },
+})
