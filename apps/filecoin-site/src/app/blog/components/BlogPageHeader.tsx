c4abe83073fc577bc2d87efc3bfccaea1c7a2957
Mirha Masala <mirha@hey.com>
2025-09-24 12:23:31 +0100

    Create BlogPageHeader component

---
+++ apps/filecoin-site/src/app/blog/components/BlogPageHeader.tsx
@@ -0,0 +1,60 @@
+import Image, { type ImageProps } from 'next/image'
+
+import { PATHS } from '@/constants/paths'
+
+import { Button } from '@/components/Button'
+import { Heading } from '@/components/Heading'
+
+import type { BlogPost } from '../types/blogPostType'
+
+type BlogPageHeaderProps = {
+  title: BlogPost['title']
+  description: BlogPost['excerpt']
+  slug: BlogPost['slug']
+  image: ImageProps
+}
+
+export function BlogPageHeader({
+  title,
+  description,
+  image,
+  slug,
+}: BlogPageHeaderProps) {
+  return (
+    <header className="grid grid-cols-1 gap-6 lg:grid-cols-2">
+      <div className="flex flex-col justify-between gap-10">
+        <div className="flex flex-col gap-6">
+          {/* todo */}
+          <span className="font-mono text-zinc-600">Latest updates</span>
+          <Heading tag="h1" variant="section-heading">
+            {title}
+          </Heading>
+          {/* todo */}
+          <p className="text-pretty text-[var(--color-paragraph-text)] md:text-xl/7">
+            {description}
+          </p>
+        </div>
+
+        <Button
+          variant="primary"
+          href={`${PATHS.BLOG.path}/${slug}`}
+          className="w-full lg:w-fit"
+        >
+          Read full article
+        </Button>
+      </div>
+
+      <div className="relative aspect-[3/2]">
+        <Image
+          fill
+          priority
+          className="rounded-2xl"
+          quality={100}
+          sizes="100vw"
+          src={image.src}
+          alt={image.alt}
+        />
+      </div>
+    </header>
+  )
+}
