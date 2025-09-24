3617db8d99a2a56442fb4f744693f1ea5032abd2
Mirha Masala <mirha@hey.com>
2025-09-24 15:13:43 +0100

    Redesign BlogPostHeader

--- apps/filecoin-site/src/app/blog/components/BlogPostHeader.tsx
+++ apps/filecoin-site/src/app/blog/components/BlogPostHeader.tsx
@@ -1,28 +1,25 @@
-import Image from 'next/image'
+import Image, { type ImageProps } from 'next/image'
 
 import { ShareArticle } from '@filecoin-foundation/ui/ShareArticle'
 
 import { PATHS } from '@/constants/paths'
 import { BASE_URL } from '@/constants/siteMetadata'
 
-import { graphicsData } from '@/data/graphicsData'
-
 import { Heading } from '@/components/Heading'
 import { SectionDivider } from '@/components/SectionDivider'
 
 import type { BlogPost } from '../types/blogPostType'
 
-import { BlogPostContainer } from './BlogPostContainer'
 import { Categories } from './Categories'
 import { PostMetadata } from './PostMetadata'
 
 type BlogPostHeaderProps = {
-  image?: string
   categories: BlogPost['categories']
   title: BlogPost['title']
-  slug: string
-  author?: BlogPost['author']
   date: BlogPost['publishedOn']
+  slug: BlogPost['slug']
+  image: ImageProps
+  author?: BlogPost['author']
 }
 
 export function BlogPostHeader({
@@ -35,39 +32,40 @@ export function BlogPostHeader({
 }: BlogPostHeaderProps) {
   return (
     <header>
-      <div className="relative mb-10 min-h-80">
+      <div className="space-y-10">
+        <Categories categories={categories} />
+
+        <Heading tag="h1" variant="section-heading">
+          {title}
+        </Heading>
+
+        <SectionDivider />
+
+        <div className="flex flex-col justify-between gap-4 md:flex-row">
+          <PostMetadata author={author} date={date} />
+
+          <div className="flex flex-col justify-between gap-15 md:flex-row md:gap-4">
+            <ShareArticle
+              articleTitle={title}
+              path={`${PATHS.BLOG.path}/${slug}`}
+              baseUrl={BASE_URL}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="relative my-12 aspect-video">
         <Image
           fill
           priority
           quality={100}
           fetchPriority="high"
-          src={image || graphicsData.fallback.data.src}
-          alt=""
-          className="object-cover"
+          src={image.src}
+          alt={image.alt}
+          className="rounded-2xl object-cover"
           sizes="100vw"
         />
       </div>
-
-      <BlogPostContainer>
-        <div className="mb-10 flex flex-col justify-between gap-15 md:flex-row md:gap-4">
-          <Categories categories={categories} />
-          <ShareArticle
-            articleTitle={title}
-            path={`${PATHS.BLOG.path}/${slug}`}
-            baseUrl={BASE_URL}
-          />
-        </div>
-
-        <SectionDivider />
-
-        <div className="mt-15 mb-4">
-          <PostMetadata author={author} date={date} />
-        </div>
-
-        <Heading tag="h1" className="text-4xl font-medium">
-          {title}
-        </Heading>
-      </BlogPostContainer>
     </header>
   )
 }
