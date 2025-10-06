0ea55e8daaa4205a5be8d130228d878ae2003811
Barbara Peric <b.peric10@gmail.com>
2025-10-06 11:47:27 +0100

    feat: add new images and update Card component for enhanced content presentation
    - Introduced new WebP images: archive-room.webp, computer-motherboard-circuit.webp, historic-library.webp, and test-tubes.webp for improved visual content.
    - Updated the Learn page to reflect changes in the description and CTA text for clarity.
    - Refactored Card component to support image props, enhancing the visual representation of content in the CardGrid.
    - Adjusted data structure in whatIsFilecoinUsedFor to include image data for better accessibility and maintainability.

--- apps/filecoin-site/src/app/_components/Card.tsx
+++ apps/filecoin-site/src/app/_components/Card.tsx
@@ -1,3 +1,6 @@
+import { CardImage } from '@filecoin-foundation/ui/Card/CardImage'
+import type { StaticImageProps } from '@filecoin-foundation/utils/types/imageType'
+
 import { Heading } from './Heading'
 import { IconBadge, type IconBadgeProps } from './IconBadge'
 
@@ -5,15 +8,20 @@ type CardProps = {
   as: 'li' | 'article' | 'div'
   title: string
   description: string
+  image?: StaticImageProps
   icon?: IconBadgeProps['component']
 }
 
-export type CardData = Pick<CardProps, 'title' | 'description' | 'icon'>
+export type CardData = Pick<
+  CardProps,
+  'title' | 'description' | 'icon' | 'image'
+>
 
-export function Card({ as: Tag, title, description, icon }: CardProps) {
+export function Card({ as: Tag, title, description, icon, image }: CardProps) {
   return (
     <Tag className="flex flex-col gap-6">
       {icon && <IconBadge component={icon} size="md" />}
+      {image && <CardImage image={image} />}
 
       <div className="space-y-2">
         <Heading tag="h3" variant="card-heading">
