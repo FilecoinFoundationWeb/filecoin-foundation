692ab90d90a64a7b40a0e27ec6364d782b260a74
Barbara Peric <50910606+barbaraperic@users.noreply.github.com>
2025-10-22 10:51:54 +0100

    [FILECOIN][UXIT-3397] Learn Page · Rework “What is Filecoin Used For” section [skip percy] (#1821)
    * feat: add new images and update Card component for enhanced content presentation
    
    - Introduced new WebP images: archive-room.webp, computer-motherboard-circuit.webp, historic-library.webp, and test-tubes.webp for improved visual content.
    - Updated the Learn page to reflect changes in the description and CTA text for clarity.
    - Refactored Card component to support image props, enhancing the visual representation of content in the CardGrid.
    - Adjusted data structure in whatIsFilecoinUsedFor to include image data for better accessibility and maintainability.
    
    * Refactor Card component types to improve type safety and flexibility. Introduced BaseCardProps and defined specific card types: CardWithIcon, CardWithImage, and CardWithText, enhancing the component's adaptability for different use cases.

--- apps/filecoin-site/src/app/_components/Card.tsx
+++ apps/filecoin-site/src/app/_components/Card.tsx
@@ -1,19 +1,42 @@
+import { CardImage } from '@filecoin-foundation/ui/Card/CardImage'
+import type { StaticImageProps } from '@filecoin-foundation/utils/types/imageType'
+
 import { Heading } from './Heading'
 import { IconBadge, type IconBadgeProps } from './IconBadge'
 
-type CardProps = {
+type BaseCardProps = {
   as: 'li' | 'article' | 'div'
   title: string
   description: string
-  icon?: IconBadgeProps['component']
 }
 
-export type CardData = Pick<CardProps, 'title' | 'description' | 'icon'>
+type CardWithIcon = BaseCardProps & {
+  icon: IconBadgeProps['component']
+  image?: never
+}
 
-export function Card({ as: Tag, title, description, icon }: CardProps) {
+type CardWithImage = BaseCardProps & {
+  image: StaticImageProps
+  icon?: never
+}
+
+type CardWithText = BaseCardProps & {
+  icon?: never
+  image?: never
+}
+
+type CardProps = CardWithIcon | CardWithImage | CardWithText
+
+export type CardData = Pick<
+  CardProps,
+  'title' | 'description' | 'icon' | 'image'
+>
+
+export function Card({ as: Tag, title, description, icon, image }: CardProps) {
   return (
     <Tag className="flex flex-col gap-6">
       {icon && <IconBadge component={icon} size="md" />}
+      {image && <CardImage image={image} />}
 
       <div className="space-y-2">
         <Heading tag="h3" variant="card-heading">
