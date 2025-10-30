057f30aa769be1f4d27ef8c7e5ae2fe0f1f6044f
Charly Martin <charly@hey.com>
2025-10-30 16:03:20 +0000

    Implement NavigationMenuLink component with external and internal link handling; update styles and types for navigation menu

--- packages/ui-filecoin/src/Navigation/types/index.ts
+++ packages/ui-filecoin/src/Navigation/types/index.ts
@@ -1,5 +1,25 @@
+import type {
+  VariantClasses,
+  NavigationMenuLinkProps as SharedNavigationMenuLinkProps,
+} from '../NavigationMenuLink'
+
+// Should be moved somewhere else maybe
 export type TouchTarget = {
   visibleElementSize?: string | number
   touchAreaOffset?: string
   touchAreaPadding: string
 }
+
+export type LinkItemProps = {
+  href: string
+  label: string
+  description?: string
+}
+
+export type LocalNavigationMenuLinkProps<Variants extends VariantClasses> =
+  Omit<
+    SharedNavigationMenuLinkProps<Variants>,
+    'variants' | 'baseDomain' | 'InternalLinkComponent'
+  > & {
+    variant?: keyof Variants
+  }
