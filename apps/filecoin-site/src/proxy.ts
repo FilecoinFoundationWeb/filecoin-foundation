92e60f1e218679733f59d8c9e04d8cd71228cd74
Mirha Masala <mirha@hey.com>
2025-10-31 12:25:20 +0000

    Trigger deployment

--- apps/filecoin-site/src/proxy.ts
+++ apps/filecoin-site/src/proxy.ts
@@ -7,7 +7,7 @@ const intlMiddleware = createMiddleware(routing)
 export default intlMiddleware
 
 export const config = {
-  // Match all pathnames except for
+  // Match all pathnames except for -
   // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
   // - … the ones containing a dot (e.g. `favicon.ico`)
   matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)',
