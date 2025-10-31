a8887311f0131611acd5b59ca1e2b5a2978ad6b4
Charly Martin <charly@hey.com>
2025-10-31 10:28:45 +0000

    Move NavigationMenuPanel

--- packages/ui-filecoin/src/Navigation/types/index.ts
+++ packages/ui-filecoin/src/Navigation/types/index.ts
@@ -23,3 +23,15 @@ export type LocalNavigationMenuLinkProps<Variants extends VariantClasses> =
   > & {
     variant?: keyof Variants
   }
+
+type NavItem = { label: string; href: string }
+
+export type ExpandedNavItem = NavItem & { description: string }
+
+export type NavigationMenuItem = {
+  label: string
+  items: Array<{
+    title: string
+    links: Array<ExpandedNavItem>
+  }>
+}
