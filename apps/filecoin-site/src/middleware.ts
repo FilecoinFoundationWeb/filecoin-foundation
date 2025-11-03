f6b19721ea291c1f145bd1becd2527afed786ced
Mirha Masala <26620750+mirhamasala@users.noreply.github.com>
2025-11-03 11:40:51 +0000

    Revert "chore: upgrade Next.js to 16.0.1 and migrate middleware (#1866)" (#1873)
    This reverts commit 7c6ac565f1c30a56020f8f354ded0c42b42cf8e0.

--- apps/filecoin-site/src/middleware.ts
+++ apps/filecoin-site/src/middleware.ts
@@ -2,9 +2,8 @@ import { routing } from '@/i18n/routing'
 
 import createMiddleware from 'next-intl/middleware'
 
-const intlMiddleware = createMiddleware(routing)
 
-export default intlMiddleware
+export default createMiddleware(routing)
 
 export const config = {
   // Match all pathnames except for
