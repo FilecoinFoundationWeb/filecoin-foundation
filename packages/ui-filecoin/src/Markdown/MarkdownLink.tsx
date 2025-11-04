d185ef5b8ac0e67ccb2fec4d0db4fd91d2af5093
Barbara Peric <b.peric10@gmail.com>
2025-11-04 15:31:49 +0000

    Refactor Markdown components: Move MarkdownContent and MarkdownLink to ui-filecoin package, updating imports and structure for improved modularity. Introduce MarkdownPage component for consistent article formatting.

---
+++ packages/ui-filecoin/src/Markdown/MarkdownLink.tsx
@@ -0,0 +1,38 @@
+import type { ComponentPropsWithoutRef } from 'react'
+
+import * as Sentry from '@sentry/nextjs'
+import { clsx } from 'clsx'
+
+import { SmartTextLink } from '../TextLink/SmartTextLink'
+
+type MarkdownLinkProps = ComponentPropsWithoutRef<'a'> & {
+  baseDomain: string
+}
+
+export function MarkdownLink({
+  href,
+  children,
+  baseDomain,
+  className,
+  ...props
+}: MarkdownLinkProps) {
+  if (!href) {
+    const errorMessage = `Invalid markdown: link is missing href attribute for text "${children}"`
+
+    console.error(errorMessage)
+    Sentry.captureException(new Error(errorMessage))
+
+    return <>{children}</>
+  }
+
+  return (
+    <SmartTextLink
+      href={href}
+      baseDomain={baseDomain}
+      className={clsx('not-prose', className)}
+      {...props}
+    >
+      {children}
+    </SmartTextLink>
+  )
+}
