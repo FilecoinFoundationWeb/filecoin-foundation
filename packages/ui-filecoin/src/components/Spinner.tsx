d6f912a0189649538a1233b9a55623b420856d00
Barbara Peric <50910606+barbaraperic@users.noreply.github.com>
2025-11-11 11:57:50 +0000

    [UI-Filecoin][UXIT-3610] Create Spinner Component (#1894)
    * Add Spinner component export to filecoin-ui package
    
    * Refactor Spinner component to use SpinnerIcon and add text prop for customizable loading messages
    
    * CR
    
    * Conflict resolution
    
    ---------
    
    Co-authored-by: Mirha Masala <mirha@hey.com>

---
+++ packages/ui-filecoin/src/components/Spinner.tsx
@@ -0,0 +1,27 @@
+import { SpinnerIcon } from '@phosphor-icons/react/dist/ssr'
+
+import { Icon, type IconProps } from './Icon'
+
+type SpinnerProps = {
+  size?: IconProps['size']
+  weight?: IconProps['weight']
+} & (
+  | { message: string; ariaLabel?: string }
+  | { message?: never; ariaLabel: string }
+)
+
+export function Spinner({ message, ariaLabel, size = 20 }: SpinnerProps) {
+  const accessibleLabel = ariaLabel || message
+
+  return (
+    <div className="flex flex-col items-center gap-2" role="status">
+      <span
+        className="text-brand-700 shrink-0 animate-spin"
+        aria-label={accessibleLabel}
+      >
+        <Icon component={SpinnerIcon} size={size} />
+      </span>
+      {message && <p>{message}</p>}
+    </div>
+  )
+}
