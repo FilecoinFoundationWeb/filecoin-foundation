5ea76cb3fdc2c8767e7d84c36c70dc7d932ea791
Barbara Peric <50910606+barbaraperic@users.noreply.github.com>
2025-11-12 09:35:22 +0000

    [UI-Filecoin][UXIT-3610] Create Loading Component - part 2 (#1902)
    * Add LoadingContainer component export to ui-filecoin package
    
    * CR
    
    * Scout CR - Alphabetize
    
    * Update dependencies
    
    ---------
    
    Co-authored-by: Mirha Masala <mirha@hey.com>

---
+++ packages/ui-filecoin/src/components/LoadingStateCard.tsx
@@ -0,0 +1,18 @@
+import { Spinner } from './Spinner'
+import { StateCard } from './StateCard'
+
+type LoadingStateCardProps = {
+  message: string
+}
+
+export function LoadingStateCard({
+  message = 'Loading...',
+}: LoadingStateCardProps) {
+  return (
+    <StateCard variant="solid">
+      <div className="flex min-h-60 w-full items-center justify-center">
+        <Spinner message={message} />
+      </div>
+    </StateCard>
+  )
+}
