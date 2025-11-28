4aa3e4dbe594ee02e23250d11923015f721ab863
Charly Martin <charly@hey.com>
2025-11-28 17:48:10 +0000

    feat: update form components

--- packages/ui-filecoin/src/components/Input.tsx
+++ packages/ui-filecoin/src/components/Input.tsx
@@ -8,13 +8,13 @@ type InputProps = {
   onChange: (value: string) => void
 } & Omit<HeadlessInputProps, 'onChange'>
 
-export default function Input({ onChange, className, ...rest }: InputProps) {
+export function Input({ onChange, className, ...rest }: InputProps) {
   return (
     <HeadlessInput
       {...rest}
       onChange={(event) => onChange(event.target.value)}
       className={clsx(
-        'focus:brand-outline block w-full rounded-lg border border-(--input-border-color) p-3 placeholder:text-(--input-placeholder-color)',
+        'focus:brand-outline block w-full rounded-lg border border-(--input-border-color) p-3 text-(--color-text-base) placeholder:text-(--input-placeholder-color)',
         className,
       )}
     />
