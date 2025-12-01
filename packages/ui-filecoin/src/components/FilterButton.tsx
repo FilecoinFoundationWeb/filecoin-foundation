adcd0b6b88295f1da40da2717ea76c56a33a7493
Charly MARTIN <charly@hey.com>
2025-12-01 16:47:48 +0000

    Cm/fix-filter-button (#1981)
    * export FilterButtonProps pass button props to FilterButton
    
    * Add changetset

--- packages/ui-filecoin/src/components/FilterButton.tsx
+++ packages/ui-filecoin/src/components/FilterButton.tsx
@@ -2,14 +2,14 @@ import { Button, type ButtonProps } from '@headlessui/react'
 
 import { Icon as IconComponent, type IconProps } from './Icon'
 
-type FilterButtonProps = {
+export type FilterButtonProps = {
   Icon: IconProps['component']
   children: React.ReactNode
 } & Omit<ButtonProps, 'className'>
 
-export function FilterButton({ children, Icon }: FilterButtonProps) {
+export function FilterButton({ children, Icon, ...rest }: FilterButtonProps) {
   return (
-    <Button className="listbox-button">
+    <Button className="listbox-button" {...rest}>
       <span className="flex items-center gap-2">
         <IconComponent component={Icon} size={20} />
         {children}
