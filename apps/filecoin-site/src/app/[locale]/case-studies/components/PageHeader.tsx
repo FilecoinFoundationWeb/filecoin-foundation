aef059742368a7a53c8bea709b41bf8da1bbadf0
Charly Martin <charly@hey.com>
2025-12-10 12:13:05 +0000

    refactor(ui): reuse shared PageHeader and update Badge theming

--- apps/filecoin-site/src/app/[locale]/case-studies/components/PageHeader.tsx
+++ apps/filecoin-site/src/app/[locale]/case-studies/components/PageHeader.tsx
@@ -1,54 +1,44 @@
-import type { ReactNode } from 'react'
-
 import Image from 'next/image'
-
 import type { ImageProps } from 'next/image'
 
 import { Badge } from '@filecoin-foundation/ui-filecoin/Badge'
-
 import {
-  Heading,
-  type HeadingProps,
-} from '@filecoin-foundation/ui-filecoin/Heading'
+  PageHeader as SharedPageHeader,
+  type PageHeaderProps as SharedPageHeaderProps,
+} from '@filecoin-foundation/ui-filecoin/PageHeader'
 
 type PageHeaderProps = {
-  title: HeadingProps['children']
-  description?: ReactNode
   label?: string
   image?: ImageProps
-}
+} & SharedPageHeaderProps
 
 export function PageHeader({
-  title,
-  description,
   label,
   image,
+  ...pageHeaderProps
 }: PageHeaderProps) {
   return (
-    <header className="space-y-10">
-      {label && (
-        <div className="flex">
-          <Badge variant="primaryLight">{label}</Badge>
-        </div>
-      )}
-
-      <div className="grid max-w-full grid-cols-1 items-center gap-25 lg:grid-cols-2 lg:gap-15">
-        <div className="space-y-6">
-          <Heading tag="h1" variant="page-heading">
-            {title}
-          </Heading>
-
-          <p className="text-pretty text-(--color-paragraph-text) md:text-xl/7">
-            {description}
-          </p>
-        </div>
-
-        {image && (
-          <div className="relative aspect-video w-full object-cover">
-            <Image fill src={image.src} alt={image.alt || ''} />
+    <header className="grid max-w-full grid-cols-1 gap-25 xl:grid-cols-2 xl:gap-15">
+      <div className="space-y-6">
+        {label && (
+          <div className="flex">
+            <Badge variant="primary">{label}</Badge>
           </div>
         )}
+
+        <SharedPageHeader {...pageHeaderProps} />
       </div>
+
+      {image && (
+        <div className="relative aspect-video w-full object-cover">
+          <Image
+            fill
+            src={image.src}
+            alt={image.alt || ''}
+            className="rounded-xl"
+          />
+        </div>
+      )}
     </header>
   )
 }
