8dffb5ba50c459692489684452025ecfb1fcebd4
Charly Martin <charly@hey.com>
2025-12-11 15:18:12 +0000

    refactor(ui-filecoin): replace NetworkProvider with useNetwork hook

--- apps/filecoin-site/src/app/_components/_storybook/_NetworkSelectorShowcase.tsx
+++ apps/filecoin-site/src/app/_components/_storybook/_NetworkSelectorShowcase.tsx
@@ -1,18 +1,15 @@
-import { NetworkProvider } from '@filecoin-foundation/ui-filecoin/Network/NetworkProvider'
 import { NetworkSelector } from '@filecoin-foundation/ui-filecoin/Network/NetworkSelector'
 import { PageSection } from '@filecoin-foundation/ui-filecoin/PageSection'
 import { SectionContent } from '@filecoin-foundation/ui-filecoin/SectionContent'
 
 export function NetworkSelectorShowcase() {
   return (
-    <NetworkProvider>
-      <PageSection backgroundVariant="light">
-        <SectionContent centerTitle title="Network Selector Showcase">
-          <div className="max-w-md">
-            <NetworkSelector />
-          </div>
-        </SectionContent>
-      </PageSection>
-    </NetworkProvider>
+    <PageSection backgroundVariant="light">
+      <SectionContent centerTitle title="Network Selector Showcase">
+        <div className="max-w-md">
+          <NetworkSelector />
+        </div>
+      </SectionContent>
+    </PageSection>
   )
 }
--- packages/ui-filecoin/src/components/Network/utils/index.ts
+++ packages/ui-filecoin/src/components/Network/utils/index.ts
@@ -6,6 +6,16 @@ export const parseAsChainId = createParser({
   parse(queryValue) {
     const integer = parseAsInteger.parse(queryValue)
     const validChainId = supportedChainIds.find((id) => id === integer)
+
+    if (
+      process.env.NODE_ENV === 'development' &&
+      typeof validChainId === 'undefined'
+    ) {
+      console.warn(
+        `Invalid chain ID "${integer}" provided. Falling back to ${defaultChain.name} (${defaultChain.id})`,
+      )
+    }
+
     return validChainId || defaultChain.id
   },
   serialize: parseAsInteger.serialize,
