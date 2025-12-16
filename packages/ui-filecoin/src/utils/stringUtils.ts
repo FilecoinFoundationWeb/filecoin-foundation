e70458991d43a4bebf7eee14cfa4565fba5fb046
Mirha Masala <mirha@hey.com>
2025-12-16 20:12:41 +0000

    CR - Edit capitalize function with trim and lowercase handling

--- packages/ui-filecoin/src/utils/stringUtils.ts
+++ packages/ui-filecoin/src/utils/stringUtils.ts
@@ -1,3 +1,4 @@
-export function capitalize(string: string) {
-  return string.charAt(0).toUpperCase() + string.slice(1)
+export function capitalize(str: string) {
+  const trimmed = str.trim()
+  return trimmed.charAt(0).toUpperCase() + trimmed.slice(1).toLowerCase()
 }
