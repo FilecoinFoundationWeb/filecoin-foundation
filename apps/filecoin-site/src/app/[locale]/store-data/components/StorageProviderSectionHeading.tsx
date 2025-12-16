6c0d9a54614d468b94da9828bbca93a7083ce394
Mirha Masala <mirha@hey.com>
2025-12-16 11:13:28 +0000

    CR - Use Heading component

--- apps/filecoin-site/src/app/[locale]/store-data/components/StorageProviderSectionHeading.tsx
+++ apps/filecoin-site/src/app/[locale]/store-data/components/StorageProviderSectionHeading.tsx
@@ -1,9 +1,18 @@
+import {
+  Heading,
+  type HeadingProps,
+} from '@filecoin-foundation/ui-filecoin/Heading'
+
 type StorageProviderSectionHeadingProps = {
-  children: string
+  children: HeadingProps['children']
 }
 
 export function StorageProviderSectionHeading({
   children,
 }: StorageProviderSectionHeadingProps) {
-  return <h4 className="text-4xl font-medium">{children}</h4>
+  return (
+    <Heading tag="h4" className="text-4xl font-medium">
+      {children}
+    </Heading>
+  )
 }
