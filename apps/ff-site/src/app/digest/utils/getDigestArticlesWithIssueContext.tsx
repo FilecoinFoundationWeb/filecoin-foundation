5d2c113a6e36ccc945a2b4881aaad0f42440478f
Mirha Masala <mirha@hey.com>
2025-12-30 17:04:13 +0000

    CR - Refactor getDigestArticlesWithIssueContext and getDigestIssueData to use DigestIssueFrontmatter type for issueNumber parameter

--- apps/ff-site/src/app/digest/utils/getDigestArticlesWithIssueContext.tsx
+++ apps/ff-site/src/app/digest/utils/getDigestArticlesWithIssueContext.tsx
@@ -1,8 +1,11 @@
 import { getDigestArticlesWithIssueContext as sharedGetDigestArticlesWithIssueContext } from '@filecoin-foundation/utils/getDigestArticlesWithIssueContext'
+import type { DigestIssueFrontmatter } from '@filecoin-foundation/utils/schemas/DigestIssueFrontmatterSchema'
 
 import { PATHS } from '@/constants/paths'
 
-export async function getDigestArticlesWithIssueContext(issueNumber: number) {
+export async function getDigestArticlesWithIssueContext(
+  issueNumber: DigestIssueFrontmatter['issue-number'],
+) {
   return sharedGetDigestArticlesWithIssueContext({
     issueNumber,
     directoryPath: PATHS.DIGEST.articlesPath,
--- apps/ff-site/src/app/digest/utils/getDigestIssueData.tsx
+++ apps/ff-site/src/app/digest/utils/getDigestIssueData.tsx
@@ -2,10 +2,13 @@ import {
   getAllDigestIssuesData as sharedGetAllDigestIssuesData,
   getDigestIssueData as sharedGetDigestIssueData,
 } from '@filecoin-foundation/utils/getDigestIssueData'
+import type { DigestIssueFrontmatter } from '@filecoin-foundation/utils/schemas/DigestIssueFrontmatterSchema'
 
 import { PATHS } from '@/constants/paths'
 
-export async function getDigestIssueData(issueNumber: number) {
+export async function getDigestIssueData(
+  issueNumber: DigestIssueFrontmatter['issue-number'],
+) {
   return sharedGetDigestIssueData(issueNumber, PATHS.DIGEST.issuesPath)
 }
 
