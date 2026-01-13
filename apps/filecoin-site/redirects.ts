9945007b9dab8ec1b76cd62264d8f8e55bc60721
Barbara Peric <b.peric10@gmail.com>
2026-01-13 16:30:09 +0000

    Add blog post for June 2020 roadmap update and update redirects
    - Introduced a new blog post detailing the June 2020 roadmap update, including significant project highlights and future milestones.
    - Updated the redirects configuration to include a permanent redirect from a sample blog post to the new roadmap update post.

--- apps/filecoin-site/redirects.ts
+++ apps/filecoin-site/redirects.ts
@@ -1,1 +1,7 @@
-export const redirects = []
+export const redirects = [
+  {
+    source: '/blog/this-is-a-sample-post/',
+    destination: '/blog/filecoin-roadmap-update-june-2020',
+    permanent: true,
+  },
+]
