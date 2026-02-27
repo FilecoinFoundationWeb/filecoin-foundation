2376513de8cd624527eb87f007da960f16041f9f
Charly Martin <charly@hey.com>
2026-02-27 17:13:44 +0000

    fixup! refactor: export Direction type and enhance pagination type safety

--- packages/ui-filecoin/src/components/Pagination/index.ts
+++ packages/ui-filecoin/src/components/Pagination/index.ts
@@ -1,3 +1,4 @@
 export * from './Pagination'
 
 export { usePagination } from '@filecoin-foundation/hooks/usePagination'
+export type { PaginationLabels } from '@filecoin-foundation/utils/constants/paginationLabels'
