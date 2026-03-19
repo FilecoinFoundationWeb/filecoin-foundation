38024f1b6d9f676c27713c4c05e89c2f234a37ca
Charly Martin <charly@hey.com>
2026-03-19 11:29:55 +0000

    [UI-Filecoin] Set mainnet as default chain instead of calibration

--- packages/ui-filecoin/src/components/Network/config/index.ts
+++ packages/ui-filecoin/src/components/Network/config/index.ts
@@ -3,7 +3,7 @@ import { filecoin, filecoinCalibration } from 'wagmi/chains'
 const mainnet = { ...filecoin, name: 'Mainnet' } as const
 const calibration = { ...filecoinCalibration, name: 'Calibration' } as const
 
-export const supportedChains = [calibration, mainnet]
+export const supportedChains = [mainnet, calibration]
 export const supportedChainIds = supportedChains.map((chain) => chain.id)
 export const defaultChain = supportedChains[0]
 
